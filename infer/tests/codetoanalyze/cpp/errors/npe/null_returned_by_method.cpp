f63582893fb980cfc88296ef8d5bd9b1fa6d68c0
Andrzej Kotulski <akotulski@fb.com>
2015-10-09 04:07:24 -0700

    Add basic e2e tests for C++
    Summary: @​public Add some basic tests to make sure that there is no
    regression afterwards
    
    Reviewed By: @dulmarod
    
    Differential Revision: D2521887
    
    fb-gh-sync-id: 1b8a15c

---
+++ infer/tests/codetoanalyze/cpp/errors/npe/null_returned_by_method.cpp
@@ -0,0 +1,34 @@
+/*
+* Copyright (c) 2015 - present Facebook, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+class X {
+public:
+  int field;
+};
+
+class XFactory {
+public:
+  X nonNull;
+  X* getNull() { return nullptr; }
+  X* getNonNull() { return &nonNull; }
+};
+
+int testNullDeref(XFactory *factory) {
+  if (factory) {
+    X *x = factory->getNull();
+    return x->field;
+  }
+}
+
+int testNoNullDeref(XFactory *factory) {
+  if (factory) {
+    X *x = factory->getNonNull();
+    return x->field;
+  }
+}
