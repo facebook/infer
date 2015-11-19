761e4acc080f66a5d67a8a4c501f33e14317cb0f
Dino Distefano <ddino@fb.com>
2015-11-19 07:08:22 -0800

    Adding models of CADisplayLink for catching retain cycles.
    Reviewed By: dulmarod
    
    Differential Revision: D2668570
    
    fb-gh-sync-id: 79063e4

---
+++ infer/models/objc/src/CADisplayLink.m
@@ -0,0 +1,31 @@
+/*
+* Copyright (c) 2015 - present Facebook, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+#import "CADisplayLink.h"
+
+@implementation CADisplayLink
+
+// Returns a new display link.
+// The model retains strongly the target object.
++ (CADisplayLink *)displayLinkWithTarget:(id)target
+                                selector:(SEL)sel {
+
+    CADisplayLink *c = [CADisplayLink alloc];
+        c->_target = target;
+        c->_selector = sel;
+
+    return c;
+}
+
+// Release the target
+- (void) invalidate {
+    self->_target = nil;
+}
+
+@end
---
+++ infer/tests/codetoanalyze/objc/errors/memory_leaks_benchmark/CADisplayLinkRetainCycle.m
@@ -0,0 +1,58 @@
+/*
+* Copyright (c) 2015 - present Facebook, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
+
+
+@interface A : NSObject
+@property (nonatomic, strong) CADisplayLink *displayLink;
+
+- (void) bla;
+- (void) invalidate;
+@end
+
+@implementation A
+
+- init {
+    _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(bla)];
+
+    return self;
+
+}
+
+- (void) bla {};
+
+- (void) invalidate {
+    // unregister displayLink target
+    [_displayLink invalidate];
+
+};
+
+- (void) dealloc {
+    [self dealloc];
+}
+
+@end
+
+
+void testCycle() {
+
+    A* a = [[A alloc] init];
+    A* b = a;
+
+}
+
+void testNoCycle() {
+
+    A* a = [[A alloc] init];
+    [a invalidate]; //break the cycle
+    [a dealloc];
+
+}
