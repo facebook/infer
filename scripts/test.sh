7a0c9c88bd478717c3de4f1a5cc09102310edca5
Jules Villard <jul@fb.com>
2015-12-15 15:27:17 -0800

    add test target
    Summary:
    public
    `make test` now runs the e2e tests.
    
    Reviewed By: jeremydubreil
    
    Differential Revision: D2754740
    
    fb-gh-sync-id: 9f80815

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,43 +12,10 @@ set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INFER_DIR="$SCRIPT_DIR/.."
-INFER_BIN="$INFER_DIR/infer/bin"
 
+XML=
 if [ "$1" == "--xml" ]; then
-    XML="--xml test.xml"
+    XML="_xml"
 fi
 
-TARGETS_TO_COMPILE=()
-TARGETS_TO_TEST=()
-if [ -e "$INFER_BIN/InferJava" ]; then
-    TARGETS_TO_COMPILE+=('java')
-    TARGETS_TO_TEST+=('java')
-fi
-
-platform=`uname`
-if [ -e "$INFER_BIN/InferClang" ]; then
-    TARGETS_TO_COMPILE+=('clang')
-    TARGETS_TO_TEST+=('c' 'cpp')
-    if command -v xcode-select > /dev/null; then
-        TARGETS_TO_TEST+=('objc')
-    fi
-fi
-
-# We must collect at least one target, or Infer must be recompiled
-([ -z "$TARGETS_TO_TEST" ] || [ -z "$TARGETS_TO_COMPILE" ]) \
-  && echo 'Infer is not compiled properly. Run make clean all' \
-  && exit 1
-
-cd $SCRIPT_DIR/..
-make ${TARGETS_TO_COMPILE[@]}
-
-# Must clean in order to force running the tests with the latest version
-# of infer. There is no dependency between buck targets and infer.
-buck clean
-rm -rf test.xml
-
-buck test ${TARGETS_TO_TEST[@]} $XML || exit 1
-
-if [ -e "$INFER_BIN/InferJava" ]; then
-    ./scripts/build_integration_tests.py
-fi
+make -C "$INFER_DIR" test$XML
