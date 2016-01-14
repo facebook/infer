1fc7d4c0a3faee87b05d82f02a8e5da8159da849
Dino Distefano <ddino@fb.com>
2016-01-14 10:06:43 -0800

    Fix year
    Reviewed By: dulmarod
    
    Differential Revision: D2831207
    
    fb-gh-sync-id: 67ea222

--- infer/tests/endtoend/objc/BlockCaptureCXXRefTest.java
+++ infer/tests/endtoend/objc/BlockCaptureCXXRefTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - present Facebook, Inc.
+ * Copyright (c) 2016 - present Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the BSD style license found in the
