45c02116f0085daf76474a78a25433372d28ee10
Vaibhav Tulsyan <vstulsyan@gmail.com>
2016-08-16 03:53:15 +0400

    Catch Checker further implementation

--- infer/src/checkers/catchChecker.ml
+++ infer/src/checkers/catchChecker.ml
@@ -3,60 +3,38 @@ open! Utils
 module F = Format
 module L = Logging
 
-(** backward analysis for computing set of maybe-live variables at each program point *)
+module Domain = struct (* implementation of join, widen, <=, pp *)
+  type astate = { in_catch : bool ; seen_call : bool; }
+  let is_bottom = false
+  let initial = { in_catch = false; seen_call = false; }
+  let (<=) ~lhs ~rhs = lhs == rhs
+  let join astate1 astate2 =
+    if (astate1.is_catch=true && astate2.is_catch=true)
+    then { astate with is_catch = true; seen_call = astate1.seen_call || astate2.seen_call }
+    else { astate with is_catch = false; seen_call = astate1.seen_call || astate2.seen_call }
+  let widen ~prev ~next ~num_iters:_ = join prev next
+  let pp fmt (astate1)= function
+    F.printf fmt "(State: is_catch=%s, seen_call=%s)" (bool_of_string astate1.is_catch, bool_of_string astate1.seen_call)
+end
 
-module Domain = AbstractDomain.FiniteSet(Var.Set)
-
-(* Check if current instruction is within a catch or not *)
 module TransferFunctions (CFG : ProcCfg.S) = struct
-  module CFG = CFG
-  module Domain = Domain
-  type extras = ProcData.no_extras
-
-  (* let in_catch astate =
-       state -> within_catch = true
-     let not_in_catch astate =
-       state -> within_catch = false
-     RETURN: astate' -> new state after performing changes on state
-     *)
-
   let exec_instr astate proc_data node instr =
-    (* if instr is a start or end of catch, modify state else do nothing *)
-
-    let is_catch_pvar _pvar : PVar.t =
-      (* Check if name of current Program Variable is "Catch" *)
-      (* Note: This is a slightly hacky way  of checking *)
-      if (_pvar Pvar.to_string = "Catch") then true else false
+    let is_catch_pvar _pvar =
+      if (string_is_prefix "CatchVar" (Pvar.to_string _pvar)) then true else false
     in
-
     let is_start_of_catch = function
       | Sil.Set (Exp.Lvar lhs_pvar, _, _, _) -> is_catch_pvar lhs_pvar
       | _ -> false in
-
     let is_maybe_end_of_catch = function
       | Sil.Abstract _ -> true
       | _ -> false in
 
-    astate
-
-    (*
-      Core logic:
-      If start_of_catch is encountered, change state and return new state
-      Else if end_of_catch is encountered, change state and return new state
-      Else return same state
-      | is_start_of_catch ->
-      | is_maybe_end_of_catch ->
-      | _ ->
-    *)
-
-    
-
+    if is_start_of_catch then {astate with is_catch=true;} else {astate with is_catch=astate.is_catch || false}
 end
 
 module Analyzer =
   AbstractInterpreter.Make
-    (ProcCfg.Backward(ProcCfg.Exceptional))
-    (Scheduler.ReversePostorder)
+    (ProcCfg.Forward(ProcCfg.Exceptional))
     (TransferFunctions)
 
 let checker { Callbacks.proc_desc; tenv; } =
