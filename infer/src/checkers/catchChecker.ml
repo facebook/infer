580f20b268205803b11e9eefff833802f3c2407d
Vaibhav Tulsyan <vstulsyan@gmail.com>
2016-08-16 03:57:17 +0400

    Changes to catch checker

--- infer/src/checkers/catchChecker.ml
+++ infer/src/checkers/catchChecker.ml
@@ -9,27 +9,27 @@ module Domain = struct (* implementation of join, widen, <=, pp *)
   let initial = { in_catch = false; seen_call = false; }
   let (<=) ~lhs ~rhs = lhs == rhs
   let join astate1 astate2 =
-    if (astate1.is_catch=true && astate2.is_catch=true)
-    then { astate with is_catch = true; seen_call = astate1.seen_call || astate2.seen_call }
-    else { astate with is_catch = false; seen_call = astate1.seen_call || astate2.seen_call }
+    if (astate1.in_catch=true && astate2.in_catch=true)
+    then { astate with in_catch = true; seen_call = astate1.seen_call || astate2.seen_call }
+    else { astate with in_catch = false; seen_call = astate1.seen_call || astate2.seen_call }
   let widen ~prev ~next ~num_iters:_ = join prev next
-  let pp fmt (astate1)= function
-    F.printf fmt "(State: is_catch=%s, seen_call=%s)" (bool_of_string astate1.is_catch, bool_of_string astate1.seen_call)
+  let pp fmt (astate1) = function
+    F.printf fmt "(State: in_catch=%b, seen_call=%b)" (astate1.in_catch, astate1.seen_call)
 end
 
 module TransferFunctions (CFG : ProcCfg.S) = struct
   let exec_instr astate proc_data node instr =
-    let is_catch_pvar _pvar =
+    let in_catch_pvar _pvar =
       if (string_is_prefix "CatchVar" (Pvar.to_string _pvar)) then true else false
     in
     let is_start_of_catch = function
-      | Sil.Set (Exp.Lvar lhs_pvar, _, _, _) -> is_catch_pvar lhs_pvar
+      | Sil.Set (Exp.Lvar lhs_pvar, _, _, _) -> in_catch_pvar lhs_pvar
       | _ -> false in
     let is_maybe_end_of_catch = function
       | Sil.Abstract _ -> true
       | _ -> false in
 
-    if is_start_of_catch then {astate with is_catch=true;} else {astate with is_catch=astate.is_catch || false}
+    if is_start_of_catch then {astate with in_catch=true;} else {astate with in_catch=astate.in_catch || false}
 end
 
 module Analyzer =
