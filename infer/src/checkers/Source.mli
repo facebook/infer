31093801d491157c7085a5e12197c52061dec973
Sam Blackshear <shb@fb.com>
2016-11-02 10:51:27 -0700

    [traces] Move all trace-related files to checkers
    Summary: Other checkers are going to start using these, so they shouldn't live in the Quandary directory anymore
    
    Reviewed By: jvillard
    
    Differential Revision: D4117359
    
    fbshipit-source-id: e3f151e

---
+++ infer/src/checkers/Source.mli
@@ -0,0 +1,21 @@
+(*
+ * Copyright (c) 2016 - present Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *)
+
+module type S = sig
+  include TraceElem.S
+
+  val is_footprint : t -> bool
+
+  val make_footprint : AccessPath.t -> CallSite.t -> t
+
+  val get_footprint_access_path: t -> AccessPath.t option
+
+  (** return Some (kind) if the call site is a taint source, None otherwise *)
+  val get : CallSite.t -> t option
+end
