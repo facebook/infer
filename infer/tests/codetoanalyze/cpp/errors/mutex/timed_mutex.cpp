c9803a648106c9c34318a62ca9ca84cf09ccd5cb
Mehdi Bouaziz <mbouaziz@fb.com>
2017-05-31 05:20:09 -0700

    False negative tests for std::timed_mutex
    Reviewed By: akotulski
    
    Differential Revision: D5154569
    
    fbshipit-source-id: c142261

--- infer/tests/codetoanalyze/cpp/errors/mutex/timed_mutex.cpp
+++ infer/tests/codetoanalyze/cpp/errors/mutex/timed_mutex.cpp
@@ -6,9 +6,26 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+#include <chrono>
 #include <mutex>
 
 void alarm1(std::timed_mutex& m) {
   m.lock();
+  m.lock();
+}
+
+void try_lock_bad(std::timed_mutex& m) {
+  m.try_lock();
+  m.lock();
+}
+
+void try_lock_for_bad_FN(std::timed_mutex& m) {
+  m.try_lock_for(std::chrono::seconds(123));
+  m.lock();
+}
+
+void try_lock_until_bad_FN(std::timed_mutex& m) {
+  std::chrono::time_point<std::chrono::steady_clock> timeout;
+  m.try_lock_until(timeout);
   m.lock();
 }
