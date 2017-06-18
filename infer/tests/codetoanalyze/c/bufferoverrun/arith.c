aad66bc6f691a9e3f2fdfc2ef4541b64fd29b0a7
Mehdi Bouaziz <mbouaziz@fb.com>
2017-06-18 13:07:53 -0700

    [inferbo] More tests
    Reviewed By: skcho
    
    Differential Revision: D5265505
    
    fbshipit-source-id: 5fc05f9

---
+++ infer/tests/codetoanalyze/c/bufferoverrun/arith.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017 - present Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+void modulo_signed_Bad(int i) {
+  char arr[5];
+  arr[i % 5] = 123;
+}
+
+void modulo_signed_Good_FP(int i) {
+  char arr[5];
+  if (i >= 0) {
+    arr[i % 5] = 123;
+  }
+}
+
+void modulo_signed_Good2_FP(int i) {
+  char arr[5];
+  int j = i % 5;
+  if (j >= 0) {
+    arr[j] = 123;
+  }
+}
+
+void modulo_unsigned_Good_FP(unsigned int i) {
+  char arr[5];
+  arr[i % 5] = 123;
+}
+
+void modulo_signed_var_Bad_FN(unsigned int len, int i) {
+  char arr[len];
+  arr[i % len] = 123;
+}
+
+void modulo_unsigned_var_Good(unsigned int len, unsigned int i) {
+  char arr[len];
+  arr[i % len] = 123;
+}
+
+unsigned int modulo_unsigned(unsigned int a, unsigned int b) { return a % b; }
+
+void modulo_call_Good(unsigned int len, unsigned int i) {
+  char arr[len];
+  arr[modulo_unsigned(i, len)] = 123;
+}
+
+int modulo_signed(int a, int b) { return a % b; }
+
+void modulo_call_Bad_FN(unsigned int len, int i) {
+  char arr[len];
+  arr[modulo_signed(i, len)] = 123;
+}
