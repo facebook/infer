4958130297901889208db5a1ce2f9df954c8442e
Jules Villard <jul@fb.com>
2017-08-01 09:25:27 -0700

    [reportdiff] take key into account for file renamings
    Summary:
    Previously, only the bug type + file name (up to renaming) were taken into
    account, which was too coarse. The key is file-independent and provides
    additional signal.
    
    Reviewed By: martinoluca
    
    Differential Revision: D5536858
    
    fbshipit-source-id: 70b732b

--- infer/src/backend/DifferentialFilters.ml
+++ infer/src/backend/DifferentialFilters.ml
@@ -118,9 +118,9 @@ let skip_duplicated_types_on_filenames renamings (diff: Differential.t) : Differ
     String.compare f1 f2
   in
   let cmp (issue1, _ as issue_with_previous_file1) (issue2, _ as issue_with_previous_file2) =
-    [%compare : string * issue_file_with_renaming]
-      (issue1.Jsonbug_t.bug_type, issue_with_previous_file1)
-      (issue2.Jsonbug_t.bug_type, issue_with_previous_file2)
+    [%compare : int * string * issue_file_with_renaming]
+      (issue1.Jsonbug_t.key, issue1.Jsonbug_t.bug_type, issue_with_previous_file1)
+      (issue2.Jsonbug_t.key, issue2.Jsonbug_t.bug_type, issue_with_previous_file2)
   in
   let introduced, preexisting, fixed =
     (* All comparisons will be made against filenames *before* renamings.
