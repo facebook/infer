f5ee50e7b5a9cae590b3c6b0c756019555d2e7db
Jules Villard <jul@fb.com>
2017-08-09 01:05:21 -0700

    [graphql] kill unused files
    Reviewed By: dulmarod
    
    Differential Revision: D5583094
    
    fbshipit-source-id: 63da86a

--- infer/src/clang/cFrontend_errors.ml
+++ infer/src/clang/cFrontend_errors.ml
@@ -85,8 +85,7 @@ let decl_checkers_list =
 (* List of checkers on stmts *that return 0 or 1 issue* *)
 let stmt_single_checkers_list =
   [ ComponentKit.component_file_cyclomatic_complexity_info
-  ; ComponentKit.component_initializer_with_side_effects_advice
-  ; GraphQL.DeprecatedAPIUsage.checker ]
+  ; ComponentKit.component_initializer_with_side_effects_advice ]
 
 let stmt_checkers_list = List.map ~f:single_to_multi stmt_single_checkers_list
 
