f7258c2ab4a269534b7dc52955daaea7b56f8aa3
Jeremy Dubreil <jrm@fb.com>
2017-09-11 12:01:01 -0700

    [infer][java] fix the translation of the finally branch
    Summary: The previous translation was creating unecessary nodes with unecessary `instanceof` checks.
    
    Reviewed By: jberdine
    
    Differential Revision: D5801019
    
    fbshipit-source-id: eb3a543

--- infer/src/java/jTransExn.ml
+++ infer/src/java/jTransExn.ml
@@ -10,7 +10,6 @@
 
 open! IStd
 module Hashtbl = Caml.Hashtbl
-open Javalib_pack
 open Sawja_pack
 module E = Logging
 
@@ -62,69 +61,66 @@ let translate_exceptions (context: JContext.t) exit_nodes get_body_nodes handler
           | []
            -> Location.none context.source_file
         in
-        let exn_type =
-          let class_name =
-            match handler.JBir.e_catch_type with
-            | None
-             -> JBasics.make_cn "java.lang.Exception"
-            | Some cn
-             -> cn
-          in
-          match
-            JTransType.get_class_type context.program (JContext.get_tenv context) class_name
-          with
-          | {Typ.desc= Tptr (typ, _)}
-           -> typ
-          | _
-           -> assert false
-        in
-        let id_instanceof = Ident.create_fresh Ident.knormal in
-        let instr_call_instanceof =
-          let instanceof_builtin = Exp.Const (Const.Cfun BuiltinDecl.__instanceof) in
-          let args =
-            [ (Exp.Var id_exn_val, Typ.mk (Tptr (exn_type, Typ.Pk_pointer)))
-            ; ( Exp.Sizeof
-                  {typ= exn_type; nbytes= None; dynamic_length= None; subtype= Subtype.exact}
-              , Typ.mk Tvoid ) ]
-          in
-          Sil.Call
-            ( Some (id_instanceof, Typ.mk (Tint IBool))
-            , instanceof_builtin
-            , args
-            , loc
-            , CallFlags.default )
-        in
-        let if_kind = Sil.Ik_switch in
-        let instr_prune_true = Sil.Prune (Exp.Var id_instanceof, loc, true, if_kind) in
-        let instr_prune_false =
-          Sil.Prune (Exp.UnOp (Unop.LNot, Exp.Var id_instanceof, None), loc, false, if_kind)
-        in
-        let instr_set_catch_var =
-          let catch_var = JContext.set_pvar context handler.JBir.e_catch_var ret_type in
-          Sil.Store (Exp.Lvar catch_var, ret_type, Exp.Var id_exn_val, loc)
-        in
-        let instr_rethrow_exn =
-          Sil.Store (Exp.Lvar ret_var, ret_type, Exp.Exn (Exp.Var id_exn_val), loc)
-        in
-        let node_kind_true = Procdesc.Node.Prune_node (true, if_kind, exn_message) in
-        let node_kind_false = Procdesc.Node.Prune_node (false, if_kind, exn_message) in
-        let node_true =
-          let instrs_true = [instr_call_instanceof; instr_prune_true; instr_set_catch_var] in
-          create_node loc node_kind_true instrs_true
-        in
-        let node_false =
-          let instrs_false =
-            [instr_call_instanceof; instr_prune_false]
-            @ if rethrow_exception then [instr_rethrow_exn] else []
-          in
-          create_node loc node_kind_false instrs_false
-        in
-        Procdesc.node_set_succs_exn procdesc node_true catch_nodes exit_nodes ;
-        Procdesc.node_set_succs_exn procdesc node_false succ_nodes exit_nodes ;
-        let is_finally = is_none handler.JBir.e_catch_type in
-        if is_finally then [node_true]
-          (* TODO (#4759480): clean up the translation so prune nodes are not created at all *)
-        else [node_true; node_false]
+        match handler.JBir.e_catch_type with
+        | None
+         -> let finally_node = create_node loc (Procdesc.Node.Stmt_node "Finally branch") [] in
+            Procdesc.node_set_succs_exn procdesc finally_node catch_nodes exit_nodes ;
+            [finally_node]
+        | Some exn_class_name
+         -> let exn_type =
+              match
+                JTransType.get_class_type context.program (JContext.get_tenv context)
+                  exn_class_name
+              with
+              | {Typ.desc= Tptr (typ, _)}
+               -> typ
+              | _
+               -> assert false
+            in
+            let id_instanceof = Ident.create_fresh Ident.knormal in
+            let instr_call_instanceof =
+              let instanceof_builtin = Exp.Const (Const.Cfun BuiltinDecl.__instanceof) in
+              let args =
+                [ (Exp.Var id_exn_val, Typ.mk (Tptr (exn_type, Typ.Pk_pointer)))
+                ; ( Exp.Sizeof
+                      {typ= exn_type; nbytes= None; dynamic_length= None; subtype= Subtype.exact}
+                  , Typ.mk Tvoid ) ]
+              in
+              Sil.Call
+                ( Some (id_instanceof, Typ.mk (Tint IBool))
+                , instanceof_builtin
+                , args
+                , loc
+                , CallFlags.default )
+            in
+            let if_kind = Sil.Ik_switch in
+            let instr_prune_true = Sil.Prune (Exp.Var id_instanceof, loc, true, if_kind) in
+            let instr_prune_false =
+              Sil.Prune (Exp.UnOp (Unop.LNot, Exp.Var id_instanceof, None), loc, false, if_kind)
+            in
+            let instr_set_catch_var =
+              let catch_var = JContext.set_pvar context handler.JBir.e_catch_var ret_type in
+              Sil.Store (Exp.Lvar catch_var, ret_type, Exp.Var id_exn_val, loc)
+            in
+            let instr_rethrow_exn =
+              Sil.Store (Exp.Lvar ret_var, ret_type, Exp.Exn (Exp.Var id_exn_val), loc)
+            in
+            let node_kind_true = Procdesc.Node.Prune_node (true, if_kind, exn_message) in
+            let node_kind_false = Procdesc.Node.Prune_node (false, if_kind, exn_message) in
+            let node_true =
+              let instrs_true = [instr_call_instanceof; instr_prune_true; instr_set_catch_var] in
+              create_node loc node_kind_true instrs_true
+            in
+            let node_false =
+              let instrs_false =
+                [instr_call_instanceof; instr_prune_false]
+                @ if rethrow_exception then [instr_rethrow_exn] else []
+              in
+              create_node loc node_kind_false instrs_false
+            in
+            Procdesc.node_set_succs_exn procdesc node_true catch_nodes exit_nodes ;
+            Procdesc.node_set_succs_exn procdesc node_false succ_nodes exit_nodes ;
+            [node_true; node_false]
       in
       let is_last_handler = ref true in
       let process_handler succ_nodes handler =
