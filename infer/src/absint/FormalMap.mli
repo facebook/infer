2b08bc4e74ad382b2670931540f469c29aca094b
Sam Blackshear <shb@fb.com>
2017-10-13 15:28:31 -0700

    [cleanup] make it possible to print FormalMap's
    Reviewed By: ngorogiannis
    
    Differential Revision: D6054161
    
    fbshipit-source-id: a1f41d2

--- infer/src/absint/FormalMap.mli
+++ infer/src/absint/FormalMap.mli
@@ -31,3 +31,6 @@ val get_formal_base : int -> t -> AccessPath.base option
     linear in the size of the formal map *)
 
 val get_formals_indexes : t -> (AccessPath.base * int) list
+(** Get a list of (base * index) pairs. Note: these are sorted by base, not index *)
+
+val pp : F.formatter -> t -> unit
