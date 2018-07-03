d44293738059c9d283eef460dafab0b785f84161
Martin Trojer <mtrojer@fb.com>
2018-07-03 00:38:36 -0700

    Add CFRunLoopObserverCreateWithHandler model and test
    Reviewed By: dulmarod
    
    Differential Revision: D8713250
    
    fbshipit-source-id: 7e9761c

--- infer/models/objc/src/CoreFoundation.c
+++ infer/models/objc/src/CoreFoundation.c
@@ -7,3 +7,18 @@
 #import <Foundation/Foundation.h>
 
 void CFRelease(CFTypeRef item) { __free_cf(item); }
+
+CFRunLoopObserverRef CFRunLoopObserverCreateWithHandler(
+    CFAllocatorRef allocator,
+    CFOptionFlags activities,
+    Boolean repeats,
+    CFIndex order,
+    void (^block)(CFRunLoopObserverRef observer, CFRunLoopActivity activity)) {
+  // We need to skip allocation for this function since we currently don't
+  // handle object finalizers in objc. This is to avoid false positive memory
+  // leaks when the 'observer' is correctly freed in the block argument.
+
+  // undefined function will get skipped and thus make
+  // CFRunLoopObserverCreateWithHandler return an angelic object
+  return getAngelicObject();
+}
---
+++ infer/tests/codetoanalyze/objc/errors/memory_leaks_benchmark/ReleasedInBlock.m
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <Foundation/Foundation.h>
+
+void allocAndReleaseInBlock() {
+  void (^callback)(CFRunLoopObserverRef obs, CFRunLoopActivity activity) =
+      ^(CFRunLoopObserverRef obs, CFRunLoopActivity activity) {
+        // We should check if the object is released properly, see
+        // infer/models/objc/src/CoreFoundation.c
+        CFRelease(obs);
+      };
+
+  CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(
+      NULL, kCFRunLoopBeforeWaiting, NO, UINT_MAX, callback);
+  CFRunLoopAddObserver(CFRunLoopGetMain(), observer, kCFRunLoopCommonModes);
+}
