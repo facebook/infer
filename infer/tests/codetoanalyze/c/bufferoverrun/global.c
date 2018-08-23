76bf31bc17079d055801040d347c557132e20e60
Sungkeun Cho <scho@fb.com>
2018-08-23 03:53:24 -0700

    [inferbo] Add a test case of global constant
    Summary: `CONDITION_ALWAYS_**` can be introduced by global constants.
    
    Reviewed By: ezgicicek, mbouaziz
    
    Differential Revision: D9478528
    
    fbshipit-source-id: 7b1a46e7a

--- infer/tests/codetoanalyze/c/bufferoverrun/global.c
+++ infer/tests/codetoanalyze/c/bufferoverrun/global.c
@@ -11,3 +11,13 @@ void compare_global_variable_bad() {
   if (global < 10)
     arr[10] = 1;
 }
+
+const int global_const_zero = 0;
+
+enum { global_const = global_const_zero };
+
+void compare_global_const_enum_bad_FN() {
+  char arr[10];
+  if (global_const < 10)
+    arr[10] = 1;
+}
