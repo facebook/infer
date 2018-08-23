a3d5f0283d323e4526f85494a1cd782a4cae1917
Jules Villard <jul@fb.com>
2018-08-23 05:43:34 -0700

    [clang] only pass `--Xclang` args to driver commands
    Summary:
    Not all clang commands are happy with all arguments, but the driver is usually
    the place we want to add arguments to.
    
    Reviewed By: martinoluca
    
    Differential Revision: D9421403
    
    fbshipit-source-id: fa6d39a9b

--- infer/src/clang/ClangCommand.ml
+++ infer/src/clang/ClangCommand.ml
@@ -190,7 +190,9 @@ let mk ~is_driver quoting_style ~prog ~args =
   let sanitized_args =
     filter_and_replace_unsupported_args ~blacklisted_flags:clang_blacklisted_flags
       ~blacklisted_flags_with_arg args
-    @ List.rev Config.clang_extra_flags
+  in
+  let sanitized_args =
+    if is_driver then sanitized_args @ List.rev Config.clang_extra_flags else sanitized_args
   in
   {exec= prog; orig_argv= sanitized_args; argv= sanitized_args; quoting_style; is_driver}
 
