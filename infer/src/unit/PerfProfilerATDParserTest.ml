b5efc56afb0c883ab8ce580e3cbfb1a567f80ac6
Martino Luca <martinoluca@fb.com>
2018-09-06 07:50:09 -0700

    [Perf] Add more fields to the perf profiler json parser
    Reviewed By: ddino
    
    Differential Revision: D9682878
    
    fbshipit-source-id: aebec94e1

--- infer/src/unit/PerfProfilerATDParserTest.ml
+++ infer/src/unit/PerfProfilerATDParserTest.ml
@@ -15,10 +15,12 @@ let test_parser =
   in
   [ ("test_parser_1", "[]", [])
   ; ( "test_parser_2"
-    , {|[{"function_name":"pkg/cls::\u003Cclinit>","avg_inclusive_cpu_time_ms":123.01234567899,"avg_exclusive_cpu_time_ms":9.8765432123456}]|}
+    , {|[{"function_name":"pkg/cls::\u003Cclinit>","avg_inclusive_cpu_time_ms":123.01234567899,"avg_exclusive_cpu_time_ms":9.8765432123456,"p90_inclusive_cpu_time_ms":1.012,"p90_exclusive_cpu_time_ms":3.14159}]|}
     , [ { Perf_profiler_t.function_name= "pkg/cls::<clinit>"
         ; avg_inclusive_cpu_time_ms= 123.01234567899
-        ; avg_exclusive_cpu_time_ms= 9.8765432123456 } ] ) ]
+        ; avg_exclusive_cpu_time_ms= 9.8765432123456
+        ; p90_inclusive_cpu_time_ms= 1.012
+        ; p90_exclusive_cpu_time_ms= 3.14159 } ] ) ]
   |> List.map ~f:(fun (name, test_input, expected_output) ->
          name >:: create_test test_input expected_output )
 
