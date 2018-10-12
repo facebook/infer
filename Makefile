f6ba0c81378729941325268f013899ac0fe40dd7
Josh Berdine <jjb@fb.com>
2018-10-12 09:03:51 -0700

    [sledge] Update build system, etc.
    Reviewed By: jvillard
    
    Differential Revision: D9850346
    
    fbshipit-source-id: 16fa35ae2

--- Makefile
+++ Makefile
@@ -151,7 +151,7 @@ OCAMLFORMAT_EXE?=ocamlformat
 fmt:
 	parallel $(OCAMLFORMAT_EXE) -i ::: $$(git diff --name-only --diff-filter=ACMRU $$(git merge-base origin/master HEAD) | grep "\.mli\?$$")
 
-DUNE_ML:=$(shell find * -name 'dune*.in' | grep -v workspace | grep -v sledge)
+DUNE_ML:=$(shell find * -name 'dune*.in' | grep -v workspace)
 
 .PHONY: fmt_dune
 fmt_dune:
--- sledge/tools/gen_version.sh
+++ sledge/tools/gen_version.sh
@@ -14,16 +14,14 @@ if [[ ! -z "$2" ]]; then
     # second arg passed when called from opam
     VERSION="$2"
 else
-    # second arg omitted when called from src/jbuild
+    # second arg omitted when called from src/dune
     if [[ ! "%%VERSION%%" == "%%"*"%%" ]]; then
         # file has been watermarked when building distrib archive
         VERSION="%%VERSION%%"
     else
         # file has not been watermarked when building in dev git tree
-        VERSION=$(git describe --tags --dirty --always)
+        VERSION=$(git describe --dirty --always)
     fi
 fi
 
-(test -e $FILE || touch $FILE);
-
-sed -e "s|%%VERSION%[%]|$VERSION|g" $FILE.in | diff $FILE - | patch --silent $FILE
+sed -e "s|%%VERSION%[%]|$VERSION|g" $FILE.in > $FILE
