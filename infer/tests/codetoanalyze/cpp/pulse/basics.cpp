6cce767d19c263051e05e8f8c4d2feae9dc46b26
Jules Villard <jul@fb.com>
2018-10-26 07:30:28 -0700

    [pulse] copy tests from ownership
    Summary:
    The time has come to keep track of which tests pass and which are FP/FN
    for pulse.
    
    Reviewed By: mbouaziz
    
    Differential Revision: D10854064
    
    fbshipit-source-id: 60938e48f

---
+++ infer/tests/codetoanalyze/cpp/pulse/basics.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <string>
+#include <vector>
+
+struct Aggregate {
+  int i;
+
+  ~Aggregate() {}
+};
+
+void FP_aggregate_reassign_ok() {
+  const int len = 5;
+  Aggregate arr[len];
+  for (int i = 0; i < len; i++) {
+    Aggregate s = {1};
+    // assign with curly bracket syntax doesn't call constructor; need to
+    // recognize that this is a reassignment anyway
+    arr[0] = s; // shouldn't be flagged as a use-after-lifetime
+  }
+}
+
+struct AggregateWithConstructedField {
+  std::string str;
+};
+
+void FP_aggregate_reassign2_ok() {
+  AggregateWithConstructedField arr[10];
+  for (int i = 0; i < 10; i++) {
+    // this is translated as string(&(a.str), "hi"). need to make sure this is
+    // treated the same as initializing a
+    AggregateWithConstructedField a{"hi"};
+    arr[i] = a;
+  }
+}
+
+struct NestedAggregate {
+  AggregateWithConstructedField a;
+};
+
+void FP_aggregate_reassign3_ok() {
+  NestedAggregate arr[10];
+  for (int i = 0; i < 10; i++) {
+    // this is translated as std::basic_string(&(a.str), "hi"). need to make
+    // sure this is treated the same as initializing a
+    NestedAggregate a{{"hi"}};
+    arr[i] = a;
+  }
+}
+
+int multiple_invalidations_branch_bad(int n, int* ptr) {
+  if (n == 7) {
+    delete ptr;
+  } else {
+    delete ptr;
+  }
+  return *ptr;
+}
+
+int multiple_invalidations_loop_bad(int n, int* ptr) {
+  for (int i = 0; i < n; i++) {
+    if (i == 7) {
+      delete ptr;
+    } else {
+      delete ptr;
+    }
+  }
+  return *ptr;
+}
+
+Aggregate* FP_pointer_arithmetic_ok(Aggregate* a) {
+  a->~Aggregate();
+  a++;
+  return a;
+}
+
+void iterator_pointer_arithmetic_ok(std::vector<Aggregate> v) {
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    it->~Aggregate();
+  }
+}
+
+struct A {
+  ~A();
+  int f(int i);
+};
+
+A getA();
+
+int FP_struct_inside_loop_ok(std::vector<int> numbers) {
+  int sum;
+  for (auto number : numbers) {
+    A a = getA();
+    sum += a.f(number);
+  }
+  return sum;
+}
---
+++ infer/tests/codetoanalyze/cpp/pulse/use_after_destructor.cpp
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct S {
+  int* f;
+  S(int i) {
+    f = new int;
+    *f = i;
+  }
+
+  // missing: operator= to copy the pointer. double delete can happen if
+  // operator= is called
+
+  ~S() { delete f; }
+};
+
+// destructor called at end of function, no issues
+void normal_scope_destructor_ok() { S s(1); }
+
+void nested_scope_destructor_ok() {
+  { S s(1); }
+}
+
+int FP_reinit_after_explicit_destructor_ok() {
+  S s(1);
+  s.~S();
+  s = S(2);
+  return *s.f;
+}
+
+void placement_new_explicit_destructor_ok() {
+  char buf[sizeof(S)];
+  {
+    S* s = new (buf) S(1);
+    s->~S();
+  }
+  {
+    // this use of [buf] shouldn't be flagged
+    S* s = new (buf) S(2);
+    s->~S();
+  }
+}
+
+void double_destructor_bad() {
+  S s(1);
+  s.~S();
+  // destructor will be called again after S goes out of scope, which is
+  // undefined behavior
+}
+
+int use_after_destructor_bad() {
+  S s(1);
+  s.~S();
+  int ret = *s.f;
+  s = S{2};
+  return ret;
+}
+
+// can't get this yet because we assume operator= copies resources correctly
+// (but this isn't true for S)
+void FN_use_after_scope1_bad() {
+  S s(1);
+  {
+    S tmp(2);
+    s = tmp; // translated as operator=(s, tmp)
+  } // destructor for tmp runs here
+  // destructor for s here; second time the value held by s has been destructed
+}
+
+void FN_use_after_scope2_bad() {
+  S s(1);
+  {
+    s = S(1);
+  } // destructor runs here, but our frontend currently doesn't insert it
+}
+
+struct POD {
+  int f;
+};
+
+// this code is ok since double-destructing POD struct is ok
+void destruct_twice_ok() {
+  POD p{1};
+  {
+    POD tmp{2};
+    p = tmp;
+  } // destructor for tmp
+} // destructor for p runs here, but it's harmless
+
+class Subclass : virtual POD {
+  int* f;
+  Subclass() { f = new int; }
+
+  /** frontend code for this destructor will be:
+   * ~Subclass:
+   *  __infer_inner_destructor_~Subclass(this)
+   *  __infer_inner_destructor_~POD(this)
+   *
+   * __infer_inner_destructor_~Subclass:
+   *  delete f;
+   *
+   * We need to be careful not to warn that this has been double-destructed
+   */
+  ~Subclass() { delete f; }
+};
+
+void FP_basic_placement_new_ok() {
+  S* ptr = new S(1);
+  S* tptr = new (ptr) S(1);
+  tptr->~S();
+  delete[] ptr;
+}
+
+S* destruct_pointer_contents_then_placement_new1_ok(S* s) {
+  s->~S();
+  new (s) S(1);
+  return s;
+}
+
+S* placement_new_aliasing1_bad() {
+  S* s = new S(1);
+  s->~S();
+  auto alias = new (s) S(2);
+  delete alias; // this deletes s too
+  return s; // bad, returning freed memory
+}
+
+S* placement_new_aliasing2_bad() {
+  S* s = new S(1);
+  s->~S();
+  auto alias = new (s) S(2);
+  delete s; // this deletes alias too
+  return alias; // bad, returning freed memory
+}
+
+void placement_new_non_var_ok() {
+  struct M {
+    S* s;
+  } m;
+  m.s = new S(1);
+  m.s->~S();
+  new (m.s) S(2);
+  delete m.s;
+}
+
+void return_placement_new_ok() {
+  auto mem = new S(1);
+  return new (mem) S(2);
+}
+
+void FP_destructor_in_loop_ok() {
+  for (int i = 0; i < 10; i++) {
+    S s(1);
+  }
+}
+
+int FN_use_after_scope3_bad() {
+  int* p;
+  {
+    int value = 3;
+    p = &value;
+  } // we do not know in the plugin that value is out of scope
+  return *p;
+}
+
+struct C {
+  C(int v) : f(v){};
+  ~C();
+  int f;
+};
+
+int use_after_scope4_bad() {
+  C* pc;
+  {
+    C c(3);
+    pc = &c;
+  }
+  return pc->f;
+}
+
+struct B {
+  ~B();
+};
+
+struct A {
+  ~A() { (void)*f; }
+  const B* f;
+};
+
+void FN_destructor_order_bad() {
+  A a;
+  B b;
+  a.f = &b;
+}
+
+struct A2 {
+  ~A2() {}
+  const B* f;
+};
+
+// need interprocedural analysis to fix this
+void FP_destructor_order_empty_destructor_ok() {
+  A2 a;
+  B b;
+  a.f = &b;
+}
