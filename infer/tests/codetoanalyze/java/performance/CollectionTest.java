dde972392679da14df086ef85740988ea33c7d67
Ezgi Çiçek <ezgi@fb.com>
2018-12-10 08:16:47 -0800

    [inferbo] Add tests for Java collections
    Reviewed By: mbouaziz
    
    Differential Revision: D13377810
    
    fbshipit-source-id: d57aa740a

--- infer/tests/codetoanalyze/java/performance/CollectionTest.java
+++ infer/tests/codetoanalyze/java/performance/CollectionTest.java
@@ -29,4 +29,33 @@ public class CollectionTest {
       iterate_over_mycollection(list);
     }
   }
+
+  // expected: same as iterate_over_mycollection(list)
+  void ensure_call(MyCollection<Integer> list) {
+    iterate_over_mycollection(list);
+  }
+
+  // expected: O (|size| . |list|)
+  void loop_over_call(int size, MyCollection<Integer> list) {
+    for (int i = 0; i < size; i++) {
+      iterate_over_mycollection(list);
+    }
+  }
+
+  // expected: O (|list|^2)
+  void iterate_over_call_quad(int size, MyCollection<Integer> list) {
+    for (Integer i : list) {
+      iterate_over_mycollection(list);
+    }
+  }
+
+  // expected O (|list|^3)
+  void nested_iterator_qubic(int size, MyCollection<Integer> list1, MyCollection<Integer> list2) {
+    for (Integer i : list1) {
+      for (Integer j : list2) {
+        iterate_over_mycollection(list1);
+        iterate_over_mycollection(list1);
+      }
+    }
+  }
 }
