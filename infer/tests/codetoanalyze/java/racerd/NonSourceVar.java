b50f56de16b5ff2aa605542be381f9c831854baa
Nikos Gorogiannis <nikosgorogiannis@fb.com>
2018-12-20 03:05:40 -0800

    [racerd] properly recognize all non-source variables
    Reviewed By: jeremydubreil, mbouaziz
    
    Differential Revision: D13505828
    
    fbshipit-source-id: f5a0be17d

---
+++ infer/tests/codetoanalyze/java/racerd/NonSourceVar.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package codetoanalyze.java.checkers;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+// TODO fix FN T38248006
+@ThreadSafe
+public class NonSourceVar {
+  private long field;
+
+  public void FN_conditionalOperatorBad(long v) {
+    field = field < v ? field : v;
+  }
+}
