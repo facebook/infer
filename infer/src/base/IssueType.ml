a3ecfdb8adf8c95ff8cc0331cacf53565718d6d7
Jeremy Dubreil <jrm@fb.com>
2019-02-13 16:18:05 -0800

    [infer][nullsafe] add a NULLSAFE_ prefix to the internal name of the Nullsafe errors
    Reviewed By: ngorogiannis
    
    Differential Revision: D13356313
    
    fbshipit-source-id: 77103f723

--- infer/src/base/IssueType.ml
+++ infer/src/base/IssueType.ml
@@ -329,7 +329,7 @@ let null_dereference = from_string "NULL_DEREFERENCE"
 
 let null_test_after_dereference = from_string ~enabled:false "NULL_TEST_AFTER_DEREFERENCE"
 
-let nullable_dereference = from_string "NULLABLE_DEREFERENCE"
+let nullable_dereference = from_string "NULLSAFE_NULLABLE_DEREFERENCE"
 
 let parameter_not_null_checked = from_string "PARAMETER_NOT_NULL_CHECKED"
 
