616be32b5b68817ed70a04670305821795ba3190
Josh Berdine <jjb@fb.com>
2019-02-25 07:07:15 -0800

    [sledge] Merge Config.trace_all into Config.trace
    Reviewed By: mbouaziz
    
    Differential Revision: D14075519
    
    fbshipit-source-id: 740e0f02a

--- sledge/src/config.ml
+++ sledge/src/config.ml
@@ -8,23 +8,21 @@
 (** Configuration options *)
 
 let trace_conv =
-  let parse s =
-    match Trace.parse s with
-    | Ok c -> Ok c.trace_mods_funs
-    | Error err -> Error err
-  in
-  let print fs c =
+  let print fs {trace_all; trace_mods_funs} =
     let pf fmt = Format.fprintf fs fmt in
-    Map.iteri c ~f:(fun ~key:mod_name ~data:{trace_mod; trace_funs} ->
-        ( match trace_mod with
-        | Some true -> pf "+%s" mod_name
-        | Some false -> pf "-%s" mod_name
-        | None -> () ) ;
-        Map.iteri trace_funs ~f:(fun ~key:fun_name ~data:fun_enabled ->
-            if fun_enabled then pf "+%s.%s" mod_name fun_name
-            else pf "-%s.%s" mod_name fun_name ) )
+    if trace_all then pf "*"
+    else
+      Map.iteri trace_mods_funs
+        ~f:(fun ~key:mod_name ~data:{trace_mod; trace_funs} ->
+          ( match trace_mod with
+          | Some true -> pf "+%s" mod_name
+          | Some false -> pf "-%s" mod_name
+          | None -> () ) ;
+          Map.iteri trace_funs ~f:(fun ~key:fun_name ~data:fun_enabled ->
+              if fun_enabled then pf "+%s.%s" mod_name fun_name
+              else pf "-%s.%s" mod_name fun_name ) )
   in
-  (parse, print)
+  (Trace.parse, print)
 
 type t =
   { compile_only: bool
@@ -38,17 +36,17 @@ type t =
          [@aka ["o"]] [@docv "output.llair"]
         (** Dump $(i,input.bc) translated to LLAIR in human-readable form to
             $(i,output.llair), or $(b,-) for $(b,stdout). *)
-  ; trace: Trace.trace_mods_funs
+  ; trace: Trace.config
          [@aka ["t"]]
          [@docv "spec"]
          [@conv trace_conv]
-         [@default Trace.none.trace_mods_funs]
+         [@default Trace.none]
         (** Enable debug tracing according to $(i,spec), which is a sequence
             of module and function names separated by $(b,+) or $(b,-). For
             example, $(b,Control-Control.exec_inst) enables all tracing in
             the $(b,Control) module except the $(b,Control.exec_inst)
-            function. *)
-  ; trace_all: bool [@aka ["v"]]  (** Enable all debug tracing. *) }
+            function. The $(i,spec) value $(b,* )enables all debug tracing. *)
+  }
 [@@deriving cmdliner]
 
 let run main =
@@ -57,6 +55,6 @@ let run main =
   |> function
   | `Error _ -> Caml.exit 1
   | `Help | `Version -> Caml.exit 0
-  | `Ok {compile_only; input; output; trace; trace_all} ->
-      Trace.init ~config:{trace_all; trace_mods_funs= trace} () ;
+  | `Ok {compile_only; input; output; trace} ->
+      Trace.init ~config:trace () ;
       main ~input ~output ~compile_only
