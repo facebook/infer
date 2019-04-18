c114a4b9f2ea16d09440e07efbe33cced0f970f8
Ezgi Çiçek <ezgi@fb.com>
2019-04-18 06:08:36 -0700

    [control,cost] Add test for dangling global enum
    Reviewed By: mbouaziz
    
    Differential Revision: D14995152
    
    fbshipit-source-id: d5f26727e

---
+++ infer/tests/codetoanalyze/objc/performance/control.m
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <stdbool.h>
+
+const int gvar = 0;
+
+enum {
+  kEof = gvar + 1,
+
+};
+
+// expected linear
+bool wrong_cvar_FN(int x) {
+
+  while (true) {
+    switch (x) {
+      // can't pick up the control variable gvar here due to dangling node.
+      case kEof:
+        return false;
+    }
+    x++;
+  }
+  return false;
+}
