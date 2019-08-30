43964c898c340d0db9b24650a5e994ecf3d268da
Phoebe Nichols <pnichols@fb.com>
2019-08-30 10:12:59 -0700

    Add test for introducing a procedure
    Summary: This tests the previous commit D17093980, which moves incremental analysis to run before capture
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D17113475
    
    fbshipit-source-id: 702d967b3

---
+++ infer/tests/build_systems/incremental_analysis_add_procedure/src_after/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+void a() {}
+
+void b() { a(); }
+
+void c() {}
+
+int main() {
+  c();
+  return 0;
+}
