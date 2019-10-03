fb77efea6ab3a8e8f72968c56f6140eef85252cd
Nikos Gorogiannis <nikosg@fb.com>
2019-10-03 05:08:34 -0700

    [starvation] add test for master-lock false positive
    Summary: Holding a master lock and then acquiring two other locks inside can generate a false positive as shown.
    
    Reviewed By: mityal
    
    Differential Revision: D17710076
    
    fbshipit-source-id: 5bc910ba2

---
+++ infer/tests/codetoanalyze/java/starvation/MasterLock.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+class MasterLock {
+  Object a, b;
+
+  // classic deadlock between 2 methods
+  // 1st method has a and wants b
+  void oneWayBad() {
+    synchronized (a) {
+      synchronized (b) {
+      }
+    }
+  }
+
+  // 2nd method has b and wants a
+  void theOtherWayBad() {
+    synchronized (b) {
+      synchronized (a) {
+      }
+    }
+  }
+
+  Object master, x, y;
+
+  // both methods hold the master lock so cannot interleave
+  // and thus cannot deadlock
+  void FP_oneWayOk() {
+    synchronized (master) {
+      synchronized (x) {
+        synchronized (y) {
+        }
+      }
+    }
+  }
+
+  void FP_theOtherWayOk() {
+    synchronized (master) {
+      synchronized (y) {
+        synchronized (x) {
+        }
+      }
+    }
+  }
+}
