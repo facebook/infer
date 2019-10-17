0a06353bcea6a6c51ec22619ceaf9ade49437fdc
Nikos Gorogiannis <nikosg@fb.com>
2019-10-17 02:55:28 -0700

    [starvation] more tests documenting interaction with thread status
    Summary: As per title.  These test pass already because the previous thread domain was sufficient to express them.  This won't necessarily be true when the whole-program analysis version comes around, because we may decide to not report on the `Threaded` elements (see domain).
    
    Reviewed By: dulmarod
    
    Differential Revision: D17930653
    
    fbshipit-source-id: 2174f6b22

---
+++ infer/tests/codetoanalyze/java/starvation/ThreadDeadlock.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import android.support.annotation.UiThread;
+import android.support.annotation.WorkerThread;
+
+class ThreadDeadlock {
+  Object lockA;
+
+  // methods cannot run in parallel because both are on UI thread, thus no deadlock
+
+  @UiThread
+  public synchronized void noParallelismAOk() {
+    synchronized (lockA) {
+    }
+  }
+
+  @UiThread
+  public void noParallelismBOk() {
+    synchronized (lockA) {
+      synchronized (this) {
+      }
+    }
+  }
+
+  Object lockB;
+
+  // deadlock, one method on UI thread, one on Worker thread
+
+  @UiThread
+  public synchronized void annotatedUiThreadBad() {
+    synchronized (lockB) {
+    }
+  }
+
+  @WorkerThread
+  public void annotatedWorkerThreadBad() {
+    synchronized (lockB) {
+      synchronized (this) {
+      }
+    }
+  }
+
+  Object lockC;
+
+  // deadlock as above, but here assertions are used to determine thread status
+
+  public synchronized void assertOnUIThreadBad() {
+    OurThreadUtils.assertOnUiThread();
+    synchronized (lockC) {
+    }
+  }
+
+  public void assertOnBackgroundThreadBad() {
+    OurThreadUtils.assertOnBackgroundThread();
+    synchronized (lockC) {
+      synchronized (this) {
+      }
+    }
+  }
+
+  Object lockD;
+
+  // deadlock as above, though less certain because the only hint of concurrency is that
+  // methods take locks
+
+  public synchronized void notAnnotatedBadA() {
+    synchronized (lockD) {
+    }
+  }
+
+  public void notAnnotatedBBad() {
+    synchronized (lockD) {
+      synchronized (this) {
+      }
+    }
+  }
+}
