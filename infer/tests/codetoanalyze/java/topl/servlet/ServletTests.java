fe736f4151855376f7c9e0847b68bce49ece7888
Radu Grigore <rgrigore@fb.com>
2020-01-22 08:25:16 -0800

    [topl] Added two (test) properties for ServletResponse.
    Summary:
    1. One should use either a writer or a stream to send a response, but not both.
    2. A response should be forwarded only if it was commited.
    Both properties are extracted from API comments on classes in the servlet API.
    
    Reviewed By: jvillard
    
    Differential Revision: D19514568
    
    fbshipit-source-id: 79f0257ed

---
+++ infer/tests/codetoanalyze/java/topl/servlet/ServletTests.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+class ServletTests {
+  void aBad(ServletResponse response) throws IOException {
+    PrintWriter w = response.getWriter();
+    ServletOutputStream s = response.getOutputStream();
+  }
+
+  void bBad(ServletResponse response) throws IOException {
+    PrintWriter w = response.getWriter();
+    ServletOutputStream s = response.getOutputStream();
+  }
+
+  void cBad(ServletRequest request, ServletResponse response, RequestDispatcher dispatcher)
+      throws IOException, ServletException {
+    PrintWriter w = response.getWriter();
+    dispatcher.forward(request, response);
+  }
+
+  // A bugfix for cBad.
+  void cOk(ServletRequest request, ServletResponse response, RequestDispatcher dispatcher)
+      throws IOException, ServletException {
+    PrintWriter w = response.getWriter();
+    w.flush();
+    dispatcher.forward(request, response);
+  }
+}
