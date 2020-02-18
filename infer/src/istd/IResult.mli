72a061d101b9c58ae4be9a2a20b6c5bff808b975
Sungkeun Cho <scho@fb.com>
2020-02-18 04:54:22 -0800

    [infer] Fix `make deadcode`
    Summary:
    ```
    [*ERROR**][99281] Entering directory '/Users/scho/infer/infer/src'
    [*ERROR**][99281] File "istd/IResult.mli", line 8, characters 25-43:
    [*ERROR**][99281] Error: Unbound module Result.Monad_infix
    [*ERROR**][99281] make[1]: *** [detect_dead_code] Error 1
    make: *** [real_deadcode] Error 2
    ```
    
    because of no `open! IStd`.
    
    Reviewed By: dulmarod
    
    Differential Revision: D19948195
    
    fbshipit-source-id: 4f36bcb47

--- infer/src/istd/IResult.mli
+++ infer/src/istd/IResult.mli
@@ -4,6 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *)
+
+open! IStd
+
 module Let_syntax : sig
   include module type of Result.Monad_infix
 
