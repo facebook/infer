2888a90e2a34a1a84f9480c6e2c9f8b8d657fe87
Radu Grigore <rgrigore@fb.com>
2020-02-26 07:33:39 -0800

    [topl] Added tests for inefficient iteration
    Summary:
    The test shows what that TOPL can express, in addition to bugs,
    efficiency properties. However, there seems to be an underlying problem
    in biabdaction that prevents this particular problem from being caught.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D20005404
    
    fbshipit-source-id: 466f79050

---
+++ infer/tests/codetoanalyze/java/topl/slowIter/SlowIterTests.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import java.util.Map;
+
+class SlowIterTests {
+  // T62611635
+  static <K, V> void FN_aBad(Map<K, V> m) {
+    for (K k : m.keySet()) {
+      System.out.printf("%s -> %s\n", k, m.get(k));
+    }
+  }
+
+  static <K, V> void aOk(Map<K, V> m) {
+    for (Map.Entry<K, V> e : m.entrySet()) {
+      System.out.printf("%s -> %s\n", e.getKey(), e.getValue());
+    }
+  }
+
+  // Inter-procedural variant of aBad.
+  static <K, V> void FN_bBad(Map<K, V> m) {
+    for (K k : m.keySet()) {
+      print(k, m);
+    }
+  }
+
+  static <K, V> void print(K k, Map<K, V> m) {
+    System.out.printf("%s -> %s\n", k, m.get(k));
+  }
+}
