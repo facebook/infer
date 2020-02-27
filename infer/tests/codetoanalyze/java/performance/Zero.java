43350f7c6b9bfbdce625d6ef66f9cece5873d7dd
Ezgi Çiçek <ezgi@fb.com>
2020-02-27 03:56:43 -0800

    [cost] Add Java tests for ZERO_EXECUTION_TIME
    Reviewed By: skcho
    
    Differential Revision: D20117064
    
    fbshipit-source-id: 15c5eae64

--- infer/tests/codetoanalyze/java/performance/Zero.java
+++ infer/tests/codetoanalyze/java/performance/Zero.java
@@ -4,7 +4,19 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import com.google.common.base.Preconditions;
+
 class Zero {
 
-  void zero_cost() {};
+  // cost: 1
+  void unit_cost() {};
+
+  public void infeasible_path_zero() {
+    Preconditions.checkState(false); // pruned to bottom
+  }
+
+  // we can't handle doubles properly in Inferbo
+  public void double_prune_FN(double fpp) {
+    Preconditions.checkArgument(fpp > 0.0 && fpp < 0.0);
+  }
 }
