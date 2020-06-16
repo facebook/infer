fecf954c6e34e8232ad4f38d97f9f10170a62f4e
Qianyi Shu <qianyishu@fb.com>
2020-06-16 00:48:57 -0700

    [cost] add objc test for NSArray and NSMutableArray
    Summary:
    Add objc test for ```NSArray``` and ```NSMutableArray```.
    ```NSMutableArray``` is a subclass of ```NSArray```.
    For documentation of  ```NSArray```, https://developer.apple.com/documentation/foundation/nsarray?language=objc
    
    For documentation of ```NSMutableArray```, https://developer.apple.com/documentation/foundation/nsmutablearray?language=objc
    
    The underlying mechanism for ```NSMutableArray``` is quite complicated. It changes the underlying data structure during runtime, so it is possible to have say O(log n) complexity for accessing element in array. (See here https://opensource.apple.com/source/CF/CF-855.11/CFArray.h) However, this is unlikely to happen if the engineer does not abuse the usage of the class ```NSMutableArray``` according to at least two ios engineers. So here the complexity is set to match the normal expectation of the complexity.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D22041277
    
    fbshipit-source-id: c27f43167

---
+++ infer/tests/codetoanalyze/objc/performance/NSArray.m
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#import <Foundation/Foundation.h>
+
+// init array
+
+void nsarray_init_constant_FP() {
+  NSArray* array = [[NSArray alloc] init];
+
+  for (int i = 0; i < array.count; i++) {
+  }
+}
+
+void nsarray_init_with_array_linear_FP(NSArray* array) {
+  NSArray* ref_array = [[NSArray alloc] initWithArray:array];
+
+  for (int i = 0; i < ref_array.count; i++) {
+  }
+}
+
+void nsarray_init_with_array_constant_FP() {
+  NSArray* array = [[NSArray alloc] init];
+  nsarray_init_with_array_linear_FP(array);
+}
+
+void nsarray_init_with_array_copy_linear_FP(NSArray* array) {
+  NSArray* copy_array = [[NSArray alloc] initWithArray:array copyItems:YES];
+  for (int i = 0; i < copy_array.count; i++) {
+  }
+}
+
+NSArray* nsarray_init_with_objects_constant() {
+  NSString* strings[3];
+  strings[0] = @"First";
+  strings[1] = @"Second";
+  strings[2] = @"Third";
+
+  return [NSArray arrayWithObjects:strings count:2];
+}
+
+// derive new array
+
+NSArray* nsarray_add_object_constant(id obj) {
+  NSArray* array = [[NSArray alloc] init];
+  return [array arrayByAddingObject:obj];
+}
+
+NSArray* nsarray_add_objects_from_array_linear_FN(NSArray* append_array) {
+  NSArray* array = [[NSArray alloc] init];
+  return [array arrayByAddingObjectsFromArray:append_array];
+}
+
+// query element
+
+void nsarray_access_constant() {
+  NSArray* array = @[ @1.0f, @2.0f, @3.0f, @4.0f, @5.0f, @6.0f, @7.0f, @8.0f ];
+  for (int i = 0; i < 4; i++) {
+    [array objectAtIndex:i];
+  }
+}
+
+void nsarray_access_linear_FP(NSArray* array) {
+  id obj;
+  for (int i = 0; i < array.count; i++) {
+    obj = array[i];
+  }
+}
+
+void nsarray_contains_object_linear_FN(NSArray* array) {
+  [array containsObject:@1];
+}
+
+id nsarray_first_object_constant(NSArray* array) { return array.firstObject; }
+
+id nsarray_last_object_constant(NSArray* array) { return array.lastObject; }
+
+// find element
+NSInteger nsarray_binary_search_log_FN(NSArray* sorted_array) {
+  NSNumber* target = @5;
+  return [sorted_array indexOfObject:target
+                       inSortedRange:NSMakeRange(0, sorted_array.count)
+                             options:NSBinarySearchingFirstEqual
+                     usingComparator:^(id lhs, id rhs) {
+                       return [lhs compare:rhs];
+                     }];
+}
+
+// sort array
+
+NSArray* nsarray_sort_using_descriptors_constant() {
+  NSArray* array = @[ @"Grapes", @"Apples", @"Oranges" ];
+  NSSortDescriptor* sd = [[NSSortDescriptor alloc] initWithKey:nil
+                                                     ascending:YES];
+  return [array sortedArrayUsingDescriptors:@[ sd ]];
+}
+
+NSArray* nsarray_sort_using_descriptors_nlogn_FN(NSArray* array) {
+  NSSortDescriptor* sd = [[NSSortDescriptor alloc] initWithKey:nil
+                                                     ascending:YES];
+  return [array sortedArrayUsingDescriptors:@[ sd ]];
+}
+
+// iterate through array
+
+void nsarray_iterate_linear_FN(NSArray* array) {
+  NSInteger sum = 0;
+  for (id obj in array) {
+    sum += (NSInteger)obj;
+  }
+}
+
+void nsarray_enumerator_linear_FN(NSArray* array) {
+  NSEnumerator* enumerator = [array objectEnumerator];
+
+  id obj;
+  NSInteger sum = 0;
+
+  while (obj = [enumerator nextObject]) {
+    sum += (NSInteger)obj;
+  }
+}
+
+// compare array
+boolean_t nsarray_is_equal_to_array_linear_FN(NSArray* array1,
+                                              NSArray* array2) {
+  return [array1 isEqualToArray:array2];
+}
---
+++ infer/tests/codetoanalyze/objc/performance/NSMutableArray.m
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#import <Foundation/Foundation.h>
+
+// init array
+
+void nsmarray_init_with_capacity_constant_FP() {
+  NSMutableArray* table = [[NSMutableArray alloc] initWithCapacity:256];
+
+  for (int i = 0; i < table.count; i++) {
+    table[i] = @"somevalue";
+  }
+}
+
+// add element
+
+void nsmarray_empty_ok_costant() {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  [array insertObject:@1 atIndex:0];
+}
+
+void nsmarray_add_in_loop_constant_FP() {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  for (int i = 0; i < 10; i++) {
+    [array addObject:[NSNumber numberWithInt:i]];
+  }
+  for (int i = 0; i < array.count; i++) {
+  }
+}
+
+void nsmarray_add_in_loop_linear(NSUInteger n) {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  for (int i = 0; i < n; i++) {
+    [array addObject:[NSNumber numberWithInt:i]];
+  }
+  for (int i = 0; i < n; i++) {
+  }
+}
+
+void nsmarray_add_in_loop_quadratic(NSUInteger n, NSUInteger m) {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      [array addObject:[NSNumber numberWithInt:i + j]];
+    }
+  }
+}
+
+void nsmarray_add_then_loop_constant_FP() {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  [array addObject:@0];
+  [array addObject:@1];
+  [array addObject:@2];
+  [array addObject:@3];
+  [array addObject:@4];
+  [array addObject:@5];
+  [array addObject:@6];
+  [array addObject:@7];
+  [array addObject:@8];
+  [array addObject:@9];
+  [array addObject:@10];
+
+  for (int i = 0; i < array.count; i++) {
+  }
+}
+
+void nsmarray_add_all_constant() {
+  NSMutableArray* array1 = [[NSMutableArray alloc] init];
+  [array1 addObject:@2];
+  [array1 addObject:@3];
+
+  NSMutableArray* array2 = [[NSMutableArray alloc] init];
+  [array2 addObject:@0];
+  [array2 addObject:@1];
+
+  NSIndexSet* index_set = [[NSIndexSet alloc] initWithIndex:0];
+
+  [array2 addObjectsFromArray:array1];
+}
+
+// set element
+
+void nsmarray_set_constant(NSMutableArray* array) { array[0] = @1; }
+
+void nsmarray_set_linear_FP(NSMutableArray* array) {
+  for (int i = 0; i < array.count; i++) {
+    array[i] = [NSNumber numberWithInt:([array[i] intValue] + 1)];
+  }
+}
+
+void nsmarray_set_constant_FP() {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  [array addObject:@0];
+  [array addObject:@1];
+  [array addObject:@2];
+
+  for (int i = 0; i < array.count; i++) {
+    array[i] = [NSNumber numberWithInt:i];
+  }
+}
+
+// remove element
+
+id nsmarray_reomove_constant() {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  [array addObject:@0];
+  [array addObject:@1];
+  [array removeObjectAtIndex:0];
+  return array[0];
+}
+
+void nsmarray_remove_in_loop_constant_FP() {
+  NSMutableArray* array = [[NSMutableArray alloc] init];
+  for (int i = 0; i < 10; i++) {
+    [array addObject:[NSNumber numberWithInt:i]];
+  }
+  for (int i = 0; i < array.count; i++) {
+    [array removeObjectAtIndex:i];
+  }
+}
