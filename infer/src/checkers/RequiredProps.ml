05f1d1676ee5d2055ba71050a0200cdbc3da4ac0
Ezgi Çiçek <ezgi@fb.com>
2020-07-06 05:57:30 -0700

    [litho] Tweak the error message
    Summary: Make the message more informative.
    
    Reviewed By: Katalune
    
    Differential Revision: D22389426
    
    fbshipit-source-id: bb98e17e8

--- infer/src/checkers/RequiredProps.ml
+++ infer/src/checkers/RequiredProps.ml
@@ -68,7 +68,9 @@ let report_missing_required_prop proc_desc err_log prop parent_typename ~create_
       | VarProp {var_prop; prop} ->
           F.asprintf "Either @Prop %s or @Prop(varArg = %s)" prop var_prop
     in
-    F.asprintf "%a is required for component %s, but is not set before the call to build()"
+    F.asprintf
+      "%a is required for component %s, but is not set before the call to build(). Either set the \
+       missing @Prop or make @Prop(optional = true)."
       MarkupFormatter.pp_bold prop_string (Typ.Name.name parent_typename)
   in
   let make_single_trace loc message = Errlog.make_trace_element 0 loc message [] in
