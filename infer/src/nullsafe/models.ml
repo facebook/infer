7d0671910a0e5ec98d0086e54f11e2a7e05bfbf1
Mitya Lyubarskiy <mityal@fb.com>
2020-08-14 10:00:50 -0700

    [nullsafe][easy] Add interface file for models.ml
    Summary:
    This is one of two files in nullsafe that remains interface-less.
    Let's fix it!
    
    Reviewed By: jvillard
    
    Differential Revision: D23056324
    
    fbshipit-source-id: a6592f7ae

--- infer/src/nullsafe/models.ml
+++ infer/src/nullsafe/models.ml
@@ -9,8 +9,6 @@ open! IStd
 module Hashtbl = Caml.Hashtbl
 open ModelTables
 
-(** Module for standard library models. *)
-
 (** Unique representation of a method signature in form of a string. *)
 let to_unique_id proc_name = Procname.to_unique_id (Procname.Java proc_name)
 
@@ -54,8 +52,6 @@ let get_special_method_modelled_nullability tenv java_proc_name =
   else None
 
 
-(** Return the annotated signature of the procedure, taking into account models. External models
-    take precedence over internal ones. *)
 let get_modelled_annotated_signature ~is_callee_in_trust_list tenv proc_attributes =
   let proc_name =
     Procname.as_java_exn proc_attributes.ProcAttributes.proc_name
@@ -99,50 +95,37 @@ let get_modelled_annotated_signature ~is_callee_in_trust_list tenv proc_attribut
   annotated_signature |> correct_by_internal_models |> correct_by_external_models
 
 
-(** Check if the procedure is one of the known methods asserting nullability of the object. Nullsafe
-    should understand that both the argument and return value are non-nullable after the call. *)
 let is_check_not_null proc_name =
   table_has_procedure check_not_null_table proc_name || match_method_name proc_name "checkNotNull"
 
 
-(** Parameter number (starting from 1) for a procedure known to produce a non-nullable assertion.
-    [None] if the function is not known to be an aseertion OR the parameter number is not known *)
 let get_check_not_null_parameter proc_name =
   let proc_id = to_unique_id proc_name in
   Hashtbl.find_opt check_not_null_parameter_table proc_id
 
 
-(** Check if the procedure is one of the known Preconditions.checkState. *)
 let is_check_state proc_name =
   table_has_procedure check_state_table proc_name || match_method_name proc_name "checkState"
 
 
-(** Check if the procedure is one of the known Preconditions.checkArgument. *)
 let is_check_argument proc_name =
   table_has_procedure check_argument_table proc_name || match_method_name proc_name "checkArgument"
 
 
-(** Check if the procedure does not return. *)
 let is_noreturn proc_name = table_has_procedure noreturn_table proc_name
 
-(** Check if the procedure returns true on null. *)
 let is_true_on_null proc_name = table_has_procedure true_on_null_table proc_name
 
-(** Check if the procedure returns false on null. *)
 let is_false_on_null proc_name =
   (* The only usecase for now - consider all overrides of `Object.equals()` correctly
      implementing the Java specification contract (returning false on null). *)
   PatternMatch.Java.is_override_of_lang_object_equals (Procname.Java proc_name)
 
 
-(** Check if the procedure is Map.containsKey(). *)
 let is_containsKey proc_name = table_has_procedure containsKey_table proc_name
 
-(** Check if the procedure is Map.put(). *)
 let is_mapPut proc_name = table_has_procedure mapPut_table proc_name
 
-(** Check if a (nullable) method has a non-nullable alternative: A method that does the same as
-    [proc_name] but asserts the result is not null before returning to the caller. *)
 let find_nonnullable_alternative proc_name =
   (* NOTE: For now we fetch this info from internal models.
      It is a good idea to support this feature in a user-facing third party repository. *)
@@ -150,7 +133,6 @@ let find_nonnullable_alternative proc_name =
   Hashtbl.find_opt nonnull_alternatives_table proc_id
 
 
-(* Check if a given field is known to be a non-nullable *)
 let is_field_nonnullable field_name =
   Hashtbl.find_opt field_nullability_table (Fieldname.to_full_string field_name)
   |> Option.value_map ~f:(fun is_nullable -> not is_nullable) ~default:false
