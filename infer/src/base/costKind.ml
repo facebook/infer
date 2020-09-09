18baa1f1261a5037a1bdc36f9dfafd843f1f8122
Sungkeun Cho <scho@fb.com>
2020-09-09 06:32:54 -0700

    [cost] Enable top_and_unreachable issues of autoreleasepool size
    Reviewed By: ezgicicek
    
    Differential Revision: D23597736
    
    fbshipit-source-id: 5eca0a02c

--- infer/src/base/costKind.ml
+++ infer/src/base/costKind.ml
@@ -53,4 +53,4 @@ type kind_spec = {kind: t; (* for non-diff analysis *) top_and_unreachable: bool
 
 let enabled_cost_kinds =
   [ {kind= OperationCost; top_and_unreachable= true}
-  ; {kind= AutoreleasepoolSize; top_and_unreachable= false} ]
+  ; {kind= AutoreleasepoolSize; top_and_unreachable= true} ]
