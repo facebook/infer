7eba62c9edd72fb9927b5aa05df927b1151934db
Jules Villard <jul@fb.com>
2020-09-09 08:35:21 -0700

    [doc] update documentation on building checkers
    Summary:
    - freshen up /docs/next/absint-framework to give sensible advice, and
      delete outdated bits that are now in the API docs so they remain fresh
    - delete SimpleChecker.ml as it's just a source of bitrot
    - delete the "adding checkers" page as it's completely outdated and
      subsumed by the "AI framework" page + the labs.
    
    Reviewed By: jberdine
    
    Differential Revision: D23597271
    
    fbshipit-source-id: 78b541746

--- infer/src/backend/registerCheckers.ml
+++ infer/src/backend/registerCheckers.ml
@@ -11,13 +11,6 @@ open! IStd
 
 module F = Format
 
-(* make sure SimpleChecker.ml is not dead code *)
-let () =
-  if false then
-    let module SC = SimpleChecker.Make in
-    ()
-
-
 type callback_fun =
   | Procedure of Callbacks.proc_callback_t
   | DynamicDispatch of Callbacks.proc_callback_t
