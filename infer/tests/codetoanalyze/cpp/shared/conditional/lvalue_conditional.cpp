b6460870dc04bc7e70b036f77e001dc5f9fd3d15
Jules Villard <jul@fb.com>
2020-11-09 03:17:39 -0800

    [biabd] rename a test to follow naming conventions
    Reviewed By: ngorogiannis
    
    Differential Revision: D24794526
    
    fbshipit-source-id: 9b2392c35

--- infer/tests/codetoanalyze/cpp/shared/conditional/lvalue_conditional.cpp
+++ infer/tests/codetoanalyze/cpp/shared/conditional/lvalue_conditional.cpp
@@ -28,18 +28,18 @@ int div_temp_lvalue(int a, int b) {
   return 1 / r;
 }
 
-int div0_choose_lvalue() { return 1 / choose_lvalue(1); }
+int div0_choose_lvalue_bad() { return 1 / choose_lvalue(1); }
 
-int div1_choose_lvalue() { return 1 / choose_lvalue(0); }
+int div1_choose_lvalue_ok() { return 1 / choose_lvalue(0); }
 
-int div0_choose_rvalue() { return 1 / choose_rvalue(1); }
+int div0_choose_rvalue_bad() { return 1 / choose_rvalue(1); }
 
-int div1_choose_rvalue() { return 1 / choose_rvalue(0); }
+int div1_choose_rvalue_ok() { return 1 / choose_rvalue(0); }
 
-int div0_assign_conditional() { return 1 / assign_conditional(0); }
+int div0_assign_conditional_bad() { return 1 / assign_conditional(0); }
 
-int div1_assign_conditional() { return 1 / assign_conditional(1); }
+int div1_assign_conditional_ok() { return 1 / assign_conditional(1); }
 
-int div0_temp_lvalue() { return div_temp_lvalue(1, 0); }
+int div0_temp_lvalue_bad() { return div_temp_lvalue(1, 0); }
 
-int div1_temp_lvalue() { return div_temp_lvalue(0, 1); }
+int div1_temp_lvalue_ok() { return div_temp_lvalue(0, 1); }
