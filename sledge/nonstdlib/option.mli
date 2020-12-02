c063a91c7c53bd2f29bd05603bfa920744890f41
Josh Berdine <jjb@fb.com>
2020-12-02 13:46:46 -0800

    [sledge] Add Option.flat_map
    Reviewed By: jvillard
    
    Differential Revision: D25146161
    
    fbshipit-source-id: a8d60de12

--- sledge/nonstdlib/option.mli
+++ sledge/nonstdlib/option.mli
@@ -17,6 +17,7 @@ include Monad_intf.S with type 'a t := 'a t
 
 val pp : ('a_pp -> 'a -> unit, unit) fmt -> 'a_pp -> 'a option pp
 val map_or : 'a t -> default:'b -> f:('a -> 'b) -> 'b
+val flat_map : 'a t -> f:('a -> 'b t) -> 'b t
 val iter : 'a t -> f:('a -> unit) -> unit
 val exists : 'a t -> f:('a -> bool) -> bool
 val for_all : 'a t -> f:('a -> bool) -> bool
