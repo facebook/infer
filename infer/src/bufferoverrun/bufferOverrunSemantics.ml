a705fd0fc606b8848ce7226f894087f1170e3d24
David Hauzar <hauzar@adacore.com>
2021-06-03 17:30:10 +0200

    [F] Fix a value used to represent a pointer to a function.
    Use an abstract value representing any non-null pointer to
    represent value of a function name constant instead of Val.Itv.top
    that represents any numerical value.
    
    TODO: Use an abstract value exactly representing the function by
    setting Val.func_ptrs to be a singleton set containing the function.

--- infer/src/bufferoverrun/bufferOverrunSemantics.ml
+++ infer/src/bufferoverrun/bufferOverrunSemantics.ml
@@ -21,6 +21,11 @@ let eval_const : Typ.IntegerWidths.t -> Const.t -> Val.t =
       Val.of_big_int (IntLit.to_big_int intlit)
   | Const.Cstr s ->
       Val.of_literal_string integer_type_widths s
+  | Const.Cfun _ ->
+      (* Const.Cfun represents the address of a function call.
+         For now, return just non-null unknown pointer.
+         TODO: set func_ptrs field to be a singleton set containing the function *)
+      {Val.top with itv= Itv.one}
   | _ ->
       Val.Itv.top
 
