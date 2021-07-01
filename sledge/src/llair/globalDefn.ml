ee4bb29cc4022fac0befd1aa3f83b596216065f9
Josh Berdine <jjb@fb.com>
2021-07-01 12:18:23 -0700

    [sledge] Global initializers do not contain registers
    Summary:
    The initializers of globals are constant expressions, and therefore
    contain no registers. This change adds an assertion to make this
    existing invariant explicit.
    
    Differential Revision: D29441160
    
    fbshipit-source-id: 4da4e74d1

--- sledge/src/llair/globalDefn.ml
+++ sledge/src/llair/globalDefn.ml
@@ -18,8 +18,10 @@ let pp ppf {name; init; loc} =
 
 let invariant g =
   let@ () = Invariant.invariant [%here] g [%sexp_of: t] in
-  match Global.typ g.name with
+  ( match Global.typ g.name with
   | Pointer {elt} -> assert (Option.is_none g.init || Typ.is_sized elt)
-  | _ -> assert false
+  | _ -> assert false ) ;
+  Option.iter g.init ~f:(fun init ->
+      Exp.fold_regs init () ~f:(fun _ () -> assert false) )
 
 let mk ?init name loc = {name; init; loc} |> check invariant
