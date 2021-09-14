8ffd5b0d7716e38729d5143b72cb2dcc784d4f1e
Akos Hajdu <akoshajdu@fb.com>
2021-09-14 07:01:06 -0700

    [erl-frontend] Extract Erlang block related stuff to own module
    Summary: ErlangTranslator is big, extracting Block related stuff (which was already a submodule) makes it more readable.
    
    Reviewed By: skcho
    
    Differential Revision: D30901893
    
    fbshipit-source-id: 6f3db82c9

--- infer/src/erlang/ErlangTranslator.ml
+++ infer/src/erlang/ErlangTranslator.ml
@@ -7,104 +7,19 @@
 
 open! IStd
 module Ast = ErlangAst
+module Block = ErlangBlock
 module Env = ErlangEnvironment
 module L = Logging
 module Node = ErlangNode
 
 let mangled_arg (n : int) : Mangled.t = Mangled.from_string (Printf.sprintf "$arg%d" n)
 
-let ( |~~> ) from to_ = Procdesc.set_succs from ~normal:(Some to_) ~exn:None
+let ( |~~> ) = ErlangBlock.( |~~> )
 
 let update_location line (env : (_, _) Env.t) =
   let location = {env.location with line; col= -1} in
   {env with location}
 
-
-(** Groups several helpers used to create blocks. *)
-module Block = struct
-  type t = {start: Procdesc.Node.t; exit_success: Procdesc.Node.t; exit_failure: Procdesc.Node.t}
-
-  let make_success env =
-    let exit_success, exit_failure = (Node.make_nop env, Node.make_nop env) in
-    {start= exit_success; exit_success; exit_failure}
-
-
-  let make_failure env =
-    let exit_success, exit_failure = (Node.make_nop env, Node.make_nop env) in
-    {start= exit_failure; exit_success; exit_failure}
-
-
-  (** Makes one block of a list of blocks. Meant to be used only by the functions [all_blocks] and
-      [any_block] defined immediately below. If [b] comes before [c] in the list [blocks], then an
-      edge is added from [continue b] to [c.start]. For all blocks [b] in the list [blocks], an edge
-      is added from [stop b] to [new_stop], where [new_stop] is a new node of type join. If there is
-      only one block, then it is returned with no modification.*)
-  let sequence ~(continue : t -> Procdesc.Node.t) ~(stop : t -> Procdesc.Node.t) env
-      (blocks : t list) =
-    match blocks with
-    | [] ->
-        L.die InternalError "blocks should not be empty"
-    | [one_block] ->
-        (one_block.start, continue one_block, stop one_block)
-    | first_block :: next_blocks ->
-        let continue_node =
-          let f previous b =
-            previous |~~> [b.start] ;
-            continue b
-          in
-          List.fold ~f ~init:(continue first_block) next_blocks
-        in
-        let new_stop = Node.make_join env in
-        List.iter ~f:(fun b -> stop b |~~> [new_stop]) blocks ;
-        (first_block.start, continue_node, new_stop)
-
-
-  (** Chain a list of blocks together in a conjunctive style: a failure in any block leads to a
-      global failure, and successes lead to the next block. *)
-  let all env (blocks : t list) : t =
-    match blocks with
-    | [] ->
-        make_success env
-    | _ ->
-        let continue b = b.exit_success in
-        let stop b = b.exit_failure in
-        let start, exit_success, exit_failure = sequence ~continue ~stop env blocks in
-        {start; exit_success; exit_failure}
-
-
-  (** Chain a list of blocks together in a disjunctive style: a success in any block leads to a
-      global success, and failures lead to the next block. *)
-  let any env (blocks : t list) : t =
-    match blocks with
-    | [] ->
-        make_failure env
-    | _ ->
-        let continue b = b.exit_failure in
-        let stop b = b.exit_success in
-        let start, exit_failure, exit_success = sequence ~continue ~stop env blocks in
-        {start; exit_success; exit_failure}
-
-
-  let make_instruction env instructions =
-    let exit_success = Node.make_stmt env ~kind:ErlangExpression instructions in
-    let exit_failure = Node.make_nop env in
-    {start= exit_success; exit_success; exit_failure}
-
-
-  let make_load env id e typ =
-    let exit_success = Node.make_load env id e typ in
-    let exit_failure = Node.make_nop env in
-    {start= exit_success; exit_success; exit_failure}
-
-
-  (** Make a branch based on the condition: go to success if true, go to failure if false *)
-  let make_branch env condition =
-    let start = Node.make_nop env in
-    let exit_success = Node.make_if env true condition in
-    let exit_failure = Node.make_if env false condition in
-    start |~~> [exit_success; exit_failure] ;
-    {start; exit_success; exit_failure}
-end
 
 let has_type (env : (_, _) Env.t) ~result ~value (name : ErlangTypeName.t) : Sil.instr =
   let fun_exp : Exp.t = Const (Cfun BuiltinDecl.__instanceof) in
