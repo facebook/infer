54a1ce498464936940622f7e9a6640b3b35ba2f0
Sofia Reis <sofiaoreis@fb.com>
2021-09-20 08:02:19 -0700

    [tests] c++ tests for cost analysis (data structures)
    Summary: Adding new tests for cost analysis (data structures). Tests for `array`, `list` and `vector`.
    
    Reviewed By: ezgicicek, skcho
    
    Differential Revision: D30930275
    
    fbshipit-source-id: ecee31ac3

---
+++ infer/tests/codetoanalyze/cpp/performance/list.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <list>
+#include <string>
+
+void iterate_over_list_linear_FN(std::list<int> mlist) {
+  std::list<int>::iterator it;
+  for (it = mlist.begin(); it != mlist.end(); it++) {
+  }
+}
+
+void iterate_over_list_for_linear_FN(std::list<int> mlist) {
+  for (const int& el : mlist) {
+  }
+}
+
+void iterate_over_local_list_linear_FN(std::list<int> mlist) {
+  std::list<int> local_list = mlist;
+  std::list<int>::iterator it;
+  for (it = local_list.begin(); it != local_list.end(); it++) {
+  }
+}
+
+void list_push_back_constant() {
+  std::list<int> list;
+  list.push_back(42);
+}
+
+void list_insert_constant() {
+  std::list<int> list;
+  std::list<int>::iterator it;
+  it = list.begin();
+  list.insert(it, 42);
+}
+
+void list_add_in_loop_constant_FP() {
+  std::list<int> list;
+  for (int i = 0; i < 10; ++i) {
+    list.push_back(i);
+  }
+  for (int i = 0, size = list.size(); i < size; ++i) {
+  }
+}
+
+void list_add_in_nested_loop_constant_FP() {
+  for (int j = 0; j < 10; j++) {
+    list_add_in_loop_constant_FP();
+  }
+}
+
+void list_push_back_then_loop_constant_FP() {
+  std::list<int> list;
+  list.push_back(0);
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  list.push_back(4);
+  list.push_back(5);
+
+  for (int i = 0, size = list.size(); i < size; ++i) {
+  }
+}
+
+void list_get_underrun_constant() {
+  std::list<int> list;
+  auto l_front = list.begin();
+  std::advance(l_front, 0);
+}
+
+void list_get_overrun_constant() {
+  std::list<int> list;
+  list.push_back(0);
+  auto l_front = list.begin();
+  std::advance(l_front, 2);
+}
+
+void list_get_constant_FP() {
+  std::list<int> list;
+  list.push_back(0);
+  list.push_back(1);
+  list.push_back(2);
+  auto l_front = list.begin();
+
+  for (int i = 0, size = list.size(); i < size; ++i) {
+    std::advance(l_front, i);
+  }
+}
+
+void sort_list_int_nlogn_FN(std::list<int> mylist) { mylist.sort(); }
+
+void sort_list_string_constant() {
+  std::list<std::string> mylist;
+
+  mylist.push_back("one");
+  mylist.push_back("two");
+  mylist.push_back("Three");
+
+  mylist.sort();
+}
+
+bool compare_nocase_linear(const std::string& first,
+                           const std::string& second) {
+  unsigned int i = 0;
+  while ((i < first.length()) && (i < second.length())) {
+    if (tolower(first[i]) < tolower(second[i]))
+      return true;
+    else if (tolower(first[i]) > tolower(second[i]))
+      return false;
+    ++i;
+  }
+  return (first.length() < second.length());
+}
+
+void sort_list_string_no_case_constant() {
+  std::list<std::string> mylist;
+
+  mylist.push_back("one");
+  mylist.push_back("two");
+  mylist.push_back("Three");
+
+  mylist.sort(compare_nocase_linear);
+}
+
+void sort_list_string_no_case_nlogn_FN(std::list<std::string> list2) {
+  std::list<std::string> mylist;
+
+  for (std::string x : list2) {
+    mylist.push_back(x);
+  }
+
+  mylist.sort(compare_nocase_linear);
+}
+
+std::list<int> remove_duplicates_quadratic_FN(std::list<int>& list1) {
+  std::list<int> list2;
+
+  for (int x : list1) {
+    bool found = false;
+    for (int y : list2)
+      if (x == y)
+        found = true;
+    if (!found)
+      list2.push_back(x);
+  }
+
+  return list2;
+}
---
+++ infer/tests/codetoanalyze/cpp/performance/vector.cpp
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+bool binary_search_log_FN(std::vector<std::string>& vec) {
+  return std::binary_search(vec.begin(), vec.end(), "x");
+}
+
+void copy_iterative_method_linear_FN(std::vector<std::string>& vec1) {
+  std::vector<std::string> vec2;
+  for (int i = 0; i < vec2.size(); i++)
+    vec2.push_back(vec1[i]);
+}
+
+void copy_iterative_method_constant(std::vector<std::string>& vec1) {
+  std::vector<std::string> vec2;
+  for (int i = 0; i < 4; i++)
+    vec2.push_back(vec1[i]);
+}
+
+void copy_assignment_operator_linear_FN(std::vector<std::string>& vec1) {
+  std::vector<std::string> vec2;
+  vec2 = vec1;
+}
+
+void copy_contructor_linear_FN(std::vector<std::string>& vec1) {
+  std::vector<std::string> vec2(vec1);
+}
+
+void copy_inbuild_func_linear_FN(std::vector<std::string>& vec1) {
+  std::vector<std::string> vec2;
+  std::copy(vec1.begin(), vec1.end(), back_inserter(vec2));
+}
+
+void copy_assign_linear_FN(std::vector<std::string>& vec1) {
+  std::vector<std::string> vec2;
+  vec2.assign(vec1.begin(), vec1.end());
+}
+
+void copy_n_linear_FN(std::vector<std::string>& vec1, int n) {
+  std::vector<std::string> vec2;
+  copy_n(vec1.begin(), n, vec2.begin());
+}
+
+void copy_n_constant(std::vector<std::string>& vec1, int n) {
+  std::vector<std::string> vec2;
+  copy_n(vec1.begin(), 4, vec2.begin());
+}
+
+void iterate_vector_linear(std::vector<std::string>& vec) {
+  for (int i = 0; i < vec.size(); i++) {
+  }
+}
+
+void iterate_two_vectors_linear(std::vector<std::string>& vec1,
+                                std::vector<std::string>& vec2) {
+  for (int i = 0; i < vec1.size(); i++) {
+  }
+
+  for (int i = 0; i < vec2.size(); i++) {
+  }
+}
+
+void iterate_vector_quadratic(std::vector<std::string>& vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    iterate_vector_linear(vec);
+  }
+}
+
+void iterate_vector_constant_times_linear(std::vector<std::string>& vec) {
+  for (int i = 0; i < 5; i++) {
+    iterate_vector_linear(vec);
+  }
+}
+
+void iterate_vector_auto_linear_FN(std::vector<int>& vec) {
+  for (auto& v : vec)
+    v *= v;
+}
+
+void sort_vector_nlogn_FN(std::vector<int>& vec) {
+  std::sort(vec.begin(), vec.end());
+}
+
+void insert_vector_constant(std::vector<int>& vec) {
+  std::vector<int>::iterator it;
+
+  it = vec.begin();
+  it = vec.insert(it, 100);
+  vec.insert(it, 1, 300);
+}
+
+void insert_new_vector_constant(std::vector<int>& vec) {
+  std::vector<int>::iterator it;
+
+  it = vec.begin();
+  it = vec.insert(it, 100);
+
+  std::vector<int> vec2(2, 400);
+  vec.insert(it + 1, vec2.begin(), vec2.end());
+}
+
+void insert_new_vector_loop_linear(std::vector<int>& vec, int n) {
+  std::vector<int>::iterator it;
+  it = vec.begin();
+
+  for (int i = 0; i < n; i++) {
+    vec.insert(it, n);
+  }
+}
+
+void insert_new_vector_loop_constant(std::vector<int>& vec) {
+  std::vector<int>::iterator it;
+  it = vec.begin();
+
+  vec.push_back(42);
+  vec.push_back(1337);
+  for (int i = 0; i < 5; i++) {
+    vec.insert(it, i);
+  }
+}
+
+std::vector<int> remove_duplicates_quadratic_FN(std::vector<int>& vec1) {
+  std::vector<int> vec2;
+
+  for (int x : vec1) {
+    bool found = false;
+    for (int y : vec2)
+      if (x == y)
+        found = true;
+    if (!found)
+      vec2.push_back(x);
+  }
+
+  return vec2;
+}
