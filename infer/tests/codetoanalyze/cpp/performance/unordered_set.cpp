31004e2db5a4d55d6642cfd5c3453673a3451d49
Sofia Reis <sofiaoreis@fb.com>
2021-09-23 09:34:40 -0700

    [cost] tests for c++ array and unordered_set containers
    Summary: This diff adds several tests for the c++ `array` and `unordered_set` containers.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D31088761
    
    fbshipit-source-id: f29544279

---
+++ infer/tests/codetoanalyze/cpp/performance/unordered_set.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#include <unordered_set>
+#include <iostream>
+#include <string>
+
+// worst case is quadratic
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/unordered_set/
+// Expected: O(us1^2); got constant
+void create_uset_range_quadratic_FN(std::unordered_set<std::string> us1) {
+  std::unordered_set<std::string> us2(us1.begin(), us1.end());
+}
+
+// worst case is quadratic
+// Expected: O(us1^2); got constant
+void create_uset_copy_quadratic_FN(std::unordered_set<std::string> us1) {
+  std::unordered_set<std::string> us2(us1);
+}
+
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/operator=/
+// Expected: O(s1); got constant
+void copy_uset_linear_FN(std::unordered_set<std::string> us1,
+                         std::unordered_set<std::string> us2) {
+  us2 = us1;
+}
+
+// Expected: O(uset); got constant
+void iterate_over_uset_linear_FN(std::unordered_set<std::string> uset) {
+  for (auto it = uset.begin(); it != uset.end(); ++it) {
+  }
+}
+
+// Expected: O(uset); got top
+void for_loop_over_uset_linear_FP(std::unordered_set<std::string> uset) {
+  for (int i = 0; i < uset.size(); i++) {
+  }
+}
+
+// Expected: O(uset); got constant
+void range_based_loop_over_uset_linear_FN(
+    std::unordered_set<std::string> uset) {
+  for (auto i : uset) {
+  }
+}
+
+// Expected: O(bucket(i).size); got constant
+void iterate_over_bucket_linear_FN(std::unordered_set<std::string> uset,
+                                   unsigned i) {
+  for (auto it = uset.begin(i); it != uset.end(i); ++it) {
+  }
+}
+
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/bucket_count/
+// Expected: O(uset.bucket_count() * bucket(i).size); got top
+void loop_over_buckets_count_FP(std::unordered_set<std::string> uset) {
+  for (unsigned i = 0; i < uset.bucket_count(); ++i) {
+    iterate_over_bucket_linear_FN(uset, i);
+  }
+}
+
+// count is linear
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/count/
+// Expected: O(uset); got constant
+bool find_str_in_uset_linear_FN(std::unordered_set<std::string> uset,
+                                std::string str) {
+  return uset.count(str) > 0;
+}
+
+// emplace is linear
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/count/
+// Expected: O(uset); got constant
+void emplace_str_in_uset_linear_FN(std::unordered_set<std::string> uset,
+                                   std::string str) {
+  uset.emplace(str);
+}
+
+// erase is linear
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/count/
+// Expected: O(uset); got constant
+void erase_from_uset_linear_FN(std::unordered_set<std::string> uset,
+                               std::string str) {
+  uset.erase(str);
+}
+
+// find is linear
+// https://www.cplusplus.com/reference/unordered_set/unordered_set/count/
+// Expected: O(uset); got constant
+void find_in_uset_linear_FN(std::unordered_set<std::string> uset,
+                            std::string str) {
+  uset.find(str);
+}
