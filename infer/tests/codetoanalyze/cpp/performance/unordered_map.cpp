23a915ebc1a5cb43797b222242444868e6cfb18c
Sofia Reis <sofiaoreis@fb.com>
2021-09-24 08:17:33 -0700

    [cost] tests for c++ unordered_map container
    Summary: This diff adds several tests for the `unordered_map` container.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D31169107
    
    fbshipit-source-id: 6ccf149ea

---
+++ infer/tests/codetoanalyze/cpp/performance/unordered_map.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#include <unordered_map>
+#include <iostream>
+#include <string>
+
+// Expected: O(m); got constant
+void iterate_over_map_linear_FN(
+    std::unordered_map<std::string, std::string> m) {
+  for (auto it = m.begin(); it != m.end(); ++it) {
+  }
+}
+
+// Expected: O(m); got top
+void for_loop_over_map_linear_FP(
+    std::unordered_map<std::string, std::string> m) {
+  for (int i = 0; i < m.size(); i++) {
+  }
+}
+
+// Expected: O(m); got constant
+void range_based_loop_over_map_linear_FN(
+    std::unordered_map<std::string, std::string> m) {
+  for (auto i : m) {
+  }
+}
+
+// Expected: O(bucket(i).size); got constant
+void iterate_over_bucket_linear_FN(
+    std::unordered_map<std::string, std::string> m, unsigned i) {
+  for (auto it = m.begin(i); it != m.end(i); ++it) {
+  }
+}
+
+// at is linear
+// https://www.cplusplus.com/reference/unordered_map/unordered_map/at/
+// Expected: O(m); got constant
+void add_element_to_map_at_linear_FN(std::unordered_map<std::string, int> m,
+                                     int n,
+                                     std::string str) {
+  m.at(str) = n;
+}
+
+// operator[] is linear
+// https://www.cplusplus.com/reference/unordered_map/unordered_map/operator[]/
+// Expected: O(m); got constant
+void add_element_to_map_operator_linear_FN(
+    std::unordered_map<std::string, int> m, int n, std::string str) {
+  m[str] = n;
+}
+
+// insert is linear
+// https://www.cplusplus.com/reference/unordered_map/unordered_map/insert/
+// Expected: O(m); got constant
+void add_element_to_map_insert_linear_FN(std::unordered_map<std::string, int> m,
+                                         std::pair<std::string, int> el) {
+  m.insert(el);
+}
+
+// emplace is linear
+// https://www.cplusplus.com/reference/unordered_map/unordered_map/emplace/
+// Expected: O(m); got constant
+void emplace_str_in_map_linear_FN(
+    std::unordered_map<std::string, std::string> m,
+    std::string str1,
+    std::string str2) {
+  m.emplace(str1, str2);
+}
+
+// erase is linear
+// https://www.cplusplus.com/reference/unordered_map/unordered_map/erase/
+// Expected: O(m); got constant
+void erase_find_from_map_linear_FN(
+    std::unordered_map<std::string, std::string> m, std::string str) {
+  m.erase(m.find(str), m.end());
+}
