2740e546e60ac317bb8460d5fd7379c989a5f752
Dulma Churchill <dulmarod@fb.com>
2021-11-09 03:13:37 -0800

    [paramNotNull] Adding traces to the Parameter Not null checked checker
    Summary: Added traces, showing the steps where we store, check for nil and execute the block, also starting with showing that it's a parameter of the method. I aimed to make the trace items similar to those of Pulse.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D32174618
    
    fbshipit-source-id: bc50e4f88

--- infer/src/checkers/ParameterNotNullChecked.ml
+++ infer/src/checkers/ParameterNotNullChecked.ml
@@ -29,40 +29,56 @@ module Vars = struct
   let compare = compare DomainData.compare
 end
 
-module Mem = struct
-  type t = {vars: Vars.t; blockParams: BlockParams.t} [@@deriving compare]
+module TraceData = struct
+  type data_usage = Assign | CheckNil | Execute | Parameter of Procname.t [@@deriving compare]
 
-  let pp fmt {vars; blockParams} = F.fprintf fmt "%a@.%a" Vars.pp vars BlockParams.pp blockParams
+  type t = {arg: Mangled.t; loc: Location.t; usage: data_usage} [@@deriving compare]
+
+  let to_string arg usage =
+    match usage with
+    | Assign ->
+        "Assigned"
+    | CheckNil ->
+        F.asprintf "Checking `%a` for nil" Mangled.pp arg
+    | Execute ->
+        F.asprintf "Executing `%a`" Mangled.pp arg
+    | Parameter proc_name ->
+        F.asprintf "Parameter `%a` of %a" Mangled.pp arg Procname.pp proc_name
+
+
+  let pp fmt {arg; loc; usage} =
+    F.fprintf fmt "%a at %a, usage is %s" Mangled.pp arg Location.pp loc (to_string arg usage)
+end
+
+module TraceInfo = struct
+  type t = TraceData.t list [@@deriving compare]
+
+  let pp = Pp.semicolon_seq TraceData.pp
+end
+
+module Mem = struct
+  type t = {vars: Vars.t; blockParams: BlockParams.t; traceInfo: TraceInfo.t [@compare.ignore]}
+  [@@deriving compare]
+
+  let pp fmt {vars; blockParams; traceInfo} =
+    F.fprintf fmt "Vars= %a@.BlockParams= %a@.TraceInfo= %a" Vars.pp vars BlockParams.pp blockParams
+      TraceInfo.pp traceInfo
+
 
   let find_block_param id (astate : t) = Vars.find_opt id astate.vars
 
-  let exec_null_check_id id ({vars; blockParams} as astate : t) =
+  let exec_null_check_id id loc ({vars; blockParams; traceInfo} as astate : t) =
     match find_block_param id astate with
-    | Some elem ->
-        let blockParams = BlockParams.add elem.arg blockParams in
-        {vars; blockParams}
+    | Some {arg} ->
+        let traceInfo = {TraceData.arg; loc; usage= CheckNil} :: traceInfo in
+        let blockParams = BlockParams.add arg blockParams in
+        let vars = Vars.add id {arg} vars in
+        {vars; blockParams; traceInfo}
     | None ->
         astate
 
 
-  let report_unchecked_block_param_issues proc_desc err_log var loc ({blockParams} as astate : t) =
-    match find_block_param var astate with
-    | Some {DomainData.arg} ->
-        if BlockParams.mem arg blockParams then ()
-        else
-          let message =
-            F.asprintf
-              "The block `%a` is executed without a check for null at %a. This could cause a crash."
-              Mangled.pp arg Location.pp loc
-          in
-          Reporting.log_issue proc_desc err_log ~loc ParameterNotNullChecked
-            IssueType.block_parameter_not_null_checked message ;
-          ()
-    | None ->
-        ()
-
-
-  let load attributes id pvar astate =
+  let load attributes id pvar _ astate =
     let name = Pvar.get_name pvar in
     let vars =
       if is_block_param attributes name then Vars.add id {arg= name} astate.vars else astate.vars
@@ -70,9 +86,10 @@ module Mem = struct
     {astate with vars}
 
 
-  let store pvar e astate =
+  let store pvar e loc ({vars; blockParams; traceInfo} as astate) =
     let name = Pvar.get_name pvar in
-    let blockParams = BlockParams.remove name astate.blockParams in
+    let traceInfo = {TraceData.arg= name; loc; usage= Assign} :: traceInfo in
+    let blockParams = BlockParams.remove name blockParams in
     let blockParams =
       match e with
       | Exp.Var id -> (
@@ -85,21 +102,52 @@ module Mem = struct
       | _ ->
           blockParams
     in
-    {astate with blockParams}
+    {vars; blockParams; traceInfo}
+
+
+  let call_trace name loc traceInfo = {TraceData.arg= name; loc; usage= Execute} :: traceInfo
+
+  let make_trace var traceInfo =
+    List.fold_left
+      ~f:(fun trace_elems {TraceData.arg; loc; usage} ->
+        if Mangled.equal var arg then
+          let trace_elem = Errlog.make_trace_element 0 loc (TraceData.to_string arg usage) [] in
+          trace_elem :: trace_elems
+        else trace_elems )
+      ~init:[] traceInfo
+
+
+  let report_unchecked_block_param_issues proc_desc err_log var loc
+      ({blockParams; traceInfo} as astate : t) =
+    match find_block_param var astate with
+    | Some {DomainData.arg} ->
+        let traceInfo = call_trace arg loc traceInfo in
+        if BlockParams.mem arg blockParams then ()
+        else
+          let ltr = make_trace arg traceInfo in
+          let message =
+            F.asprintf
+              "The block `%a` is executed without a check for nil at %a. This could cause a crash."
+              Mangled.pp arg Location.pp loc
+          in
+          Reporting.log_issue proc_desc err_log ~ltr ~loc ParameterNotNullChecked
+            IssueType.block_parameter_not_null_checked message ;
+          ()
+    | None ->
+        ()
 end
 
 module Domain = struct
   include AbstractDomain.FiniteSet (Mem)
 
-  let exec_null_check_id id astate = map (Mem.exec_null_check_id id) astate
+  let exec_null_check_id id loc astate = map (Mem.exec_null_check_id id loc) astate
+
+  let load attributes id pvar loc astate = map (Mem.load attributes id pvar loc) astate
+
+  let store pvar e loc astate = map (Mem.store pvar e loc) astate
 
   let report_unchecked_block_param_issues proc_desc err_log var loc astate =
     iter (Mem.report_unchecked_block_param_issues proc_desc err_log var loc) astate
-
-
-  let load attributes id pvar astate = map (Mem.load attributes id pvar) astate
-
-  let store pvar e astate = map (Mem.store pvar e) astate
 end
 
 module TransferFunctions = struct
@@ -114,21 +162,21 @@ module TransferFunctions = struct
       (instr : Sil.instr) =
     let attributes = Procdesc.get_attributes proc_desc in
     match instr with
-    | Load {id; e= Lvar pvar} ->
-        Domain.load attributes id pvar astate
-    | Store {e1= Lvar pvar; e2} ->
-        Domain.store pvar e2 astate
-    | Prune (Var id, _, _, _) ->
-        Domain.exec_null_check_id id astate
+    | Load {id; e= Lvar pvar; loc} ->
+        Domain.load attributes id pvar loc astate
+    | Store {e1= Lvar pvar; e2; loc} ->
+        Domain.store pvar e2 loc astate
+    | Prune (Var id, loc, _, _) ->
+        Domain.exec_null_check_id id loc astate
     (* If (block != nil) or equivalent else branch *)
-    | Prune (BinOp (Binop.Ne, e1, e2), _, _, _)
+    | Prune (BinOp (Binop.Ne, e1, e2), loc, _, _)
     (* If (!(block == nil)) or equivalent else branch *)
-    | Prune (UnOp (LNot, BinOp (Binop.Eq, e1, e2), _), _, _, _) -> (
+    | Prune (UnOp (LNot, BinOp (Binop.Eq, e1, e2), _), loc, _, _) -> (
       match (e1, e2) with
       | Var id, e when Exp.is_null_literal e ->
-          Domain.exec_null_check_id id astate
+          Domain.exec_null_check_id id loc astate
       | e, Var id when Exp.is_null_literal e ->
-          Domain.exec_null_check_id id astate
+          Domain.exec_null_check_id id loc astate
       | _ ->
           astate )
     | Call (_, Exp.Var var, _, loc, call_flags) when call_flags.CallFlags.cf_is_objc_block ->
@@ -155,8 +203,23 @@ let init_block_params attributes initBlockParams =
       initBlockParams
 
 
+let init_block_param_trace_info attributes traceInfo =
+  let formals = attributes.ProcAttributes.formals in
+  let add_trace_info_block traceInfo (formal, _) =
+    if is_block_param attributes formal then
+      let usage = TraceData.Parameter attributes.ProcAttributes.proc_name in
+      {TraceData.arg= formal; loc= attributes.ProcAttributes.loc; usage} :: traceInfo
+    else traceInfo
+  in
+  List.fold formals ~init:traceInfo ~f:add_trace_info_block
+
+
 let checker ({IntraproceduralAnalysis.proc_desc} as analysis_data) =
   let attributes = Procdesc.get_attributes proc_desc in
   let initial_blockParams = init_block_params attributes BlockParams.empty in
-  let initial = Domain.singleton {Mem.vars= Vars.empty; blockParams= initial_blockParams} in
+  let initTraceInfo = init_block_param_trace_info attributes [] in
+  let initial =
+    Domain.singleton
+      {Mem.vars= Vars.empty; blockParams= initial_blockParams; traceInfo= initTraceInfo}
+  in
   ignore (Analyzer.compute_post analysis_data ~initial proc_desc)
