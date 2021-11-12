dc1dda3447bf1f0c18e72aa216e3a35e39fda678
Dulma Churchill <dulmarod@fb.com>
2021-11-12 05:42:22 -0800

    [tests] [19/n] Improved a test about retain cycles and moved it to own folder
    Summary: Improved the names in the test and removed unused parts.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D32354612
    
    fbshipit-source-id: 2776f6e49

---
+++ infer/tests/codetoanalyze/objc/biabduction/retain_cycles/RetainCycleStaticVar.m
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <Foundation/Foundation.h>
+
+typedef void (^MyHandler)(NSString* name);
+
+@interface SharedView : NSObject
+
+@property(nonatomic, strong) MyHandler handler;
+@property(nonatomic, strong) NSString* name;
+@property(nonatomic, strong) SharedView* view;
+
+@end
+
+@implementation SharedView
+
+- (void)sharedInstanceRetainCycleBad {
+  static dispatch_once_t once;
+  static SharedView* sharedInstance;
+  dispatch_once(&once, ^{
+    sharedInstance = [[SharedView alloc] init];
+  });
+
+  _view = sharedInstance;
+  sharedInstance.handler = ^(NSString* s) {
+    _name = sharedInstance.name;
+  };
+}
+
+@end
