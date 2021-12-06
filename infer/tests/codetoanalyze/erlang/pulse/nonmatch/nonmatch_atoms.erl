502447f96d892302b8c84bbc29573a71c7d9b9f0
Akos Hajdu <akoshajdu@fb.com>
2021-12-06 06:21:55 -0800

    [erl-frontend] Add tests for matching on special true/false atoms
    Summary: Just some new tests to make sure we handle true/false properly in pattern matching.
    
    Reviewed By: mmarescotti
    
    Differential Revision: D32828068
    
    fbshipit-source-id: 831f5045a2

--- infer/tests/codetoanalyze/erlang/pulse/nonmatch/nonmatch_atoms.erl
+++ infer/tests/codetoanalyze/erlang/pulse/nonmatch/nonmatch_atoms.erl
@@ -8,7 +8,11 @@
 -export([
     test_match1_Ok/0,
     test_match2_Ok/0,
-    test_match3_Bad/0
+    test_match3_Bad/0,
+    test_match4_Ok/0,
+    test_match5_Ok/0,
+    test_match6_Bad/0,
+    test_match7_Bad/0
 ]).
 
 matches_ok(ok) -> ok.
@@ -18,3 +22,13 @@ test_match1_Ok() -> matches_ok(ok).
 test_match2_Ok() -> matches_ok('ok').
 
 test_match3_Bad() -> matches_ok(not_ok).
+
+matches_true(true) -> ok.
+
+test_match4_Ok() -> matches_true(true).
+
+test_match5_Ok() -> matches_true(1 == 1).
+
+test_match6_Bad() -> matches_true(false).
+
+test_match7_Bad() -> matches_true(1 == 0).
