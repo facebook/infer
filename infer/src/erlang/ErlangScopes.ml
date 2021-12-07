afedeb646299019958a515057b63b9b7e51c49d9
Akos Hajdu <akoshajdu@fb.com>
2021-12-07 10:39:35 -0800

    [erl-frontend] Make lambda names deterministic
    Summary: Previously we used a sequential counter (across all modules) for generating lambda names, but that was not deterministic due to different ordering for processing files. Now we use a counter per function which should be deterministic.
    
    Reviewed By: mmarescotti, jvillard
    
    Differential Revision: D32881809
    
    fbshipit-source-id: 7e3d1a2302

--- infer/src/erlang/ErlangScopes.ml
+++ infer/src/erlang/ErlangScopes.ml
@@ -13,6 +13,11 @@ module L = Logging
 (* One frame in the stack of scopes *)
 type scope = {procname: Procname.t; locals: String.Set.t; captured: Pvar.Set.t}
 
+(* Data structure we pass around for giving unique names to lambdas (per function).
+   Initial plan was to use line+column, but if a macro expands to multiple lambdas,
+   that is not unique. *)
+type lambda_name_counter = {funcname: string; mutable counter: int}
+
 let lookup_var (scopes : scope list) (vname : string) : Procname.t option =
   List.find_map scopes ~f:(fun {procname; locals} ->
       if String.Set.mem locals vname then Some procname else None )
@@ -37,76 +42,75 @@ let assert_empty scopes =
   if not (List.is_empty scopes) then L.die InternalError "Expected empty stack of scopes."
 
 
-let create_fresh_lambda_name =
-  let last = ref 0 in
-  fun () ->
-    incr last ;
-    Printf.sprintf "anon_fun_%d" !last
+let create_unique_lambda_name (lambda_cntr : lambda_name_counter) =
+  let name = Printf.sprintf "anon_fun_%d_of_%s" lambda_cntr.counter lambda_cntr.funcname in
+  lambda_cntr.counter <- lambda_cntr.counter + 1 ;
+  name
 
 
-let rec annotate_expression (env : (_, _) Env.t) (scopes : scope list)
+let rec annotate_expression (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
     ({simple_expression; _} : Ast.expression) =
   (* Most constructs just do the traversal recursively.
      We only have to treat lambdas and variables in a special way. *)
   match simple_expression with
   | BinaryOperator (e1, _, e2) ->
-      annotate_expression_list env scopes [e1; e2]
+      annotate_expression_list env lambda_cntr scopes [e1; e2]
   | BitstringConstructor elems ->
       List.fold_left
         ~f:(fun scopes (e : Ast.bin_element) ->
-          let scopes = annotate_expression env scopes e.expression in
-          annotate_expression_opt env scopes e.size )
+          let scopes = annotate_expression env lambda_cntr scopes e.expression in
+          annotate_expression_opt env lambda_cntr scopes e.size )
         ~init:scopes elems
   | Block body ->
-      annotate_expression_list env scopes body
+      annotate_expression_list env lambda_cntr scopes body
   | Call {module_; function_; args} ->
-      let scopes = annotate_expression_opt env scopes module_ in
-      let scopes = annotate_expression env scopes function_ in
-      annotate_expression_list env scopes args
+      let scopes = annotate_expression_opt env lambda_cntr scopes module_ in
+      let scopes = annotate_expression env lambda_cntr scopes function_ in
+      annotate_expression_list env lambda_cntr scopes args
   | Case {expression; cases} ->
-      let scopes = annotate_expression env scopes expression in
-      annotate_clauses env scopes cases
+      let scopes = annotate_expression env lambda_cntr scopes expression in
+      annotate_clauses env lambda_cntr scopes cases
   | Catch expression ->
-      annotate_expression env scopes expression
+      annotate_expression env lambda_cntr scopes expression
   | Cons {head; tail} ->
-      annotate_expression_list env scopes [head; tail]
+      annotate_expression_list env lambda_cntr scopes [head; tail]
   | If clauses ->
-      annotate_clauses env scopes clauses
+      annotate_clauses env lambda_cntr scopes clauses
   | ListComprehension {expression; qualifiers} | BitstringComprehension {expression; qualifiers} ->
       (* TODO: support local variables in list comprehensions: T105967634 *)
-      let scopes = annotate_expression env scopes expression in
-      List.fold_left ~f:(annotate_qualifier env) ~init:scopes qualifiers
+      let scopes = annotate_expression env lambda_cntr scopes expression in
+      List.fold_left ~f:(annotate_qualifier env lambda_cntr) ~init:scopes qualifiers
   | Literal _ | Nil | RecordIndex _ | Fun _ ->
       scopes
   | Map {map; updates} ->
-      let scopes = annotate_expression_opt env scopes map in
-      List.fold_left ~f:(annotate_association env) ~init:scopes updates
+      let scopes = annotate_expression_opt env lambda_cntr scopes map in
+      List.fold_left ~f:(annotate_association env lambda_cntr) ~init:scopes updates
   | Match {pattern; body} ->
-      annotate_expression_list env scopes [pattern; body]
+      annotate_expression_list env lambda_cntr scopes [pattern; body]
   | Receive {cases; timeout} ->
       (* Process clauses and timeout independently and then merge as
          if the timeout was also just one of the clauses. *)
-      let clause_scopes = List.map ~f:(annotate_one_clause env scopes) cases in
+      let clause_scopes = List.map ~f:(annotate_one_clause env lambda_cntr scopes) cases in
       let timeout_scopes =
         match timeout with
         | None ->
             []
         | Some t ->
-            let scopes = annotate_expression env scopes t.time in
-            [annotate_expression_list env scopes t.handler]
+            let scopes = annotate_expression env lambda_cntr scopes t.time in
+            [annotate_expression_list env lambda_cntr scopes t.handler]
       in
       merge_scopes ~into:scopes (timeout_scopes @ clause_scopes)
   | RecordAccess {record; _} ->
-      annotate_expression env scopes record
+      annotate_expression env lambda_cntr scopes record
   | RecordUpdate {record; updates; _} ->
-      let scopes = annotate_expression_opt env scopes record in
-      List.fold_left ~f:(annotate_record_update env) ~init:scopes updates
+      let scopes = annotate_expression_opt env lambda_cntr scopes record in
+      List.fold_left ~f:(annotate_record_update env lambda_cntr) ~init:scopes updates
   | TryCatch {body; ok_cases; catch_cases; after} ->
       (* OK cases can see variables from the body, but otherwise everything is independent *)
-      let body_scopes = annotate_expression_list env scopes body in
-      let ok_scopes = annotate_clauses env body_scopes ok_cases in
-      let catch_scopes = annotate_catch_clauses env scopes catch_cases in
-      let after_scopes = annotate_expression_list env scopes after in
+      let body_scopes = annotate_expression_list env lambda_cntr scopes body in
+      let ok_scopes = annotate_clauses env lambda_cntr body_scopes ok_cases in
+      let catch_scopes = annotate_catch_clauses env lambda_cntr scopes catch_cases in
+      let after_scopes = annotate_expression_list env lambda_cntr scopes after in
       (* All variables are unsafe outside of try, so we just merge the captured vars *)
       let hd, tl = pop_scope scopes in
       let captured = hd.captured in
@@ -115,9 +119,9 @@ let rec annotate_expression (env : (_, _) Env.t) (scopes : scope list)
       let captured = Pvar.Set.union captured (top_scope after_scopes).captured in
       {hd with captured} :: tl
   | Tuple exprs ->
-      annotate_expression_list env scopes exprs
+      annotate_expression_list env lambda_cntr scopes exprs
   | UnaryOperator (_, e) ->
-      annotate_expression env scopes e
+      annotate_expression env lambda_cntr scopes e
   | Lambda lambda ->
       let arity =
         match lambda.cases with
@@ -127,11 +131,11 @@ let rec annotate_expression (env : (_, _) Env.t) (scopes : scope list)
             L.die InternalError "Lambda has no clauses, cannot determine arity"
       in
       (* A fresh id is created even for named lambdas. *)
-      let function_name = create_fresh_lambda_name () in
+      let function_name = create_unique_lambda_name lambda_cntr in
       let procname = Procname.make_erlang ~module_name:env.current_module ~function_name ~arity in
       lambda.procname <- Some procname ;
       let scopes = push_scope scopes procname in
-      let scopes = annotate_clauses env scopes lambda.cases in
+      let scopes = annotate_clauses env lambda_cntr scopes lambda.cases in
       let popped, scopes = pop_scope scopes in
       lambda.captured <- Some popped.captured ;
       scopes
@@ -157,45 +161,51 @@ let rec annotate_expression (env : (_, _) Env.t) (scopes : scope list)
         L.die InternalError "No scope found during variable annotation." )
 
 
-and annotate_expression_opt (env : (_, _) Env.t) (scopes : scope list)
+and annotate_expression_opt (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
     (maybe_expr : Ast.expression option) =
-  match maybe_expr with Some expr -> annotate_expression env scopes expr | None -> scopes
+  match maybe_expr with
+  | Some expr ->
+      annotate_expression env lambda_cntr scopes expr
+  | None ->
+      scopes
 
 
-and annotate_expression_list (env : (_, _) Env.t) (scopes : scope list) (exprs : Ast.expression list)
-    =
-  List.fold_left ~f:(annotate_expression env) ~init:scopes exprs
+and annotate_expression_list (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
+    (exprs : Ast.expression list) =
+  List.fold_left ~f:(annotate_expression env lambda_cntr) ~init:scopes exprs
 
 
-and annotate_qualifier (env : (_, _) Env.t) (scopes : scope list) (q : Ast.qualifier) =
+and annotate_qualifier (env : (_, _) Env.t) lambda_cntr (scopes : scope list) (q : Ast.qualifier) =
   match q with
   | BitsGenerator {pattern; expression} ->
-      annotate_expression_list env scopes [pattern; expression]
+      annotate_expression_list env lambda_cntr scopes [pattern; expression]
   | Filter expression ->
-      annotate_expression env scopes expression
+      annotate_expression env lambda_cntr scopes expression
   | Generator {pattern; expression} ->
-      annotate_expression_list env scopes [pattern; expression]
+      annotate_expression_list env lambda_cntr scopes [pattern; expression]
 
 
-and annotate_association (env : (_, _) Env.t) (scopes : scope list)
+and annotate_association (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
     ({key; value; _} : Ast.association) =
-  annotate_expression_list env scopes [key; value]
+  annotate_expression_list env lambda_cntr scopes [key; value]
 
 
-and annotate_record_update (env : (_, _) Env.t) (scopes : scope list) (u : Ast.record_update) =
-  annotate_expression env scopes u.expression
+and annotate_record_update (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
+    (u : Ast.record_update) =
+  annotate_expression env lambda_cntr scopes u.expression
 
 
-and annotate_clauses (env : (_, _) Env.t) (scopes : scope list) (clauses : Ast.case_clause list) =
+and annotate_clauses (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
+    (clauses : Ast.case_clause list) =
   (* Process each clause independently *)
-  let subscopes = List.map ~f:(annotate_one_clause env scopes) clauses in
+  let subscopes = List.map ~f:(annotate_one_clause env lambda_cntr scopes) clauses in
   merge_scopes ~into:scopes subscopes
 
 
-and annotate_catch_clauses (env : (_, _) Env.t) (scopes : scope list)
+and annotate_catch_clauses (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
     (clauses : Ast.catch_clause list) =
   (* Process each clause independently *)
-  let subscopes = List.map ~f:(annotate_one_catch_clause env scopes) clauses in
+  let subscopes = List.map ~f:(annotate_one_catch_clause env lambda_cntr scopes) clauses in
   merge_scopes ~into:scopes subscopes
 
 
@@ -219,32 +229,38 @@ and merge_scopes ~into (scopes : scope list list) =
     :: tl_scopes
 
 
-and annotate_one_clause (env : (_, _) Env.t) (scopes : scope list) (clause : Ast.case_clause) =
-  let scopes = annotate_expression_list env scopes clause.patterns in
-  let scopes = List.fold_left ~f:(annotate_expression_list env) ~init:scopes clause.guards in
-  let scopes = annotate_expression_list env scopes clause.body in
+and annotate_one_clause (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
+    (clause : Ast.case_clause) =
+  let scopes = annotate_expression_list env lambda_cntr scopes clause.patterns in
+  let scopes =
+    List.fold_left ~f:(annotate_expression_list env lambda_cntr) ~init:scopes clause.guards
+  in
+  let scopes = annotate_expression_list env lambda_cntr scopes clause.body in
   scopes
 
 
-and annotate_one_catch_clause (env : (_, _) Env.t) (scopes : scope list) (clause : Ast.catch_clause)
-    =
+and annotate_one_catch_clause (env : (_, _) Env.t) lambda_cntr (scopes : scope list)
+    (clause : Ast.catch_clause) =
   let scopes =
     List.fold_left
-      ~f:(fun scopes (p : Ast.catch_pattern) -> annotate_expression env scopes p.pattern)
+      ~f:(fun scopes (p : Ast.catch_pattern) -> annotate_expression env lambda_cntr scopes p.pattern)
       ~init:scopes clause.patterns
   in
-  let scopes = List.fold_left ~f:(annotate_expression_list env) ~init:scopes clause.guards in
-  let scopes = annotate_expression_list env scopes clause.body in
+  let scopes =
+    List.fold_left ~f:(annotate_expression_list env lambda_cntr) ~init:scopes clause.guards
+  in
+  let scopes = annotate_expression_list env lambda_cntr scopes clause.body in
   scopes
 
 
 let annotate_one_function (env : (_, _) Env.t) function_ clauses =
   let _, name = Env.func_procname env function_ in
   let scopes : scope list = push_scope [] name in
+  let lambda_cntr = {funcname= Procname.to_string name; counter= 0} in
   (* Process each clause independently *)
   List.iter
     ~f:(fun clause ->
-      let scopes = annotate_one_clause env scopes clause in
+      let scopes = annotate_one_clause env lambda_cntr scopes clause in
       let _, scopes = pop_scope scopes in
       assert_empty scopes )
     clauses
