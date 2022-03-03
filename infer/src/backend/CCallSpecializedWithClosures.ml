33c9bf2c7519002c3585fc2fe319998f8cb8d5df
Corentin De Souza <fantasio@fb.com>
2022-03-03 09:16:24 -0800

    [objc][block] minor fix + improve comments
    Summary:
    The error message during Procdesc's `deep_copy_code_from_pdesc` was
    wrong. Comments about block specialization were not up to date after
    captured variables were not passed as arguments anymore
    
    Reviewed By: jvillard
    
    Differential Revision: D34241554
    
    fbshipit-source-id: 7f45d41ae4

--- infer/src/backend/CCallSpecializedWithClosures.ml
+++ infer/src/backend/CCallSpecializedWithClosures.ml
@@ -32,38 +32,44 @@ let replace_with_specialize_methods instr =
 
 let process proc_desc =
   (* For each procdesc:
-     1. If we are a specialized pdesc:
+     1. If we are a specialized procdesc:
       1.1. Copy original procdesc
-      1.2. Update blocks' uses (at instruction level)
-     2. Update calls' closure arguments (at expression level)
-     3. Update calls with closures as arguments to specialized calls
-     4. Create procdescs for specialized callees if they don't already exist
-     5. Update closure calls (at expression level)
+      1.2. Update blocks' uses
+     2. Update calls' closure arguments
+     3. Update calls with closures as arguments to specialized calls:
+      3.1. Create procdescs for specialized callees if they don't already exist
+     4. Update closure calls
   *)
-  (* Create a specialized copy of proc_desc's orig_pdesc (if it exists).
-     Specialize at instruction level: [foo(f)] when [f = block] replaces every
-     use of [f] (calls and as argument) with the corresponding [block] and its
-     captured arguments. *)
+  (* 1.
+      1.1. Create a specialized copy of [proc_desc]'s [orig_pdesc] (if it exists).
+      1.2. Specialize at instruction level: [foo(f)] when [f = block] replaces every
+           use of [f] (calls and as argument) with the corresponding [block] and its
+           captured variables. *)
   ClosureSubstSpecializedMethod.process proc_desc ;
-  (* Replace each indirect use of closure (as argument) with a direct use of closure.
-     e.g. [foo(a, b, c)] when b = Closure(closure_b, ...) becomes
-          [foo(a, Closure(closure_b, ...), c)] *)
+  (* 2.
+      Replace each indirect use of closure (as argument) with a direct use of closure.
+     e.g.
+      [foo(a, b, c)] when b = Closure(closure_b, ...) becomes
+      [foo(a, Closure(closure_b, ...), c)] *)
   ClosuresSubstitution.process_closure_param proc_desc ;
-  (* Replace every function call taking a known closure as argument with specialized calls
-     and create the corresponding (empty for now) specialized pdescs.
-     e.g. [foo(a, Closure(closure_b, captured_args...), c)] becomes
-      [foo\[closure_b\](a, captured_args..., Closure(closure_b, captured_args...), c)]
-      and the pdesc for [foo\[closure_b\]\ is created.
+  (* 3.
+      Replace every function call taking a known closure as argument with specialized calls
+      and (3.1.) create the corresponding (empty for now) specialized procdescs.
+     e.g.
+      [foo(a, Closure(closure_b, captured_vars...), c)] becomes
+      [foo\[closure_b\](a, Closure(closure_b, captured_vars...), c)]
+      and the procdesc for [foo\[closure_b\]\ is created.
      Note:
-      Captured args placement in the list of args depends on the set ordering).
-      The newly created pdescs (stored in [need_specalization]) will be filled out by
-      [ClosureSubstSpecializedMethod.process] above when it will be their turn to be
-      (pre)processed.
-  *)
+      The newly created procdescs will be filled out by [ClosureSubstSpecializedMethod.process]
+      in step 1. above when it will be their turn to be (pre)processed. *)
   Procdesc.replace_instrs proc_desc ~f:(fun _node instr -> replace_with_specialize_methods instr)
   |> ignore ;
-  (* Replace [ident] calls by [closure] calls with added [closure]'s arguments
-     if [ident = closure].
-     e.g. [foo(a, b, c)] when [foo = Closure(closure_foo, captured_args...)] becomes
-          [closure_foo(captured_args..., a, b, c)] *)
+  (* 4.
+      Replace [ident] calls by [closure] calls if [ident = closure] or if any of the arguments is
+      a known [closure].
+     e.g.
+      - [id(a, b, c)] when [id = Closure(closure_id, captured_vars...)] becomes
+        [Closure(closure_id, captured_vars...)(a, b, c)]
+      - [foo\[closure_b\](a, Closure(closure_b, captured_vars...), c)] becomes
+        [Closure(foo\[closure_b\], captured_vars...)(a, Closure(closure_b, captured_vars...), c)] *)
   ClosuresSubstitution.process_closure_call proc_desc
