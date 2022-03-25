7f97f3e671a2256e640a628e7e9811025d5e2665
Xiaoyu Liu <lixiaoyu@microsoft.com>
2022-03-25 21:45:24 +0000

    address comments

--- infer/src/dotnet/ResourceLeakCSDomain.ml
+++ infer/src/dotnet/ResourceLeakCSDomain.ml
@@ -8,12 +8,6 @@
 open! IStd
 module F = Format
 
-module LeakList = struct
-  include Base.List
-
-  let append_one leakList typeWithLeak = leakList @ [typeWithLeak]
-end
-
 (** Resource analysis in loop and branch*)
 module FiniteBounds = struct
   type t = int
@@ -44,13 +38,11 @@ module BoundsWithTop = struct
         Top
 end
 
-module ResourcesHeld = AbstractDomain.Map (AccessPath) (BoundsWithTop)
-module ResourcesTypeHeld = PrettyPrintable.MakePPMap (AccessPath)
+module TypeNameInfo = AbstractDomain.Flat (Typ.Name)
+module ResourceInfo = AbstractDomain.Pair (BoundsWithTop) (TypeNameInfo)
+module ResourcesHeld = AbstractDomain.Map (AccessPath) (ResourceInfo)
 
 open AbstractDomain.Types
-
-(** Initializes resources to type map *)
-let type_map = ref ResourcesTypeHeld.empty
 
 (** Initializes resources to count map *)
 let initial = ResourcesHeld.empty
@@ -66,94 +58,47 @@ let decr_count count = update_count count (-1)
 
 (** Checks the count of a specific resource *)
 let find_count access_path held =
-  match ResourcesHeld.find_opt access_path held with Some count -> count | None -> NonTop 0
-
-
-(** Checks the count of resources held if resource exists, otherwise returns false*)
-let check_count access_path held =
-  let old_count = find_count access_path held in
-  match old_count with NonTop count when count > 0 -> true | _ -> false
-
-let reset_type_map = type_map := ResourcesTypeHeld.empty
-
-(** Return leaked types as string*)
-let type_to_list held = 
-  let leaked_type_list = ref [] in
-  let concat_types = 
-    ResourcesTypeHeld.iter
-      (fun x y ->
-        if check_count x held then
-          leaked_type_list := LeakList.append_one !leaked_type_list y )
-      !type_map 
-  in
-  concat_types ;
-  let leaked_type_list_to_str = String.concat ~sep:", " !leaked_type_list in
-  leaked_type_list_to_str
+  match ResourcesHeld.find_opt access_path held with Some (count, _) -> count | None -> NonTop 0
 
 
 (** Adds resources acquired to records *)
-let acquire_resource access_path class_name held =
-  let add_resource_to_map =
-    match ResourcesHeld.find_opt access_path held with
-    | Some _ ->
-        ()
-    | None ->
-        type_map := ResourcesTypeHeld.add access_path class_name !type_map
-  in
-  add_resource_to_map ;
+let acquire_resource access_path type_name held =
   let old_count = find_count access_path held in
-  ResourcesHeld.add access_path (incr_count old_count) held
+  ResourcesHeld.add access_path (incr_count old_count, TypeNameInfo.v type_name) held
 
 
 (** Releases acquired resources from records when release function is called*)
 let release_resource access_path held =
-  let old_count = find_count access_path held in
-  let remove_resource_from_map =
-    match old_count with
-    | Top ->
-        type_map := ResourcesTypeHeld.remove access_path !type_map
-    | NonTop count when count <= 0 ->
-        type_map := ResourcesTypeHeld.remove access_path !type_map
-    | _ ->
-        ()
-  in
-  remove_resource_from_map ;
-  ResourcesHeld.add access_path (decr_count old_count) held
+  match ResourcesHeld.find_opt access_path held with 
+  | Some (count, type_name) -> 
+    ResourcesHeld.add access_path (decr_count count, type_name) held 
+  | None -> 
+    let object_type = Typ.Name.CSharp.from_string "" in
+    ResourcesHeld.add access_path (NonTop 0, TypeNameInfo.top) held
 
 
 (** Re-assigns resources when transferred to other objects*)
 let assign lhs_access_path rhs_access_path held =
-  let add_type_map search_access_path access_path =
-    match ResourcesTypeHeld.find_opt search_access_path !type_map with
-    | Some class_name ->
-        type_map := ResourcesTypeHeld.add access_path class_name !type_map ;
-        type_map := ResourcesTypeHeld.remove search_access_path !type_map
-    | None ->
-        ()
-  in
   let equal_base (base1, _) (base2, _) = AccessPath.equal_base base1 base2 in
-  let one_binding access_path count held =
+  let one_binding access_path (count, type_name) held =
     match
       AccessPath.replace_prefix ~prefix:rhs_access_path ~replace_with:access_path lhs_access_path
     with
     | Some base_access_path ->
-        add_type_map access_path base_access_path ;
-        ResourcesHeld.add base_access_path count held
+        ResourcesHeld.add base_access_path (count, type_name) held
     | None ->
         if AccessPath.equal rhs_access_path access_path then (
-          add_type_map access_path lhs_access_path ;
-          ResourcesHeld.add lhs_access_path count held )
+          ResourcesHeld.add lhs_access_path (count, type_name) held )
         else if equal_base rhs_access_path access_path then
           match
             AccessPath.replace_prefix ~prefix:rhs_access_path ~replace_with:lhs_access_path
               access_path
           with
           | Some new_access_path ->
-              add_type_map access_path new_access_path ;
-              ResourcesHeld.add new_access_path count held
+              ResourcesHeld.add new_access_path (count, type_name) held
           | None ->
-              ResourcesHeld.add access_path count held
-        else ResourcesHeld.add access_path count held
+              ResourcesHeld.add access_path (count, type_name) held
+        else ResourcesHeld.add access_path (count, type_name) held
   in
   ResourcesHeld.fold one_binding held ResourcesHeld.empty
 
@@ -161,7 +106,7 @@ let assign lhs_access_path rhs_access_path held =
 (** Checks if there is a resource leak*)
 let has_leak formal_map held =
   (* test if we acquired resources that we do not return to the caller *)
-  let is_local_leak access_path count =
+  let is_local_leak access_path (count, _) =
     let base, _ = access_path in
     match (count, base) with
     | Top, _ ->
@@ -205,15 +150,43 @@ module Summary = struct
   end
 
   module ResourcesFromFormals = PrettyPrintable.MakePPMap (InterfaceAccessPath)
-  module ResourcesFromFormalsType = PrettyPrintable.MakePPMap (InterfaceAccessPath)
 
-  let interface_type_map = ref ResourcesFromFormalsType.empty
+  type t = (BoundsWithTop.t * TypeNameInfo.t) ResourcesFromFormals.t
 
-  let reset_interface_type_map = interface_type_map := ResourcesFromFormalsType.empty
+  let pp = ResourcesFromFormals.pp ~pp_value:ResourceInfo.pp
 
-  type t = BoundsWithTop.t ResourcesFromFormals.t
-
-  let pp = ResourcesFromFormals.pp ~pp_value:BoundsWithTop.pp
+  (** Return leaked resources and types as string*)
+  let resource_and_type_to_str held debug = 
+    let check_count access_path held =
+      let old_count = 
+        match ResourcesHeld.find_opt access_path held with Some (count, _) -> count | None -> NonTop 0
+      in
+      match old_count with NonTop count when count > 0 -> true | _ -> false
+    in
+    let text = ref [] in
+    let concat_text =
+      ResourcesHeld.iter
+        (fun x (y, type_name) ->
+          if check_count x held && debug then
+            let type_name_str = TypeNameInfo.get type_name in
+            match type_name_str with
+            | Some type_str ->
+              text := (F.asprintf "Leaked resource %a of type %a" ResourcesHeld.pp_key x Typ.Name.pp_name_only type_str) :: !text
+            | _ ->
+              ()
+          else if check_count x held then
+            let type_name_str = TypeNameInfo.get type_name in
+            let temp_var_regexp = Re.Str.regexp " n\\$[0-9]+ " in
+            match type_name_str with
+            | Some type_str ->
+              text := Re.Str.global_replace temp_var_regexp "" (F.asprintf "Leaked resource %a of type %a" ResourcesHeld.pp_key x Typ.Name.pp_name_only type_str) :: !text
+            | _ ->
+              ()
+        )
+        held 
+    in
+    concat_text ;
+    String.concat ~sep:", " !text
 
   let make formal_map held =
     let to_interface access_path =
@@ -224,29 +197,18 @@ module Summary = struct
       | None ->
           if Var.is_return (fst base) then Some (InterfaceAccessPath.Return, accesses) else None
     in
-    let add_resource_to_hash interface_access_path class_name =
-      ResourcesFromFormalsType.add interface_access_path class_name !interface_type_map
-    in
-    let add_to_type_map search_access_path interface_access_path =
-      match ResourcesTypeHeld.find_opt search_access_path !type_map with
-      | Some class_name ->
-          interface_type_map := add_resource_to_hash interface_access_path class_name
-      | None ->
-          ()
-    in
     ResourcesHeld.fold
-      (fun access_path count acquired ->
+      (fun access_path (count, type_name) acquired ->
         match to_interface access_path with
         | Some interface_access_path ->
-            add_to_type_map access_path interface_access_path ;
-            ResourcesFromFormals.add interface_access_path count acquired
+            ResourcesFromFormals.add interface_access_path (count, type_name) acquired
         | None ->
             acquired )
       held ResourcesFromFormals.empty 
 
 
   let apply ~callee:summary ~return ~actuals held =
-    let apply_one (base, accesses) callee_count held =
+    let apply_one (base, accesses) (callee_count, type_name) held =
       let access_path_opt =
         match (base : InterfaceAccessPath.base) with
         | Return ->
@@ -258,13 +220,6 @@ module Summary = struct
           | _ ->
               None )
       in
-      let add_type_map search_access_path =
-        match ResourcesFromFormalsType.find_opt (base, accesses) !interface_type_map with
-        | Some class_name ->
-            type_map := ResourcesTypeHeld.add search_access_path class_name !type_map
-        | None ->
-            ()
-      in
       match access_path_opt with
       | None ->
           held
@@ -275,15 +230,15 @@ module Summary = struct
                 Top
             | NonTop callee_count ->
                 let old_count =
-                  ResourcesHeld.find_opt access_path held |> Option.value ~default:(NonTop 0)
+                  match ResourcesHeld.find_opt access_path held with
+                  | Some (count, _) ->
+                    count
+                  | None ->
+                    NonTop 0
                 in
                 update_count old_count callee_count
           in
-          let add_resource_to_hash =
-            match new_count with NonTop count when count > 0 -> add_type_map access_path | _ -> ()
-          in
-          add_resource_to_hash ;
-          ResourcesHeld.add access_path new_count held
+          ResourcesHeld.add access_path (new_count, type_name) held
     in
     ResourcesFromFormals.fold apply_one summary held
 end
