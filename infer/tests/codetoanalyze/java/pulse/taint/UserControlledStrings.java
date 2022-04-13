bbd49132c4c139920ccb6b7396d07357c2e663c8
Jules Villard <jul@fb.com>
2022-04-13 08:05:42 -0700

    [pulse] import quandary/UserControlledStrings.java test
    Summary:
    Not much to see here except an off-by-one error in the configuration
    that is corrected to make the tests pass.
    
    Reviewed By: skcho
    
    Differential Revision: D35496295
    
    fbshipit-source-id: 410e271767469ac3632a58ad63bf66687151888e

---
+++ infer/tests/codetoanalyze/java/pulse/taint/UserControlledStrings.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package codetoanalyze.java.pulse;
+
+import android.content.ClipboardManager;
+import android.text.Html;
+import android.text.Spanned;
+import android.widget.EditText;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserControlledStrings {
+  ClipboardManager clipboard;
+
+  Spanned clipboardToHtmlBad() {
+    return Html.fromHtml(clipboard.getText().toString());
+  }
+
+  EditText mEditText;
+
+  Spanned editTextToHtmlBad() {
+    return Html.fromHtml(mEditText.getText().toString());
+  }
+
+  void clipboardToShellDirectBad() throws IOException {
+    Runtime.getRuntime().exec(clipboard.getText().toString());
+  }
+
+  void FN_clipboardToShellArrayBad() throws IOException {
+    String[] cmds = new String[] {"ls", clipboard.getText().toString()};
+    Runtime.getRuntime().exec(cmds);
+  }
+
+  ProcessBuilder FN_clipboardToProcessBuilder1Bad() {
+    return new ProcessBuilder(clipboard.getText().toString());
+  }
+
+  ProcessBuilder FN_clipboardToProcessBuilder2Bad() {
+    return new ProcessBuilder("sh", clipboard.getText().toString());
+  }
+
+  ProcessBuilder FN_clipboardToProcessBuilder3Bad(ProcessBuilder builder) {
+    return builder.command(clipboard.getText().toString());
+  }
+
+  ProcessBuilder FN_clipboardToProcessBuilder4Bad(ProcessBuilder builder) {
+    List<String> cmds = new ArrayList();
+    cmds.add(clipboard.getText().toString());
+    return builder.command(cmds);
+  }
+}
