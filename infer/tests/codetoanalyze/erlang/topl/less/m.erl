1c5c130979aef7db56b5fede9ebe98f37ff190be
Radu Grigore <rgrigore@fb.com>
2022-04-28 03:10:50 -0700

    [topl] Test for using inequalities in conditions
    Summary: see subj
    
    Reviewed By: mmarescotti
    
    Differential Revision: D35313826
    
    fbshipit-source-id: ab8cd04137b1432841612ecc51dbe24c2ad86602

---
+++ infer/tests/codetoanalyze/erlang/topl/less/m.erl
@@ -0,0 +1,28 @@
+% Copyright (c) Facebook, Inc. and its affiliates.
+%
+% This source code is licensed under the MIT license found in the
+% LICENSE file in the root directory of this source tree.
+-module(m).
+-export([test_a_Ok/0, test_b_Bad/0, test_c_Bad/0, test_d_Ok/0, test_e_Bad/0]).
+
+test_a_Ok() ->
+    bar(10).
+
+test_b_Bad() ->
+    bar(11).
+
+test_c_Bad() ->
+    bar(14).
+
+test_d_Ok() ->
+    bar(15).
+
+test_e_Bad() ->
+    bar(10 + one()).
+
+%%% Helpers below
+
+bar(T) when 10 < T, T < 15 -> error(topl_error);
+bar(_) -> ok.
+
+one() -> 1.
