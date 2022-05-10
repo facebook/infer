c965636cc4d1254d4bf51c3ec34398b46f5af9b9
Radu Grigore <rgrigore@fb.com>
2022-05-10 02:33:44 -0700

    [lineage] Treat synthetic methods as unknown
    Summary:
    For spec-only methods we get some synthetic bodies. For lineage,
    however, we want to ignore those synthetic bodies. The synthesized code
    does not have information flow -- but we want to be conservative and
    assume that information flow may happen.
    
    Differential Revision: D36276116
    
    fbshipit-source-id: e9cf817c8177ed7a325d2f225ab0e4f1bacb17f8

--- infer/src/checkers/SimpleLineage.ml
+++ infer/src/checkers/SimpleLineage.ml
@@ -971,7 +971,12 @@ let checker ({InterproceduralAnalysis.proc_desc} as analysis_data) =
       ~f:(fun limit -> proc_size > limit)
       Config.simple_lineage_max_cfg_size
   in
-  if too_big then (
+  let is_synthetic =
+    (* Ignore bodies synthesized by frontend for spec-only functions. *)
+    (Procdesc.get_attributes proc_desc).ProcAttributes.is_synthetic_method
+  in
+  if is_synthetic then None
+  else if too_big then (
     L.user_warning "Skipped large (%d) procedure (%a)@." proc_size Procname.pp
       (Procdesc.get_proc_name proc_desc) ;
     None )
