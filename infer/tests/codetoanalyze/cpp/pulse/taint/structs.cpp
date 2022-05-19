fa8bc4457246ce992c36c81ad3a417f47e427f52
Hrutvik Kanabar <hrutvik@fb.com>
2022-05-19 06:28:25 -0700

    [pulse][taint] Add a C++ test which uses structs
    Summary:
    - Copy `codetoanalyze/cpp/quandary/structs.cpp` to taint analysis tests directory
    - Remove mention of `std::getenv` to ensure missed errors are struct-related
    - Rename functions to reflect missed errors
    - Re-record tests
    
    Reviewed By: da319
    
    Differential Revision: D36347704
    
    fbshipit-source-id: 7391e3b37bb8cf41bd1a1f9256427d37eee5f7a7

---
+++ infer/tests/codetoanalyze/cpp/pulse/taint/structs.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <memory>
+#include <string>
+
+struct mystruct {
+  char* str;
+  int i;
+};
+
+extern mystruct* __infer_taint_source();
+extern void __infer_taint_sink(mystruct);
+
+namespace structs {
+
+void struct_source_bad_FN() {
+  mystruct* source = __infer_taint_source();
+  __infer_taint_sink(*source);
+}
+
+void struct_field_source_unique_pointer_bad_FN() {
+  std::unique_ptr<mystruct> source(__infer_taint_source());
+  __infer_taint_sink(*source);
+}
+
+void struct_field_source_bad_FN() {
+  mystruct* tainted = __infer_taint_source();
+  mystruct source;
+  source.str = tainted->str;
+  source.i = 0;
+  __infer_taint_sink(source);
+}
+
+void read_from_struct_source_field_bad_FN() {
+  mystruct* source = __infer_taint_source();
+  system(source->str);
+}
+
+} // namespace structs
