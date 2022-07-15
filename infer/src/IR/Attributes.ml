8bba343edfec8072688ad19d65d25a77e64b3aeb
Nikos Gorogiannis <nikosg@fb.com>
2022-07-15 06:17:06 -0700

    [sqlite] remove attr_kind column
    Summary: Now that `attr_kind!=0` is equivalent to `cfg IS NOT NULL` the column can be removed.
    
    Reviewed By: martintrojer
    
    Differential Revision: D37854154
    
    fbshipit-source-id: 8f3bfc08fe86481231509a7cdf2df8623c2552f7

--- infer/src/IR/Attributes.ml
+++ infer/src/IR/Attributes.ml
@@ -6,25 +6,6 @@
  *)
 
 open! IStd
-module F = Format
-
-type attributes_kind = ProcUndefined | ProcDefined [@@deriving compare, equal]
-
-let attributes_kind_to_int64 = [(ProcUndefined, Int64.zero); (ProcDefined, Int64.of_int 2)]
-
-let int64_of_attributes_kind a =
-  List.Assoc.find_exn ~equal:equal_attributes_kind attributes_kind_to_int64 a
-
-
-let deserialize_attributes_kind =
-  let int64_to_attributes_kind = List.Assoc.inverse attributes_kind_to_int64 in
-  function[@warning "-8"]
-  | Sqlite3.Data.INT n -> List.Assoc.find_exn ~equal:Int64.equal int64_to_attributes_kind n
-
-
-let proc_kind_of_attr (proc_attributes : ProcAttributes.t) =
-  if proc_attributes.is_defined then ProcDefined else ProcUndefined
-
 
 let find =
   let select_statement =
@@ -61,10 +42,8 @@ let load, clear_cache, store =
         "Was given UNDEFINED procedure WITH procdesc:@\nAttributes:%a@\nProcdesc:%a@."
         ProcAttributes.pp attr (Pp.option Procdesc.pp_signature) proc_desc ;
     let pname = attr.proc_name in
-    let akind = proc_kind_of_attr attr in
     let proc_uid = Procname.to_unique_id pname in
     let proc_name = Procname.SQLite.serialize pname in
-    let attr_kind = int64_of_attributes_kind akind in
     let cfg = Procdesc.SQLite.serialize proc_desc in
     let proc_attributes = ProcAttributes.SQLite.serialize attr in
     let source_file = SourceFile.SQLite.serialize attr.loc.Location.file in
@@ -72,17 +51,10 @@ let load, clear_cache, store =
       Option.value_map proc_desc ~f:Procdesc.get_static_callees ~default:[]
       |> Procname.SQLiteList.serialize
     in
-    DBWriter.replace_attributes ~proc_uid ~proc_name ~attr_kind ~source_file ~proc_attributes ~cfg
-      ~callees ;
+    DBWriter.replace_attributes ~proc_uid ~proc_name ~source_file ~proc_attributes ~cfg ~callees ;
     Procname.Hash.remove cache pname
   in
   (load, clear_cache, store)
 
 
 let is_no_return pname = match load pname with Some {is_no_return} -> is_no_return | _ -> false
-
-let pp_attributes_kind f = function
-  | ProcUndefined ->
-      F.pp_print_string f "<undefined>"
-  | ProcDefined ->
-      F.pp_print_string f "<defined>"
--- infer/src/IR/Attributes.mli
+++ infer/src/IR/Attributes.mli
@@ -9,10 +9,6 @@
 
 open! IStd
 
-type attributes_kind
-
-val deserialize_attributes_kind : Sqlite3.Data.t -> attributes_kind
-
 val store : proc_desc:Procdesc.t option -> ProcAttributes.t -> unit
 (** Save .attr file for the procedure into the attributes database. *)
 
@@ -20,8 +16,6 @@ val load : Procname.t -> ProcAttributes.t option
 (** Load the attributes for the procedure from the attributes database. *)
 
 val is_no_return : Procname.t -> bool
-
-val pp_attributes_kind : Format.formatter -> attributes_kind -> unit
 
 val clear_cache : unit -> unit
 (** clear attribute cache *)
