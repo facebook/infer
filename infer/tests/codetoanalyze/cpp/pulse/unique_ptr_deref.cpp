cc93c18fc4b932ecc9e143516d7d5a9dd7460f47
Linpeng Zhang <linpengzhang@fb.com>
2022-07-20 06:15:55 -0700

    [Pulse] Added model for std::unique_ptr
    Summary: Added a (preliminary) model for some methods of the c++ template std::unique_ptr. Added new tests.
    
    Reviewed By: skcho
    
    Differential Revision: D37848192
    
    fbshipit-source-id: eb16018fe400fc4ecf74c23b2e203a5eba48207a

--- infer/tests/codetoanalyze/cpp/pulse/unique_ptr_deref.cpp
+++ infer/tests/codetoanalyze/cpp/pulse/unique_ptr_deref.cpp
@@ -9,6 +9,151 @@
 
 namespace unique_ptr {
 
+int constructor0_ok() {
+  std::unique_ptr<int> x(new int(42));
+  if (*x != 42) {
+    // Should not report a NPE here as *x is equal to 42
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int constructor0_bad() {
+  int* p = new int(42);
+  std::unique_ptr<int> x(p);
+  if (*x == 42) {
+    // Should report a NPE here as *x is equal to 42
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int destructor0_ok() {
+  auto x = new std::unique_ptr<int>(new int(5));
+  delete x;
+  return 0;
+}
+
+int destructor1_ok() {
+  { std::unique_ptr<int>(new int(5)); }
+  return 0;
+}
+
+int destructor2_ok() {
+  { std::unique_ptr<int> p; }
+  return 0;
+}
+
+int FP_destructor3_ok() {
+  auto x = new std::unique_ptr<int>();
+  delete x;
+  return 0;
+}
+
+int destructor0_bad() {
+  auto p = new int(5);
+  auto x = new std::unique_ptr<int>(p);
+  delete x;
+  // Should report a NPE here as p has been deallocated
+  return *p;
+}
+
+int destructor1_bad() {
+  auto p = new int(5);
+  { std::unique_ptr<int> x(p); }
+  // Should report a NPE here as p has been deallocated
+  return *p;
+}
+
+int array_access0_ok() {
+  auto p = new int[3];
+  p[1] = 42;
+  std::unique_ptr<int[]> x(p);
+  if (x[1] != 42) { // Should not report a NPE here as x[1] is equal to 42
+    int* q = nullptr;
+    *q;
+  }
+  return 0;
+}
+
+int array_access1_ok() {
+  auto p = new int[3];
+  p[1] = 42;
+  std::unique_ptr<int[]> x(p);
+  if (x[1] != 42) { // Should not report a NPE here as x[1] is equal to 42
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int array_access0_bad() {
+  std::unique_ptr<int[]> x(new int[3]);
+  x[1] = 42;
+  if (x[1] == 42) { // Should report a NPE here as x[1] is equal to 42
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int array_access1_bad() {
+  auto p = new int[3];
+  p[1] = 42;
+  std::unique_ptr<int[]> x(p);
+  if (x[1] == 42) { // Should report a NPE here as x[1] is equal to 42
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int get0_ok() {
+  int* p = new int(42);
+  std::unique_ptr<int> x(p);
+  if (x.get() != p) {
+    // Should not report a NPE here as x contains the pointer p
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int get1_ok() {
+  std::unique_ptr<int> x;
+  int* p = x.get(); // no dereference
+  if (p) {
+    // Should not report a NPE here as p points to nullptr
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int get0_bad() {
+  int* p = new int(42);
+  std::unique_ptr<int> x(p);
+  if (x.get() == p) {
+    // Should not report a NPE here as x contains the pointer p
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
+int get1_bad() {
+  std::unique_ptr<int> x;
+  int* p = x.get(); // no dereference
+  if (!p) {
+    // Should report a NPE here as p points to nullptr
+    int* q = nullptr;
+    return *q;
+  }
+  return 0;
+}
+
 struct X {
   int field;
   int* pointer_field;
@@ -18,60 +163,57 @@ struct X {
   ~X() { delete pointer_field; }
 };
 
-int empty_ptr_access() {
-  std::unique_ptr<int> x;
-  int* p = x.get(); // no dereference
-  if (p) {
-    return 1;
-  }
-  return 0;
-}
-
-int FN_empty_ptr_deref_bad() {
+int empty_ptr_deref_bad() {
   std::unique_ptr<int> x;
   return *x;
 }
 
-int FN_empty_array_ptr_deref_bad() {
+int empty_array_ptr_deref_bad() {
   std::unique_ptr<int[]> x;
   return x[0];
 }
 
-int FN_nullptr_ptr_deref_bad() {
+int nullptr_ptr_deref_bad() {
   std::unique_ptr<int> x(nullptr);
   return *x;
 }
 
-int FN_nullptr_array_ptr_deref_bad() {
+int nullptr_array_ptr_deref_bad() {
   std::unique_ptr<int[]> x(nullptr);
   return x[2];
 }
 
-int FN_empty_ptr_field_deref_bad() {
+int empty_ptr_field_deref_bad() {
   std::unique_ptr<X> x;
   return x.get()->field;
 }
 
-int FN_empty_ptr_field_deref2_bad() {
+int empty_ptr_field_deref2_bad() {
   std::unique_ptr<X> x;
   return x->field;
 }
 
-int FN_empty_ptr_method_deref_bad() {
+int empty_ptr_method_deref_bad() {
   std::unique_ptr<X> x;
   return x->get();
 }
 
-int FP_unique_ptr_create_use_ok() {
+int unique_ptr_create_use_ok() {
   std::unique_ptr<X> x(new X());
   return x->get();
 }
 
-void unique_ptr_release_bad() {
+void unique_ptr_release0_bad() {
   std::unique_ptr<X> x(new X());
   x.release();
+  *x; // Should report a NPE here as x, after the release, contains a nullptr
 }
 
+void FN_unique_ptr_release1_bad() {
+  std::unique_ptr<X> x(new X());
+  x.release(); // Should report a memory leak since the object of X has not been
+               // deallocated
+}
 
 int reset_ptr_null_deref_bad() {
   std::unique_ptr<int> x(new int);
@@ -79,11 +221,36 @@ int reset_ptr_null_deref_bad() {
   return *x;
 }
 
+void unique_ptr_release_get_ok() {
+  std::unique_ptr<X> x(new X());
+  if (x.get() != x.release()) {
+    // Should not report a NPE here as both x.release() and x.get() return the
+    // pointers contained in x
+    *x; // x, after the release, contains a nullptr
+  }
+}
+
+void unique_ptr_release_get_bad() {
+  std::unique_ptr<X> x(new X());
+  if (x.get() == x.release())
+  // Should report a NPE here as both x.release() and x.get() return the
+  // pointers contained in x
+  {
+    *x; // x, after the release, contains a nullptr
+  }
+}
+
 int reset_ptr_null_deref2_bad() {
   std::unique_ptr<int> x(new int);
   x.reset(new int);
   x.reset();
   return *x;
+}
+
+int reset_ptr_null_deref_ok() {
+  std::unique_ptr<int> x(new int);
+  x.reset();
+  return 42;
 }
 
 int reset_ptr_deref_ok() {
@@ -99,7 +266,7 @@ int reset_ptr_deref2_ok() {
   return *x;
 }
 
-int FN_unique_ptr_copy_null_deref_bad() {
+int unique_ptr_copy_null_deref_bad() {
   std::unique_ptr<int> p1;
   std::unique_ptr<int> p2 = std::move(p1);
   return *p2;
@@ -112,13 +279,13 @@ int unique_ptr_assign_null_deref_bad() {
   return *p1;
 }
 
-int FP_unique_ptr_move_deref_ok() {
+int unique_ptr_move_deref_ok() {
   std::unique_ptr<int> p1(new int);
   std::unique_ptr<int> p2 = std::move(p1);
   return *p2;
 }
 
-int FP_unique_ptr_assign_deref_ok() {
+int unique_ptr_assign_deref_ok() {
   std::unique_ptr<int> p1(new int);
   std::unique_ptr<int> p2;
   p2 = std::move(p1);
