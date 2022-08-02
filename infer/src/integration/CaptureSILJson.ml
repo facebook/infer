908e770d7323a1cd0b63d74f41c99f0a5ee911f1
Xiaoyu Liu <lixiaoyu@microsoft.com>
2022-08-02 23:29:24 +0000

    further improve efficiency

--- infer/src/integration/CaptureSILJson.ml
+++ infer/src/integration/CaptureSILJson.ml
@@ -18,8 +18,18 @@ let capture ~changed_files ~cfg_json ~tenv_json =
   Language.curr_language := Language.CIL ;
   match changed_files with
   | None ->
-    let source_file = SourceFile.create ~warn_on_error:false "./Program.cs" in
-    SourceFiles.add source_file cfg Tenv.Global None ;
+    let save_proc _ proc_desc =
+      let attributes = Procdesc.get_attributes proc_desc in
+      let source_file = attributes.loc.Location.file in
+      let loc = attributes.loc in
+      let attributes' =
+        let loc' = if Location.equal loc Location.dummy then {loc with file= source_file} else loc in
+        {attributes with loc= loc'; translation_unit= source_file}
+      in
+      Procdesc.set_attributes proc_desc attributes' ;
+      Attributes.store ~proc_desc:(Option.some_if attributes.is_defined proc_desc) attributes'
+    in
+    Procname.Hash.iter save_proc cfg ;
   | Some changed_files_set ->
     let add_file sf = SourceFiles.add sf cfg Tenv.Global None in
     SourceFile.Set.iter add_file changed_files_set ;
