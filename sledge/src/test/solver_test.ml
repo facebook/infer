59cf5eab3b881acdd00707b4c15adcc7e97564b9
Josh Berdine <jjb@fb.com>
2022-09-01 02:36:29 -0700

    [sledge] Strengthen Solver with last-resort fallback to Z3
    Summary:
    This diff strengthens the frame inference solver to use a combination
    of the internal first-order solver and Z3. Z3 is queried only when a
    `Solver.frame_inference` query is about to fail, which would otherwise
    lead to an alarm.
    
    The overall approach is, for a goal that is about to fail:
    
    1. Ask Z3 if the hypotheses are inconsistent (in a way sledge could
       not prove), in which case we are done.
    
    2. If the subtrahend is (satisfied by the) empty (heap), then
       entailment can be reduced to a first-order validity query.
    
    3. Otherwise search for a minuend segment and a subtrahend segment
       which Z3 can prove must overlap. For such a pair of segments, fork
       the proof search into 9 subgoals, one for each segment excision
       proof rule, and disjoining the remainders of the subgoals. Since
       the Z3 solver has just been warmed by the overlap query, we query
       it for applicability of each proof rule before continuing proof
       search. This helps since usually only a few of the 9 branches have
       consistent hypotheses. These queries are implemented in way to keep
       the solver in non-incremental mode, using temporary assumptions
       rather than a `push; assert; pop` sequence.
    
    Reviewed By: bennostein
    
    Differential Revision: D39136448
    
    fbshipit-source-id: 4b160de551a31571f1b5003879779f368056b1b0

--- sledge/src/test/solver_test.ml
+++ sledge/src/test/solver_test.ml
@@ -242,7 +242,7 @@ let%test_module _ =
                ∧ (%l_6 + 16) -[ %l_6, 16 )-> ⟨0,%a_3⟩)
             ) |}]
 
-    (* Incompleteness: equivalent to above but using ≤ instead of ∨ *)
+    (* Equivalent to above but using ≤ instead of ∨ *)
     let%expect_test _ =
       infer_frame
         (Sh.and_ (Formula.le n (i 2)) seg_split_symbolically)
@@ -255,7 +255,11 @@ let%test_module _ =
           ∧ %l_6 -[ %l_6, 16 )-> ⟨8×%n_9,%a_2⟩^⟨(-8×%n_9 + 16),%a_3⟩
           \- ∃ %a_1, %m_8 .
             %l_6 -[ %l_6, %m_8 )-> ⟨%m_8,%a_1⟩
-        ) Solver.infer_frame: |}]
+        ) Solver.infer_frame:
+            16 = %m_8
+          ∧ (⟨8×%n_9,%a_2⟩^⟨(-8×%n_9 + 16),%a_3⟩) = %a_1
+          ∧ ((16 > 8×%n_9) ∧ (2 ≥ %n_9))
+          ∧ emp |}]
 
     (* Incompleteness: cannot witness existentials to satisfy non-equality
        pure constraints *)
