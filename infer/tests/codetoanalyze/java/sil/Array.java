d872984222dc1d0ddb1307ff6585d3bb42694f3a
David Pichardie <dpichardie@fb.com>
2022-09-16 00:33:54 -0700

    [IR][Textual] Introducing an allocation builtin for arrays
    Summary:
    We don't have array allocation builtin yet. It make translation from Java program fails.
    We add a naive builtin, without type information. We will have to improve that later if we want to generate more precise SIL programs.
    
    Reviewed By: jvillard
    
    Differential Revision: D39540182
    
    fbshipit-source-id: 6bc3a748a1c1e3aef60c13431ffdef0bd25d3215

---
+++ infer/tests/codetoanalyze/java/sil/Array.java
@@ -0,0 +1,13 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+class Array {
+
+  void main(int n) {
+    int[] t1 = new int[n];
+    int[][] t2 = new int[n][n];
+  }
+
+}
