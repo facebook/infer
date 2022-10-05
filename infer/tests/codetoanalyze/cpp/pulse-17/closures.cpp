fed9bd3615a0001596109897e2f85c22dcfc7800
Daiva Naudziuniene <daivan@fb.com>
2022-10-05 07:56:09 -0700

    [pulse][lambdas] A false positive that includes generic lambdas
    Summary: Method signature is not being created for operator() for generic lambdas when captured variables are created, hence captured variables are missing in the procdesc.
    
    Reviewed By: fantazio
    
    Differential Revision: D40030246
    
    fbshipit-source-id: 0777d8552aa2e95c980bdc6232d9b620e489df17

---
+++ infer/tests/codetoanalyze/cpp/pulse-17/closures.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+int generic_lambda_ok_FP() {
+  int x = 1;
+  [&](auto y) { x += y; }(3);
+  if (x != 4) {
+    int* x = nullptr;
+    return *x;
+  }
+}
