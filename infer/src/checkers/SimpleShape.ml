1b64129c7bb2af6f5c484b17b809afa6eefccafb
Thibault Suzanne <tsuzanne@fb.com>
2022-10-06 01:10:45 -0700

    Put Shape env into its own module
    Summary: This is a no-op refactoring that will ease the later definition of summaries.
    
    Reviewed By: rgrig
    
    Differential Revision: D39935081
    
    fbshipit-source-id: c2c07b6bf0ffff857d81580ed5ef9391d26ba580

--- infer/src/checkers/SimpleShape.ml
+++ infer/src/checkers/SimpleShape.ml
@@ -9,6 +9,18 @@ open! IStd
 module L = Logging
 module CFG = ProcCfg.NormalOneInstrPerNode
 
+module Id () : sig
+  include Unique_id.Id
+
+  val pp : Format.formatter -> t -> unit
+end = struct
+  (** Generative unique identifiers with some utility functions *)
+
+  include Unique_id.Int63 ()
+
+  let pp fmt x = Format.fprintf fmt "<%a>" Int63.pp (x : t :> Int63.t)
+end
+
 (** This analysis aims at inferring all the fields that may be accessed from some variable, possibly
     through aliases.
 
@@ -19,145 +31,156 @@ module CFG = ProcCfg.NormalOneInstrPerNode
     [$argN#foo] at the same time as the node [$argN]. *)
 
 module Shape : sig
-  (** A module that manages shape definitions and environments. This is mostly done through a
-      stateful API to ease the implementation that currently heavily relies on a union-find data
-      structure (which is easier to implement imperatively). *)
+  module Env : sig
+    (** The [Env] module manages shape definitions and typing environments within the context of the
+        analysis of a function.
 
-  (** A shape can be seen as a handle to a simplified version of a type. To this handle will be
-      attached the subscriptable fields of the variables/fields that have this type. *)
-  type t
+        This is mostly done through a stateful API to ease the implementation that currently heavily
+        relies on a union-find data structure (which is easier to implement imperatively). *)
 
-  (** An environment associates shapes to variables and fields to shapes. *)
-  type env
+    (** A shape is a handle to which may be attached some subscriptable fields. These fields will
+        themselves recursively have an associated shape. Variables will also have a shape associated
+        to them. *)
+    type shape
 
-  val pp_env : Format.formatter -> env -> unit
+    (** An environment associates shapes to variables and fields to shapes. *)
+    type t
 
-  val create : unit -> t
-  (** Create a fresh shape, not associated to any variable yet, and having no known field. *)
+    val pp : Format.formatter -> t -> unit
 
-  val create_env : unit -> env
-  (** Create a fresh environment with no known variable nor shape fields. *)
+    val create_shape : unit -> shape
+    (** Create a fresh shape, not associated to any variable yet, and having no known field. *)
 
-  val of_var : Var.t -> env -> t
-  (** Returns the shape of a variable. If the variable is unknown in the environment, a fresh shape
-      will be created, associated to the variable, and returned.
+    val create : unit -> t
+    (** Create a fresh environment with no known variable nor shape fields. *)
 
-      Will always return a fresh shape for the `_` anonymous erlang variable. *)
+    val var_shape : Var.t -> t -> shape
+    (** Returns the shape of a variable. If the variable is unknown in the environment, a fresh
+        shape will be created, associated to the variable, and returned.
 
-  val of_field : t -> Fieldname.t -> env -> t
-  (** Given a shape, a subscripted field and an environment, returns the shape of the field content.
-      If this field is unknown for that shape in the environment, a fresh shape will be created,
-      associated to the field and returned. *)
+        Will always return a fresh shape for the `_` anonymous erlang variable. *)
 
-  val unify : t -> t -> env -> unit
-  (** Makes two shapes identical by merging their defined fields (unifying the shapes of common
-      fields if needed) and making it so that they will both share the same identical set of fields
-      in the future (one can now indifferently refer to any of the shapes as an alias of the
-      result). *)
+    val field_shape : shape -> Fieldname.t -> t -> shape
+    (** Given a shape, a subscripted field and an environment, returns the shape of the field
+        content. If this field is unknown for that shape in the environment, a fresh shape will be
+        created, associated to the field and returned. *)
+
+    val unify : shape -> shape -> t -> unit
+    (** Makes two shapes identical by merging their defined fields (unifying the shapes of common
+        fields if needed) and making it so that they will both share the same identical set of
+        fields in the future (one can now indifferently refer to any of the shapes as an alias of
+        the result). *)
+  end
 end = struct
-  (** A shape Id is what links variables to defined fields. It is a unique identifier to which a set
-      of fields is associated, and that will be indirectly assigned to every variable. *)
-  module Id = Unique_id.Int63 ()
+  module Env = struct
+    (** A shape id is what links variables to defined fields. It is a unique identifier to which a
+        set of fields is associated, and that will be indirectly assigned to every variable.
 
-  (** A shape is an equivalence class of shape identifiers, that is, shape identifiers that share a
-      common determined set of fields. *)
-  type t = Id.t Union_find.t
+        Shape ids do not make sense by themselves and are only valid within the context of the
+        analysis of the function that defined them, that is, for a given environment. OCaml typing
+        should prevent them from being used elsewhere (for instance mixed in another environment). *)
+    module Shape_id = Id ()
 
-  let pp_id fmt x = Format.fprintf fmt "<%d>" (Id.to_int_exn x)
+    (** A shape is an equivalence class of shape identifiers, that is, shape identifiers that share
+        a common determined set of fields. *)
+    type shape = Shape_id.t Union_find.t
 
-  let sexp_of_t x = [%sexp_of: _] (Union_find.get x)
+    let sexp_of_shape x = [%sexp_of: _] (Union_find.get x)
 
-  type fields = (Fieldname.t, t) Hashtbl.t [@@deriving sexp_of]
+    type fields = (Fieldname.t, shape) Hashtbl.t [@@deriving sexp_of]
 
-  (** An environment associates to each variable its equivalence class, and to each possible
-      representant of an equivalence class the set of known fields. *)
-  type env = {var_shape: (Var.t, t) Hashtbl.t; shape_fields: (Id.t, fields) Hashtbl.t}
+    (** An environment associates to each variable its equivalence class, and to each possible
+        representant of an equivalence class the set of known fields. *)
+    type t = {var_shape: (Var.t, shape) Hashtbl.t; shape_fields: (Shape_id.t, fields) Hashtbl.t}
 
-  let create_env () =
-    {var_shape= Hashtbl.create (module Var); shape_fields= Hashtbl.create (module Id)}
-
-
-  let pp_hashtbl pp_key pp_value fmt hashtbl =
-    let pp_binding fmt (key, value) = Format.fprintf fmt "@[%a -> %a@]" pp_key key pp_value value in
-    Format.fprintf fmt "@[(%a)@]"
-      (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ";@ ") pp_binding)
-      (Hashtbl.to_alist hashtbl)
+    let create () =
+      {var_shape= Hashtbl.create (module Var); shape_fields= Hashtbl.create (module Shape_id)}
 
 
-  let pp_shape fmt x = pp_id fmt (Union_find.get x)
-
-  let pp_env fmt {var_shape; shape_fields} =
-    Format.fprintf fmt "@[<v>@[<v4>VAR_SHAPE@;@[%a@]@]@;@[<v4>SHAPE_FIELDS@;@[%a@]@]@]@;"
-      (pp_hashtbl Var.pp pp_shape) var_shape
-      (pp_hashtbl pp_id (pp_hashtbl Fieldname.pp pp_shape))
-      shape_fields
-
-
-  let create () =
-    let id = Id.create () in
-    Union_find.create id
+    let pp_hashtbl pp_key pp_value fmt hashtbl =
+      let pp_binding fmt (key, value) =
+        Format.fprintf fmt "@[%a -> %a@]" pp_key key pp_value value
+      in
+      Format.fprintf fmt "@[(%a)@]"
+        (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ";@ ") pp_binding)
+        (Hashtbl.to_alist hashtbl)
 
 
-  let of_var var {var_shape; _} =
-    (* TODO T132509349 -- Hopefully temporary hack: always use fresh (empty) shapes for the anonymous
-       variable (otherwise it could lead to spurious unifications between unrelated uses of this
-       special variable, and transitively between actual variables). *)
-    if String.(Format.asprintf "%a" Var.pp var = "_") then create ()
-    else Hashtbl.find_or_add ~default:create var_shape var
+    let pp_shape fmt x = Shape_id.pp fmt (Union_find.get x)
+
+    let pp fmt {var_shape; shape_fields} =
+      Format.fprintf fmt "@[<v>@[<v4>VAR_SHAPE@;@[%a@]@]@;@[<v4>SHAPE_FIELDS@;@[%a@]@]@]@;"
+        (pp_hashtbl Var.pp pp_shape) var_shape
+        (pp_hashtbl Shape_id.pp (pp_hashtbl Fieldname.pp pp_shape))
+        shape_fields
 
 
-  let of_field shape fieldname {shape_fields; _} =
-    (* Proceed in two steps: retrieve the field set of this shape or create it, then return the shape
-       of the asked field or create it. *)
-    let id = Union_find.get shape in
-    let field_table =
-      Hashtbl.find_or_add ~default:(fun () -> Hashtbl.create (module Fieldname)) shape_fields id
-    in
-    Hashtbl.find_or_add ~default:create field_table fieldname
+    let create_shape () =
+      let id = Shape_id.create () in
+      Union_find.create id
 
 
-  let rec unify shape shape' ({shape_fields; _} as env) =
-    (* We need to explicitly check that we are not trying to unify already unified classes to ensure
-       termination. Otherwise, given a recursive shape such as [<0> -> { tail : <0> }], unifying
-       [ <0> ] with itself would recursively try to unify its fields, therefore recursively
-       proceeding to unify [ <0> ] with itself and so on.
+    let var_shape var {var_shape; _} =
+      (* TODO T132509349 -- Hopefully temporary hack: always use fresh (empty) shapes for the anonymous
+         variable (otherwise it could lead to spurious unifications between unrelated uses of this
+         special variable, and transitively between actual variables). *)
+      if String.(Format.asprintf "%a" Var.pp var = "_") then create_shape ()
+      else Hashtbl.find_or_add ~default:create_shape var_shape var
 
-       This check guarantees termination because shape unification can now only strictly reduce the
-       number of equivalence classes, which is finite. *)
-    if Union_find.same_class shape shape' then ()
-    else
+
+    let field_shape shape fieldname {shape_fields; _} =
+      (* Proceed in two steps: retrieve the field set of this shape or create it, then return the shape
+         of the asked field or create it. *)
       let id = Union_find.get shape in
-      let id' = Union_find.get shape' in
-      let () = Union_find.union shape shape' in
-      let new_id = Union_find.get shape in
-      let fields = Hashtbl.find shape_fields id in
-      let fields' = Hashtbl.find shape_fields id' in
-      match (fields, fields') with
-      | None, None ->
-          (* None of the unified id had fields => no field to add to the new shape *)
-          ()
-      | Some f, None | None, Some f ->
-          (* We do not know if the new shape id after unifying them is the one that used to have
-             fields, so we explicitly set it *)
-          Hashtbl.set shape_fields ~key:new_id ~data:f
-      | Some fields, Some fields' ->
-          (* Both unified shapes had field: unify them before adding the result to the unified id *)
-          (* Should we remove old shape entries? This could potentially reduce the memory footprint but
-             preliminary experiments seem to show that it is not needed (and extra care has to be taken to not
-             remove it before trying to access its definition). *)
-          Hashtbl.set shape_fields ~key:new_id ~data:(unify_fields fields fields' env)
+      let field_table =
+        Hashtbl.find_or_add ~default:(fun () -> Hashtbl.create (module Fieldname)) shape_fields id
+      in
+      Hashtbl.find_or_add ~default:create_shape field_table fieldname
 
 
-  and unify_fields fields fields' env =
-    Hashtbl.merge
-      ~f:(fun ~key:_ values ->
-        match values with
-        | `Left shape | `Right shape ->
-            Some shape
-        | `Both (shape, shape') ->
-            unify shape shape' env ;
-            Some shape )
-      fields fields'
+    let rec unify shape shape' ({shape_fields; _} as env) =
+      (* We need to explicitly check that we are not trying to unify already unified classes to ensure
+         termination. Otherwise, given a recursive shape such as [<0> -> { tail : <0> }], unifying
+         [ <0> ] with itself would recursively try to unify its fields, therefore recursively
+         proceeding to unify [ <0> ] with itself and so on.
+
+         This check guarantees termination because shape unification can now only strictly reduce the
+         number of equivalence classes, which is finite. *)
+      if Union_find.same_class shape shape' then ()
+      else
+        let id = Union_find.get shape in
+        let id' = Union_find.get shape' in
+        let () = Union_find.union shape shape' in
+        let new_id = Union_find.get shape in
+        let fields = Hashtbl.find shape_fields id in
+        let fields' = Hashtbl.find shape_fields id' in
+        match (fields, fields') with
+        | None, None ->
+            (* None of the unified id had fields => no field to add to the new shape *)
+            ()
+        | Some f, None | None, Some f ->
+            (* We do not know if the new shape id after unifying them is the one that used to have
+               fields, so we explicitly set it *)
+            Hashtbl.set shape_fields ~key:new_id ~data:f
+        | Some fields, Some fields' ->
+            (* Both unified shapes had field: unify them before adding the result to the unified id *)
+            (* Should we remove old shape entries? This could potentially reduce the memory footprint but
+               preliminary experiments seem to show that it is not needed (and extra care has to be taken to not
+               remove it before trying to access its definition). *)
+            Hashtbl.set shape_fields ~key:new_id ~data:(unify_fields fields fields' env)
+
+
+    and unify_fields fields fields' env =
+      Hashtbl.merge
+        ~f:(fun ~key:_ values ->
+          match values with
+          | `Left shape | `Right shape ->
+              Some shape
+          | `Both (shape, shape') ->
+              unify shape shape' env ;
+              Some shape )
+        fields fields'
+  end
 end
 
 (** As the environment works imperatively, we do not need to propagate it and therefore use a simple
@@ -175,14 +198,14 @@ module Report = struct
     (* For now we just want to print environment somewhere to debug the analysis. *)
     let procname = Procdesc.get_proc_name proc_desc in
     L.debug Analysis Verbose "@[<v>Result for procedure : %a@,@]" Procname.pp procname ;
-    L.debug Analysis Verbose "@[<v>%a@,@]" Shape.pp_env result
+    L.debug Analysis Verbose "@[<v>%a@,@]" Shape.Env.pp result
 end
 
 (** Transfer functions to compute shapes. As the environment is an imperative structure, this module
     takes a global environment as a parameter that will be mutated through the analysis by the
     hereby defined transfer functions. *)
 module TransferFunctions (Env : sig
-  val env : Shape.env
+  val env : Shape.Env.t
 end) =
 struct
   module Domain = Domain
@@ -197,26 +220,26 @@ struct
     match e with
     | Const _ | Closure _ ->
         (* We use fresh ids to represent shapes that do not hold fields. *)
-        Shape.create ()
+        Shape.Env.create_shape ()
     | Var id ->
-        Shape.of_var (Var.of_id id) env
+        Shape.Env.var_shape (Var.of_id id) env
     | Lvar pvar ->
-        Shape.of_var (Var.of_pvar pvar) env
+        Shape.Env.var_shape (Var.of_pvar pvar) env
     | Lfield (e, fieldname, _) ->
         let shape_e = shape_expr e in
-        Shape.of_field shape_e fieldname env
+        Shape.Env.field_shape shape_e fieldname env
     | Sizeof {dynamic_length= None} ->
-        Shape.create ()
+        Shape.Env.create_shape ()
     | UnOp (_, e, _) | Exn e | Cast (_, e) | Sizeof {dynamic_length= Some e} ->
         (* We first shape [e] to possibly discover some fields (eg. on [not (x.f)]), then return
            a fresh id as unary operators only return scalar value. *)
-        ignore (shape_expr e : Shape.t) ;
-        Shape.create ()
+        ignore (shape_expr e : Shape.Env.shape) ;
+        Shape.Env.create_shape ()
     | BinOp (_, e1, e2) | Lindex (e1, e2) ->
         (* Similar to the UnOp case *)
-        ignore (shape_expr e1 : Shape.t) ;
-        ignore (shape_expr e2 : Shape.t) ;
-        Shape.create ()
+        ignore (shape_expr e1 : Shape.Env.shape) ;
+        ignore (shape_expr e2 : Shape.Env.shape) ;
+        Shape.Env.create_shape ()
 
 
   let exec_assignment var rhs_exp =
@@ -227,9 +250,9 @@ struct
        Note that this might lead to over-approximating the field set of a variable that would be
        reassigned in the program with completely unrelated types. We believe that it does not happen
        with the Erlang translation anyway (and even then would not be a fundamental issue). *)
-    let var_shape = Shape.of_var var env in
+    let var_shape = Shape.Env.var_shape var env in
     let expr_shape = shape_expr rhs_exp in
-    Shape.unify var_shape expr_shape env
+    Shape.Env.unify var_shape expr_shape env
 
 
   let procname_of_exp (e : Exp.t) : Procname.t option =
@@ -248,7 +271,7 @@ struct
           (Format.pp_print_option Procname.pp)
           (procname_of_exp fun_exp)
     | Prune (e, _, _, _) ->
-        ignore (shape_expr e : Shape.t)
+        ignore (shape_expr e : Shape.Env.shape)
     | Metadata _ ->
         ()
     | Load {id; e; _} ->
@@ -274,7 +297,7 @@ end
     functor to build an analysis engine. *)
 module Analyzer () = struct
   module Env = struct
-    let env = Shape.create_env ()
+    let env = Shape.Env.create ()
   end
 
   include AbstractInterpreter.MakeRPO (TransferFunctions (Env))
