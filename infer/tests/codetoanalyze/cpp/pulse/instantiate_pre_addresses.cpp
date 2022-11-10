93d40293a43786f070e7ecc328ec51278af37f51
Daniel Mercier <mercier@adacore.com>
2022-11-10 11:46:10 +0100

    [pulse] Take into account reachable addresses from pre state
    In PulseAbductiveDomain, compute the closure from pre addresses in the
    post state. This avoids seeing them as unreachable.
    
    In PulseInterproc, apply the post to the remaining addresse reachable
    from the pre state.

---
+++ infer/tests/codetoanalyze/cpp/pulse/instantiate_pre_addresses.cpp
@@ -0,0 +1,68 @@
+struct rec {
+  int* x;
+};
+
+void foo(rec** ptr) {
+  (*ptr)->x = new int;
+  (*(*ptr)->x) = 24;
+  *ptr = new rec;
+}
+
+void no_leak_nullptr() {
+  rec* r1 = new rec{nullptr};
+  rec* r2 = r1;
+
+  foo(&r1);
+
+  if (*(r2->x) != 24) {
+    int* x = nullptr;
+    *x = 12;
+  }
+  *(r2->x) = 42;
+
+  delete r1;
+  delete r2->x;
+  delete r2;
+}
+
+struct list_item {
+  list_item* next;
+};
+
+struct list {
+  list_item* first;
+  list_item* last;
+};
+
+void append(list& l) {
+  list_item* new_cell = new list_item;
+  new_cell->next = nullptr;
+
+  if (l.last == nullptr) {
+    l.first = new_cell;
+  } else {
+    l.last->next = new_cell;
+  }
+
+  l.last = new_cell;
+}
+
+void delete_list(list& l) {
+  list_item* cell = l.first;
+  list_item* next_cell;
+
+  while (cell != nullptr) {
+    next_cell = cell->next;
+    delete cell;
+    cell = next_cell;
+  }
+}
+
+void no_leak() {
+  list l = {nullptr, nullptr};
+
+  append(l);
+  append(l);
+
+  delete_list(l);
+}
