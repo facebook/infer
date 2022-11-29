798e73956231031a8af6b17817a39210b962eab5
Corentin De Souza <fantasio@fb.com>
2022-11-29 02:14:56 -0800

    [pulse][taint] add simple array tests
    Summary:
    The new tests track the limitations of information tracking in arrays:
    - one limitation is if the canonical representation for a value changes.
      Because we use abstract values as indices, if the abstract value
      representing an index is not canonical and that index is later used
      using another abstract value then we won't see the match and assume
      the two indices to be different (and therefore the two cells)
    - the other limitation is with interprocedural mathing of indices (and,
      therefore, cells). If a function reads the value in a given cell of an
      array passed as a parameter and the caller of that function had set
      that value, we need to be able to unify them which we do not because
      the indices between caller and callee are not well canonicalized
      (and even if they were, the cells would need to be canonicalized too)
    
    Reviewed By: dulmarod
    
    Differential Revision: D41083942
    
    fbshipit-source-id: 4607b2a8c0e7ee674920d7fc2dacdb94560e3a8e

---
+++ infer/tests/codetoanalyze/objc/pulse/taint/arrays.m
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+int int_source(void);
+void sink_int(int);
+
+void test_intra_proc_bad(void) {
+  int arr[2];
+  arr[0] = int_source();
+  sink_int(arr[0]);
+}
+
+void test_intra_proc_good(void) {
+  int arr[2];
+  arr[0] = int_source();
+  sink_int(arr[1]);
+}
+
+void test_intra_change_0_bad_FN(int i) {
+  int arr[2];
+  arr[0] = int_source();
+  if (i == 0)
+    sink_int(arr[i]);
+}
+
+void test_intra_change_0_good(int i) {
+  int arr[2];
+  arr[0] = int_source();
+  if (i == 0)
+    sink_int(arr[1]);
+}
+
+void taint_cell(int* arr, int idx) { arr[idx] = int_source(); }
+
+void sink_cell(int* arr, int idx) { sink_int(arr[idx]); }
+
+void test_inter_proc_bad_FN(void) {
+  int arr[2];
+  taint_cell(arr, 0);
+  sink_cell(arr, 0);
+}
+
+void test_inter_proc_good(void) {
+  int arr[2];
+  taint_cell(arr, 0);
+  sink_cell(arr, 1);
+}
