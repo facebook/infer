f6b07096cac8603450489d53043650e33107160a
Akos Hajdu <akoshajdu@fb.com>
2023-01-31 02:13:08 -0800

    [erl-frontend] Add FP example to type-based taint test
    Summary: Add a FP example for taint analysis based on return type.
    
    Reviewed By: rgrig, mmarescotti
    
    Differential Revision: D42880597
    
    fbshipit-source-id: bd9122c67abac1b63ac24405b458fe62494a5860

--- infer/tests/codetoanalyze/erlang/pulse-taint/types/pulse_taint_types.erl
+++ infer/tests/codetoanalyze/erlang/pulse-taint/types/pulse_taint_types.erl
@@ -7,10 +7,13 @@
 -export([
     test_taint1_Bad/0,
     test_taint2_Bad/0,
-    test_taint3_Ok/0
+    test_taint3_Ok/0,
+    fp_test_taint4_Ok/0
 ]).
 
 -type dirty() :: atom().
+
+-type maybe_dirty() :: dirty() | integer().
 
 -spec(source1() -> dirty()).
 source1() -> hey.
@@ -18,8 +21,11 @@ source1() -> hey.
 -spec(source2() -> dirty()).
 source2() -> hey.
 
--spec(not_source() -> atom()).
-not_source() -> hey.
+-spec(not_source1() -> atom()).
+not_source1() -> hey.
+
+-spec(not_source2() -> maybe_dirty()).
+not_source2() -> 2.
 
 sink(_) -> oops.
 
@@ -27,4 +33,6 @@ test_taint1_Bad() -> sink(source1()).
 
 test_taint2_Bad() -> sink(source2()).
 
-test_taint3_Ok() -> sink(not_source()).
+test_taint3_Ok() -> sink(not_source1()).
+
+fp_test_taint4_Ok() -> sink(not_source2()).
