084b8e0d828cf8491420052c95cf7df78ebeca2c
Dulma Churchill <dulmarod@fb.com>
2023-03-02 06:41:56 -0800

    [clang] Improve the model of view controllers lifecycles
    Summary: This improves on the problem that sometimes we were adding the method calls in methods that were themselves initialisers. Now we solve this by making sure that we add the calls only on methods that call the initialiser but are not themselves initialisers.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D43698278
    
    fbshipit-source-id: d135ef5971fb5ecb3e7e433ed393ae762b292e42

--- infer/src/clang/CViewControllerLifecycle.ml
+++ infer/src/clang/CViewControllerLifecycle.ml
@@ -16,7 +16,7 @@ let is_view_controller tenv cls =
 
 let is_init_view_controller procname =
   Procname.is_objc_init procname
-  && not (String.equal (Procname.get_method procname) "initWithFuncTable:")
+  || String.is_prefix (Procname.get_method procname) ~prefix:"_initWithFuncTable:"
   || String.is_suffix (Procname.get_method procname) ~suffix:"ViewControllerCreate"
 
 
@@ -60,31 +60,34 @@ let build_view_controller_methods tenv cls loc controller_arg =
 
 
 let replace_calls tenv _ proc_desc =
-  let add_calls node _ instr =
-    let instrs = ProcCfg.Exceptional.instrs node in
-    Ident.update_name_generator (Instrs.instrs_get_normal_vars instrs) ;
-    let instrs =
-      match (instr : Sil.instr) with
-      | Call ((ret_id, ret_typ), Const (Cfun callee), _, loc, _) when is_init_view_controller callee
-        -> (
-        match ret_typ.Typ.desc with
-        | Typ.Tptr ({desc= Tstruct cls}, _) ->
-            let cls_name = Typ.Name.name cls in
-            if is_view_controller tenv cls_name then
-              instr :: build_view_controller_methods tenv cls loc (Var ret_id, ret_typ)
-            else [instr]
+  let procname = Procdesc.get_proc_name proc_desc in
+  if is_init_view_controller procname then ()
+  else
+    let add_calls node _ instr =
+      let instrs = ProcCfg.Exceptional.instrs node in
+      Ident.update_name_generator (Instrs.instrs_get_normal_vars instrs) ;
+      let instrs =
+        match (instr : Sil.instr) with
+        | Call ((ret_id, ret_typ), Const (Cfun callee), _, loc, _)
+          when is_init_view_controller callee -> (
+          match ret_typ.Typ.desc with
+          | Typ.Tptr ({desc= Tstruct cls}, _) ->
+              let cls_name = Typ.Name.name cls in
+              if is_view_controller tenv cls_name then
+                instr :: build_view_controller_methods tenv cls loc (Var ret_id, ret_typ)
+              else [instr]
+          | _ ->
+              [instr] )
         | _ ->
-            [instr] )
-      | _ ->
-          [instr]
+            [instr]
+      in
+      Array.of_list instrs
     in
-    Array.of_list instrs
-  in
-  let update_context () _ = () in
-  let context_at_node _ = () in
-  ignore
-    (Procdesc.replace_instrs_by_using_context proc_desc ~f:add_calls ~update_context
-       ~context_at_node )
+    let update_context () _ = () in
+    let context_at_node _ = () in
+    ignore
+      (Procdesc.replace_instrs_by_using_context proc_desc ~f:add_calls ~update_context
+         ~context_at_node )
 
 
 let process cfg tenv = Procname.Hash.iter (replace_calls tenv) cfg
