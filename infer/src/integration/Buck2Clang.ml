600e1c17cd1a8c67bcb7ca42bf742df548cfb4b9
Nikos Gorogiannis <nikosg@fb.com>
2023-03-03 03:01:06 -0800

    [buck2][clang] do not use environment variables
    Summary:
    Remove code that sets environment variables for infer running under buck2: this is contra-indicated (because infer will run on another host without this environment set up) and duplicates configuration that is already in .inferconfig.
    
    Also, do not run `buck2 clean`; this is not the integration's job.
    
    Reviewed By: martintrojer
    
    Differential Revision: D43738581
    
    fbshipit-source-id: 901e18d6dd7613dc7c5888d281681dfaa35f1f4f

--- infer/src/integration/Buck2Clang.ml
+++ infer/src/integration/Buck2Clang.ml
@@ -13,16 +13,6 @@ let capture_buck2_args build_report_file =
   @ Config.buck2_build_args
 
 
-let run_buck2_build prog buck2_build_args =
-  L.debug Capture Verbose "%s %s@." prog (List.to_string ~f:Fn.id buck2_build_args) ;
-  let infer_args =
-    Option.fold (Sys.getenv CommandLineOption.args_env_var) ~init:"--fcp-syntax-only"
-      ~f:(fun acc arg -> Printf.sprintf "%s%c%s" acc CommandLineOption.env_var_sep arg)
-  in
-  let extend_env = [(CommandLineOption.args_env_var, infer_args)] in
-  Buck.wrap_buck_call ~extend_env V2 ~label:"build" (prog :: buck2_build_args) |> ignore
-
-
 let capture build_cmd =
   let prog, buck2_args = (List.hd_exn build_cmd, List.tl_exn build_cmd) in
   let command, rev_not_targets, targets = Buck.parse_command_and_targets Clang V2 buck2_args in
@@ -32,7 +22,8 @@ let capture build_cmd =
         |> List.hd
         |> Option.value ~default:target_and_platform )
   in
-  if not (List.is_empty targets) then (
+  if List.is_empty targets then L.user_warning "No targets found to capture.@\n"
+  else
     let all_args = List.rev_append rev_not_targets targets in
     let buck2_build_cmd =
       command
@@ -41,15 +32,15 @@ let capture build_cmd =
     in
     L.debug Capture Quiet "Processed buck2 command '%a'@\n" (Pp.seq F.pp_print_string)
       buck2_build_cmd ;
-    if Config.keep_going && not Config.continue_capture then
-      Process.create_process_and_wait ~prog ~args:["clean"] ;
     let build_report_file =
       Filename.temp_file ~in_dir:(ResultsDir.get_path Temporary) "buck2_build_report" ".json"
     in
-    run_buck2_build prog (buck2_build_cmd @ capture_buck2_args build_report_file) ;
+    let buck2_build_args = buck2_build_cmd @ capture_buck2_args build_report_file in
+    L.debug Capture Verbose "%s %s@." prog (List.to_string ~f:Fn.id buck2_build_args) ;
+    Buck.wrap_buck_call ~extend_env:[] V2 ~label:"build" (prog :: buck2_build_args) |> ignore ;
     let infer_deps_lines =
       BuckBuildReport.parse_infer_deps ~root:Config.buck2_root ~build_report_file
     in
     let infer_deps = ResultsDir.get_path CaptureDependencies in
     Utils.with_file_out infer_deps ~f:(fun out_channel ->
-        Out_channel.output_lines out_channel infer_deps_lines ) )
+        Out_channel.output_lines out_channel infer_deps_lines )
