7ae68b515a6e647e8c6809f8f5c4a7231c246a56
Jeremy Dubreil <jeremydubreil@users.noreply.github.com>
2023-03-16 04:06:48 -0700

    [taint] add a Java example of taint propagation with lambdas (#1742)
    Summary:
    I have seen this pattern using lambdas leading to false negatives on real code. Pulse should report a taint error where `invokeFunction(function)` is called. I think there are different ways this could be addressed. Adding the false negative example for now.
    
    Pull Request resolved: https://github.com/facebook/infer/pull/1742
    
    Reviewed By: geralt-encore
    
    Differential Revision: D44129516
    
    Pulled By: skcho
    
    fbshipit-source-id: 221ad0df42d1f75cc827c19a7f9a58509eefdfc6

---
+++ infer/tests/codetoanalyze/java/pulse/taint/Lambdas.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package codetoanalyze.java.pulse;
+
+import java.util.function.Function;
+
+public class Lambdas {
+
+  Function<Integer, String> createFunctionWithTaintedParam() {
+    Object object = InferTaint.inferSecretSource();
+    return n -> {
+      InferTaint.inferSensitiveSink(object);
+      return String.valueOf(n);
+    };
+  }
+
+  String invokeFunction(Function<Integer, String> function) {
+    return function.apply(42);
+  }
+
+  String FN_createAndInvokeFunctionBad() {
+    Function<Integer, String> function = createFunctionWithTaintedParam();
+    return invokeFunction(function);
+  }
+}
