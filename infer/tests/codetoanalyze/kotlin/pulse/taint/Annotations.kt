f60944f588308d514c45fd0aae00bf393f49c326
Ilya Zorin <izorin@fb.com>
2023-03-28 09:40:04 -0700

    [taint] Add kotlin annotations tests
    Summary: Kotlin tests to exercise annotation-based taint flows
    
    Reviewed By: jvillard
    
    Differential Revision: D44462364
    
    fbshipit-source-id: 472e85209255df799f144b8c3acce293803e1972

---
+++ infer/tests/codetoanalyze/kotlin/pulse/taint/Annotations.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package codetoanalyze.kotlin.pulse
+
+internal class Annotations {
+
+  @SensitiveSourceMarker fun markedSource() = ""
+
+  @SensitiveSinkMarker fun markedSink(input: String?) {}
+
+  @SanitizerMarker fun markedSanitizer(input: String): String = input
+
+  fun unmarkedSource() = ""
+
+  fun unmarkedSink(input: String?) {}
+
+  fun unmarkedSanitizer(input: String): String = input
+
+  fun markedSourceToMarkedSinkBad() {
+    markedSink(markedSource())
+  }
+
+  fun markedSourceUsingMarkedSanitizerToMarkedSinkOk() {
+    markedSink(markedSanitizer(markedSource()))
+  }
+
+  fun markedSourceUsingUnmarkedSanitizerToMarkedSinkBad() {
+    markedSink(unmarkedSanitizer(markedSource()))
+  }
+
+  fun unmarkedSourceToMarkedSinkOk() {
+    markedSink(unmarkedSource())
+  }
+
+  fun unmarkedSourceUsingMarkedSanitizerToMarkedSinkOk() {
+    markedSink(markedSanitizer(unmarkedSource()))
+  }
+
+  fun markedSourceToUnmarkedSinkOk() {
+    unmarkedSink(markedSource())
+  }
+
+  fun unmarkedSourceToUnmarkedSinkOk() {
+    unmarkedSink(unmarkedSource())
+  }
+}
