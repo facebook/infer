8e114728af78c15b15a3bab3d36991aafc102eff
Benno Stein <benno@fb.com>
2023-04-03 09:09:05 -0700

    [incr] track deleted procedures in dependency graph computation
    Summary:
    Invalidate summaries of procedures that have been deleted since the
    last analysis run, which shouldn't affect analysis results of non-
    deleted procedures but may mess with masking out of errors/alarms in
    reports.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D44630679
    
    fbshipit-source-id: fb402b33a88aaa9e1d82cf496c9e06f5097e6d94

--- infer/src/backend/AnalysisDependencyGraph.ml
+++ infer/src/backend/AnalysisDependencyGraph.ml
@@ -10,6 +10,18 @@ module L = Logging
 let build ~changed_files =
   let graph = CallGraph.(create default_initial_capacity) in
   let tenv_deps = SourceFile.Hash.create 0 in
+  (* set up a hashtable of procedures in changed files at the current version upfront, to avoid
+     querying SQLite repeatedly for [SourceFiles.proc_names_of_source] *)
+  let procs_in_changed_files : Procname.Set.t SourceFile.Hash.t =
+    let hashtbl = SourceFile.Hash.create (SourceFile.Set.cardinal changed_files) in
+    SourceFile.Set.iter
+      (fun changed_file ->
+        let procs = SourceFiles.proc_names_of_source changed_file |> Procname.Set.of_list in
+        SourceFile.Hash.add hashtbl changed_file procs )
+      changed_files ;
+    hashtbl
+  in
+  let deleted_procs = ref [] in
   (* First, build a reverse analysis callgraph [graph] and tenv dependency map [tenv_deps]. *)
   Summary.OnDisk.iter_specs ~f:(fun {Summary.proc_name; dependencies} ->
       let Dependencies.{callees; used_tenv_sources} =
@@ -19,6 +31,17 @@ let build ~changed_files =
         | Partial _ ->
             L.die InternalError "deserialized summary with incomplete dependencies"
       in
+      let is_deleted_proc =
+        match Attributes.load proc_name with
+        | None ->
+            true
+        | Some {ProcAttributes.translation_unit} ->
+            SourceFile.Set.mem translation_unit changed_files
+            && not
+                 (Procname.Set.mem proc_name
+                    (SourceFile.Hash.find procs_in_changed_files translation_unit) )
+      in
+      if is_deleted_proc then deleted_procs := proc_name :: !deleted_procs ;
       List.iter callees ~f:(fun callee ->
           CallGraph.add_edge graph ~pname:callee ~successor_pname:proc_name ) ;
       List.iter used_tenv_sources ~f:(fun src_file ->
@@ -27,15 +50,16 @@ let build ~changed_files =
               Procname.HashSet.add proc_name deps
           | None ->
               Procname.HashSet.singleton proc_name |> SourceFile.Hash.add tenv_deps src_file ) ) ;
-  (* Then, flag in [graph] any procedure with a summary depending (transitively) on either
-     (1) the tenv of a changed file or (2) the summary of a changed procedure. *)
+  (* Then, flag in [graph] any procedure with a summary depending (transitively) on either (1) a
+     deleted procedure, (2) the tenv of a changed file or (3) the summary of a changed procedure. *)
+  List.iter !deleted_procs ~f:(CallGraph.flag_reachable graph) ;
   SourceFile.Set.iter
     (fun sf ->
       SourceFile.Hash.find_opt tenv_deps sf
       |> Option.iter ~f:(fun sf_tenv_deps ->
              Procname.HashSet.iter sf_tenv_deps (CallGraph.flag_reachable graph) ) ;
-      SourceFiles.proc_names_of_source sf
-      |> List.iter ~f:(fun pname ->
+      SourceFile.Hash.find procs_in_changed_files sf
+      |> Procname.Set.iter (fun pname ->
              match Attributes.load pname with
              | None ->
                  CallGraph.flag_reachable graph pname
