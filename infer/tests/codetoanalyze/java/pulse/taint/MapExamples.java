9d469330b635fbe9a7dde49585c7ea2de180c2c4
Jeremy Dubreil <jeremydubreil@users.noreply.github.com>
2023-05-30 09:23:01 -0700

    [taint][java] add an example of false negatives with maps (#1764)
    Summary:
    This pull request adds an example of taint flow false negative involving maps.
    
    Pull Request resolved: https://github.com/facebook/infer/pull/1764
    
    Reviewed By: geralt-encore
    
    Differential Revision: D46222824
    
    Pulled By: jvillard
    
    fbshipit-source-id: 1e01850aff19d9e5d45c89dfc08a9f60a8bace2a

---
+++ infer/tests/codetoanalyze/java/pulse/taint/MapExamples.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package codetoanalyze.java.pulse;
+
+import java.util.Map;
+
+public class MapExamples {
+
+  native Map<String, String> split1(Object s);
+
+  void propagateTaintOnMap1Bad_FN() {
+    Object object = InferTaint.inferSecretSource();
+    Map<String, String> map = split1(object);
+    String value = map.get("Whatever");
+    InferTaint.inferSensitiveSink(value); // Taint flow not reported here
+  }
+
+  native Map<String, String[]> split2(Object s);
+
+  void propagateTaintOnMap2Bad() {
+    Object object = InferTaint.inferSecretSource();
+    Map<String, String[]> map = split2(object);
+    String value = "";
+    if (!map.isEmpty()) {
+      String[] whatever = map.get("Whatever");
+      if (whatever != null) value = whatever[0];
+    }
+    InferTaint.inferSensitiveSink(value);
+  }
+
+  static void wrapper(String s) {
+    InferTaint.inferSensitiveSink(s);
+  }
+
+  void propagateTaintOnMap3Bad() {
+    Object object = InferTaint.inferSecretSource();
+    Map<String, String[]> map = split2(object);
+    String value = null;
+    if (!map.isEmpty()) {
+      String[] whatever = map.get("Whatever");
+      if (whatever != null) value = whatever[0];
+    }
+    wrapper(value);
+  }
+
+  void propagateTaintOnMap4Bad_FN() {
+    Object object = InferTaint.inferSecretSource();
+    Map<String, String[]> map = split2(object);
+    String value = "";
+    if (!map.isEmpty()) {
+      String[] whatever = map.get("Whatever");
+      if (whatever != null) value = whatever[0];
+    }
+    wrapper(value); // Taint flow not reported here
+  }
+}
