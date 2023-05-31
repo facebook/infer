7874a77c1bc209569770a41a811b7d21dc15e06c
Jules Villard <jul@meta.com>
2023-05-31 09:50:43 -0700

    [scheduling] fix bug in is_empty
    Summary:
    Before this it could be that `is_empty ()` was true but `next ()` could return
    more work to do because `next ()` calls `fill_queue ()`. It was observed on
    OpenSSL that two procedures were left in the graph at the end of the scheduling
    (not completely sure how or why always these two!) with the replay scheduling
    in the next diff.
    
    Reviewed By: bennostein
    
    Differential Revision: D46227130
    
    fbshipit-source-id: 062569c3d0ee8b4887c2f08da1a994480337640e

--- infer/src/backend/CallGraphScheduler.ml
+++ infer/src/backend/CallGraphScheduler.ml
@@ -8,6 +8,21 @@
 open! IStd
 module L = Logging
 
+(* How it works:
+
+   - the queue [pending] contains the currently-known items of work, which are all the nodes at the
+   same height (from the leaves), filled by [fill_queue].
+
+   - when picking up a node to process, flag it so it isn't picked again. Remove it from the graph
+   once the work on that node is done in [finished] so the graph gets gradually smaller and the cost
+   of walking the graph decreases as we go.
+
+   - to figure out if there is more work, take from the queue; if that's empty then it may be that
+   there is no more work, or that we need to get nodes from the next level, so call [fill_queue] to
+   check which one is the case.
+
+   - **invariant**: if [is_empty ()] then [next ()] is [None]
+*)
 let bottom_up call_graph =
   let open TaskSchedulerTypes in
   let remaining = ref (CallGraph.n_procs call_graph) in
@@ -17,18 +32,28 @@ let bottom_up call_graph =
   (* prime the pending queue so that [empty] doesn't immediately return true *)
   fill_queue () ;
   let scheduled = ref 0 in
+  (* cached query to avoid redoing work we do at the end of scheduling such as creating debug files
+     *)
+  let totally_empty = ref false in
   let is_empty () =
-    let empty = Int.equal 0 !scheduled && Queue.is_empty pending in
-    if empty then (
-      remaining := 0 ;
-      L.progress "Finished call graph scheduling, %d procs remaining (in, or reaching, cycles).@."
-        (CallGraph.n_procs call_graph) ;
-      if Config.debug_level_analysis > 0 then CallGraph.to_dotty call_graph "cycles.dot" ;
-      (* save some memory *)
-      CallGraph.reset call_graph ;
-      (* there is no equivalent to [Hashtbl.reset] so set capacity to min, freeing the old array *)
-      Queue.set_capacity pending 1 ) ;
-    empty
+    !totally_empty
+    ||
+    let is_empty = Int.equal 0 !scheduled && Queue.is_empty pending in
+    if is_empty then (
+      (* see toplevel comment *)
+      fill_queue () ;
+      let is_really_empty = Queue.is_empty pending in
+      if is_really_empty then (
+        remaining := 0 ;
+        L.progress "Finished call graph scheduling, %d procs remaining (in, or reaching, cycles).@."
+          (CallGraph.n_procs call_graph) ;
+        if Config.debug_level_analysis > 0 then CallGraph.to_dotty call_graph "cycles.dot" ;
+        (* save some memory *)
+        CallGraph.reset call_graph ;
+        (* there is no equivalent to [Hashtbl.reset] so set capacity to min, freeing the old array *)
+        Queue.set_capacity pending 1 ;
+        totally_empty := true ) ) ;
+    !totally_empty
   in
   let rec next () =
     match Queue.dequeue pending with
