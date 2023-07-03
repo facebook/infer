332c6503ddd018c541cb7370b90aedecf89f2420
Gabriela Cunha Sampaio <gabrielasampaio@meta.com>
2023-07-03 08:53:34 -0700

    [topl] Adding test for atom-name
    Summary: Recently TOPL started to support string literals (D46735443). Added a few tests that rely on atom name comparison.
    
    Reviewed By: rgrig
    
    Differential Revision: D47119449
    
    fbshipit-source-id: 22d3b76bd02da7173fc8d55c060e73ceca6fbe5e

---
+++ infer/tests/codetoanalyze/erlang/topl/atom-name/atom_name.erl
@@ -0,0 +1,36 @@
+% Copyright (c) Facebook, Inc. and its affiliates.
+%
+% This source code is licensed under the MIT license found in the
+% LICENSE file in the root directory of this source tree.
+-module(atom_name).
+
+-export([
+    test_1_Ok/0, test_2_Ok/0, test_3_Ok/0, test_4_Ok/0, test_1_Bad/0, test_2_Bad/0]).
+
+sink(not_ok, secret) -> erlang:error(taint_error);
+sink(_, _) -> ok.
+
+source() -> secret.
+
+test_1_Ok() ->
+    _ = source(),
+    sink(not_ok, xxxxxx).
+
+test_2_Ok() ->
+    % Ok because `secret` doesn't come from `source()`
+    sink(not_ok, secret).
+
+test_3_Ok() ->
+    % Ok because `secret` doesn't come from `source()`
+    _ = source(),
+    sink(not_ok, secret).
+
+test_4_Ok() ->
+    sink(its_ok, source()).
+
+test_1_Bad() ->
+    X = source(),
+    sink(not_ok, X).
+
+test_2_Bad() ->
+    sink(not_ok, source()).
