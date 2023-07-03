db10dcc04481ce4f9de9f42fff0c71df2f23ed38
Thibault Suzanne <tsuzanne@meta.com>
2023-07-03 01:33:10 -0700

    Change the hashing function of erlang atom to the first 7 bytes of md5
    Summary:
    Erlang atoms are represented internally by a structure that has their `name` as a string field, and
    a `hash` of that name in an integer one. That `hash` is what is used by most operations as it makes
    the analyser job easier to manipulate integers.
    
    This diff changes that hashing function from using (some bits of) the JaneStreet Core standard one on
    strings into the first 7 bytes of the MD5 sum of the atom name.
    
    The rationale is to make it easier to quickly generate Topl code that matches on atom
    hashes. However, we do not make any promise of forward compatibility, thus this hashing function must
    not be considered as production-usable but rather as a better way to do quick experiments with easily
    generated hashes (see the code diff for python and shell ways of computing this hash).
    
    Note that this "unstability" is not really a regression as neither Core nor the OCaml stdlib itself
    do not make any promise regarding the hash implementation (and as a matter of fact, it has changed in
    the past and it was explicitly stated that forward compatibility was not a goal).
    
    Differential Revision: D47157305
    
    fbshipit-source-id: 4262749d9e23f36c1f67641f1b498e55c05fe90f

--- infer/src/IR/ErlangTypeName.ml
+++ infer/src/IR/ErlangTypeName.ml
@@ -82,7 +82,63 @@ let atom_true = "true"
 
 let atom_false = "false"
 
-let calculate_hash atom = String.hash atom lsl 16
+let calculate_hash atom =
+  (* DISCLAIMER: there is currently no guarantee that this remains stable in the future and no
+     production-critical code should depend on this. The goal of this unofficial specification is to
+     facilitate debugging and fast development iterations.
+
+     We hash atom names into integers to ease the job of the analysers.
+
+     To do that, we interpret the first 7 bytes (= first 56 bits) of the MD5 hash of the name as
+     a big-endian integer.
+     - Using less than 62 bits ensures that the hash fits on a positive 63-bits OCaml integer.
+     - Using a round number of bytes makes implementations easier to write.
+     - Interpreting the bytes as big endian is both easier and less confusing, cf. example below.
+
+     Example. The MD5 of "foo" is the byte sequence acbd18db4cc2f85cedef654fccc4a4d8 (in the usual
+     hexdump notation). We therefore use 0xacbd18db4cc2f8 as the integer hash. Note: that's
+     big-endian because, in little-endian, the number would be Oxf8c2... instead. Little-endian would
+     mostly make implementation more annoying to write (and less intuitive to the debugging reader
+     who has access to a md5 hexdump).
+
+
+     OTHER LANGUAGE IMPLEMENTATIONS.
+
+     For easier reference and faster moving, we provide various ways of implementing the same hashing
+     function in some common languages.
+
+     * POSIX Shell:
+     ```
+     $ echo $((0x$(echo -n foo | md5sum | head -c 14)))
+     48621610450338552
+     ```
+
+     * Python:
+     ```
+     >>> from hashlib import md5
+     >>> 0xacbd18db4cc2f8 # FYI
+     48621610450338552
+
+     >>> # Preferred: directly interpret the hash bytes as an int
+     >>> int.from_bytes(md5("foo".encode()).digest()[:7], "big")
+     48621610450338552
+
+     >>> # Easier-to-debug steps: go through the md5 hexdump
+     >>> int(md5("foo".encode()).hexdigest()[:14], base=16)
+     48621610450338552
+     ```
+
+     * Python invocation from shell:
+     ```
+     $ python -c 'from hashlib import *; print(int(md5("foo".encode()).hexdigest()[:14], 16))'
+     ```
+  *)
+  Md5.digest_string atom (* Compute md5 *)
+  |> Md5.to_binary |> Bytes.of_string (* Get the hash as bytes *)
+  |> Fn.flip Caml.Bytes.get_int64_be 0 (* Get the first 8 bytes as a big-endian int64 *)
+  |> Fn.flip Int64.shift_right_logical 8 (* Drop the last byte / keep the first 7 bytes only *)
+  |> Int64.to_int_exn (* Put in an integer, raising if it unexpectedly does not fit *)
+
 
 let integer_value = "value"
 
