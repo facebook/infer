53787d3a669d8a633e2625dee74f1c108cbff6ba
Sungkeun Cho <scho@meta.com>
2023-08-08 08:30:37 -0700

    [pulse] Expose SharedPtr.assign_count
    Summary: Add SharedPtr.assign_count to mli.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D47872527
    
    fbshipit-source-id: a8bab7cccf1444e06f3f5e48ca7e6a78d38b39c8

--- infer/src/pulse/PulseModelsSmartPointers.mli
+++ infer/src/pulse/PulseModelsSmartPointers.mli
@@ -6,6 +6,19 @@
  *)
 
 open! IStd
+open PulseBasicInterface
+open PulseDomainInterface
 open PulseModelsImport
 
 val matchers : matcher list
+
+module SharedPtr : sig
+  val assign_count :
+       PathContext.t
+    -> Location.t
+    -> AbstractValue.t * ValueHistory.t
+    -> constant:IntLit.t
+    -> desc:string
+    -> AbductiveDomain.t
+    -> AbductiveDomain.t PulseOperationResult.t
+end
