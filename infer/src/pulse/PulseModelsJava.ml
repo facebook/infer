25c876c89711f53f7d70740512b1fa5609950ec4
David Pichardie <dpichardie@meta.com>
2023-11-09 06:06:17 -0800

    [hack] adding a dummy instanceof model
    Summary:
    previously we were using Java instanceof model in Hack, but this
    leads to strange agressive pruning during formula normalization. We simplify for now.
    
    Reviewed By: artempyanykh
    
    Differential Revision:
    D51154587
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 9441143090ef0cb997ec623d11c93401a7441530

--- infer/src/pulse/PulseModelsJava.ml
+++ infer/src/pulse/PulseModelsJava.ml
@@ -38,16 +38,22 @@ let write_field path field new_val location addr astate =
 
 let instance_of (argv, hist) typeexpr : model =
  fun {location; path; ret= ret_id, _} astate ->
-  let event = Hist.call_event path location "Java.instanceof" in
-  let res_addr = AbstractValue.mk_fresh () in
-  match typeexpr with
-  | Exp.Sizeof {typ} ->
-      let<++> astate = PulseArithmetic.and_equal_instanceof res_addr argv typ astate in
-      PulseOperations.write_id ret_id (res_addr, Hist.add_event path event hist) astate
-  (* The type expr is sometimes a Var expr but this is not expected.
-     This seems to be introduced by inline mechanism of Java synthetic methods during preanalysis *)
-  | _ ->
-      astate |> Basic.ok_continue
+  if Language.curr_language_is Hack then
+    let event = Hist.call_event path location "Hack.instanceof" in
+    let res_addr = AbstractValue.mk_fresh () in
+    PulseOperations.write_id ret_id (res_addr, Hist.add_event path event hist) astate
+    |> Basic.ok_continue
+  else
+    let event = Hist.call_event path location "Java.instanceof" in
+    let res_addr = AbstractValue.mk_fresh () in
+    match typeexpr with
+    | Exp.Sizeof {typ} ->
+        let<++> astate = PulseArithmetic.and_equal_instanceof res_addr argv typ astate in
+        PulseOperations.write_id ret_id (res_addr, Hist.add_event path event hist) astate
+    (* The type expr is sometimes a Var expr but this is not expected.
+       This seems to be introduced by inline mechanism of Java synthetic methods during preanalysis *)
+    | _ ->
+        astate |> Basic.ok_continue
 
 
 let call_may_throw_exception (exn : JavaClassName.t) : model =
