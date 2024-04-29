4bff3d2efb2b98c3a8638ad6f6088bdd624b79d6
Dulma Churchill <dulmarod@meta.com>
2024-04-29 03:57:36 -0700

    [retain cycles] Add test that shows that we are finding retain cycles between ObjC classes and pointers to structs
    Summary: A cycle is only a retain cycle when the objects are Objective-C objects or Objective-C blocks. Adding an example to show that we are finding other cycles as well.
    
    Reviewed By: skcho
    
    Differential Revision: D56637953
    
    fbshipit-source-id: 503bc08dd5b0a80084f9b2075c1ebaac334dfa82

---
+++ infer/tests/codetoanalyze/objc/pulse/retain_cycles/CycleStructExample.m
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <Foundation/Foundation.h>
+
+@class BridgingDelegate;
+
+struct Person {
+  int _age;
+  BridgingDelegate* _responderBridgingDelegate;
+};
+
+@interface BridgingDelegate : NSObject
+@end
+
+@implementation BridgingDelegate {
+  struct Person* _person;
+}
+
++ (instancetype)newWithPerson:(struct Person*)person {
+  BridgingDelegate* s = [self new];
+  if (s) {
+    s->_person = person;
+  }
+  return s;
+}
+
+@end
+
+void test_no_retain_cycle_good_FP() {
+  struct Person* person = (struct Person*)malloc(sizeof(struct Person));
+  BridgingDelegate* responderBridgingDelegate =
+      [BridgingDelegate newWithPerson:person];
+  person->_responderBridgingDelegate = responderBridgingDelegate;
+  free(person);
+}
