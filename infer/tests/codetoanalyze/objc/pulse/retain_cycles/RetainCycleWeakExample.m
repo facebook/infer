12fcbe1c5cc14522d7dc212991878aa9d39ccad8
Dulma Churchill <dulmarod@meta.com>
2024-05-02 07:54:43 -0700

    [retain cycles] Adding a test that shows an example where we report a wrong cycle
    Summary:
    The issue is a FP because `_component` has the `__weak` attribute. I was able to identify the problem: it's because we somehow skip the function `NSObject.new`, that means that after line 41 we wrongly identify the objects `c` and `bridgingDelegate`, which leads to wrong reports.
    
    Seems to be a wrong translation of `[super new]`. Will send a fix soon.
    
    Reviewed By: geralt-encore
    
    Differential Revision: D56877643
    
    fbshipit-source-id: bb1976049574cefdbf395cfcf2ed75844f38327f

---
+++ infer/tests/codetoanalyze/objc/pulse/retain_cycles/RetainCycleWeakExample.m
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface Component : NSObject
+@end
+
+@interface InternalComponent : Component
+@end
+
+@interface SpecialDelegate : NSObject
++ (instancetype)newWithComponent:(Component*)component;
+@end
+
+@implementation SpecialDelegate {
+  __weak Component* _component;
+}
+
++ (instancetype)newWithComponent:(Component*)component {
+  SpecialDelegate* s = [super new];
+  if (s) {
+    s->_component = component;
+  }
+  return s;
+}
+
+@end
+
+@implementation InternalComponent {
+ @package
+  SpecialDelegate* _delegate;
+};
+
++ (void)test_no_retain_cycle_good_FP {
+  InternalComponent* c = [super new];
+  SpecialDelegate* delegate = [SpecialDelegate newWithComponent:c];
+  c->_delegate = delegate;
+}
+
+@end
