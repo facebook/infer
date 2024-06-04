cf52dada605b032a63f8d74c40190ab03b986e9a
Akos Hajdu <akoshajdu@meta.com>
2024-06-04 12:13:11 -0700

    [erl-frontend] More tests for map comprehensions and generators
    Summary: Including some false positives/negatives to illustrate limitations.
    
    Reviewed By: thizanne
    
    Differential Revision: D58081833
    
    fbshipit-source-id: c8abf18d920589e490619ffe6db9f55b37c088bd

--- infer/tests/codetoanalyze/erlang/pulse/features/features_mc.erl
+++ infer/tests/codetoanalyze/erlang/pulse/features/features_mc.erl
@@ -9,11 +9,18 @@
 -export([
     test_mapcomp_Ok/0,
     test_mapcomp_Bad/0,
+    test_mapcomp2_Ok/0,
+    fn_test_mapcomp2_Bad/0,
     test_mapgen_empty_Ok/0,
     test_mapgen_empty_Bad/0,
     test_mapgen_nonempty_Ok/0,
     test_mapgen_nonempty_Bad/0,
-    test_mapgen_badmap_Bad/0
+    fp_test_mapgen_nonempty2_Ok/0,
+    fn_test_mapgen_nonempty2_Bad/0,
+    test_mapgen_badmap_Bad/0,
+    test_mapgen_badmap2_Bad/0,
+    test_mapcomp_with_mapgen_Ok/0,
+    test_mapcomp_with_mapgen_Bad/0
 ]).
 
 test_mapcomp_Ok() ->
@@ -23,6 +30,17 @@ test_mapcomp_Ok() ->
 test_mapcomp_Bad() ->
     M = #{I => I+1 || I <- [1]},
     ?CRASH_IF_EQUAL(2, maps:get(1, M)).
+
+test_mapcomp2_Ok() ->
+    M = #{I => I+1 || I <- [1, 2]},
+    Vals = [maps:get(1, M), maps:get(2, M)],
+    ?ASSERT_EQUAL([2, 3], Vals).
+
+% FN expected due to map recency abstraction
+fn_test_mapcomp2_Bad() ->
+    M = #{I => I+1 || I <- [1, 2]},
+    Vals = [maps:get(1, M), maps:get(2, M)],
+    ?CRASH_IF_EQUAL([2, 3], Vals).
 
 test_mapgen_empty_Ok() ->
     M = #{},
@@ -44,6 +62,32 @@ test_mapgen_nonempty_Bad() ->
     L = [{K, V} || K := V <- M],
     ?CRASH_IF_EQUAL([{1, 2}], L).
 
+% FP expected due to map recency abstraction
+fp_test_mapgen_nonempty2_Ok() ->
+    M = #{1 => 2, 3 => 4},
+    L = [{K, V} || K := V <- M],
+    ?ASSERT_EQUAL([{1, 2}, {3, 4}], L).
+
+% FN expected due to map recency abstraction
+fn_test_mapgen_nonempty2_Bad() ->
+    M = #{1 => 2, 3 => 4},
+    L = [{K, V} || K := V <- M],
+    ?CRASH_IF_EQUAL([{1, 2}, {3, 4}], L).
+
 test_mapgen_badmap_Bad() ->
     M = [],
     [{K, V} || K := V <- M].
+
+test_mapgen_badmap2_Bad() ->
+    M = 1,
+    [{K, V} || K := V <- M].
+
+test_mapcomp_with_mapgen_Ok() ->
+    M = #{1 => 2},
+    M2 = #{V => K || K := V <- M},
+    ?ASSERT_EQUAL(1, maps:get(2, M2)).
+
+test_mapcomp_with_mapgen_Bad() ->
+    M = #{1 => 2},
+    M2 = #{V => K || K := V <- M},
+    ?CRASH_IF_EQUAL(1, maps:get(2, M2)).
