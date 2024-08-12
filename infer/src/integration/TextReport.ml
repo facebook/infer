db0f63342ca302f87720b01a67ce2ed330f0b793
Martin Trojer <mtrojer@meta.com>
2024-08-12 02:59:52 -0700

    include the full bug_type in text output
    Reviewed By: geralt-encore
    
    Differential Revision: D61112850
    
    fbshipit-source-id: 9561aca5fdb869618e58e1c346b849c14a83eeac

--- infer/src/integration/TextReport.ml
+++ infer/src/integration/TextReport.ml
@@ -66,15 +66,16 @@ end
 
 let pp_suggestion fmt = Option.iter ~f:(F.pp_print_string fmt)
 
-let pp_jsonbug fmt {Jsonbug_t.file; severity; line; bug_type_hum; qualifier; suggestion; _} =
-  F.fprintf fmt "%s:%d: %s: %s@\n  %s %a" file line (String.lowercase severity) bug_type_hum
-    qualifier pp_suggestion suggestion
+let pp_jsonbug fmt {Jsonbug_t.file; severity; line; bug_type; bug_type_hum; qualifier; suggestion; _}
+    =
+  F.fprintf fmt "%s:%d: %s: %s(%s)@\n  %s %a" file line (String.lowercase severity) bug_type_hum
+    bug_type qualifier pp_suggestion suggestion
 
 
 let pp_jsonbug_with_number fmt
-    (i, {Jsonbug_t.file; severity; line; bug_type_hum; qualifier; suggestion; _}) =
-  F.fprintf fmt "#%d@\n%s:%d: %s: %s@\n  %s %a" i file line (String.lowercase severity) bug_type_hum
-    qualifier pp_suggestion suggestion
+    (i, {Jsonbug_t.file; severity; line; bug_type; bug_type_hum; qualifier; suggestion; _}) =
+  F.fprintf fmt "#%d@\n%s:%d: %s: %s(%s)@\n  %s %a" i file line (String.lowercase severity)
+    bug_type_hum bug_type qualifier pp_suggestion suggestion
 
 
 let pp_source_context ~indent fmt
