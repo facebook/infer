fa0b0e3d7ac5889cdd737574cd65f1e44da87494
Jules Villard <jul@meta.com>
2024-09-12 10:00:48 -0700

    [pulse] fix recursion test
    Summary: This isn't FN provided one actually writes the correct test.
    
    Reviewed By: ngorogiannis
    
    Differential Revision:
    D62582812
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 837d2f73cc9e317369917ee13ed5fc3bf78a6a77

--- infer/tests/codetoanalyze/c/pulse/recursion2.c
+++ infer/tests/codetoanalyze/c/pulse/recursion2.c
@@ -5,6 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-void FN_across_file_1();
+void across_file_1();
 
-void across_file_2() { FN_across_file_1(); }
+void across_file_2() { across_file_1(); }
