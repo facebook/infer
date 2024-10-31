bd4f7423298d70fc12183d831bfcde8073f2db1a
Akos Hajdu <akoshajdu@meta.com>
2024-10-31 11:59:39 -0700

    [annot] Add Obj-C test cases
    Summary: Add some test cases for Obj-C code.
    
    Reviewed By: rgrig
    
    Differential Revision: D65219538
    
    fbshipit-source-id: 37b3c039b448315765e50ff60ebc1280ad3d2c62

---
+++ infer/tests/codetoanalyze/objc/annotreach/Reach.m
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <Foundation/Foundation.h>
+@interface MyClass : NSObject
+- (void)sink;
+- (void)not_si_nk;
+- (void)sanitizer;
+- (void)not_sani_tizer;
+- (void)source1_Ok;
+- (void)source2_Bad;
+- (void)source3_Ok;
+- (void)source4_Bad;
+@end
+@implementation MyClass
+- (void)sink {
+}
+- (void)not_si_nk {
+}
+- (void)sanitizer {
+  [self sink];
+}
+- (void)not_sani_tizer {
+  [self sink];
+}
+- (void)source1_Ok {
+  [self not_si_nk];
+}
+- (void)source2_Bad {
+  [self sink];
+}
+- (void)source3_Ok {
+  [self sanitizer];
+}
+- (void)source4_Bad {
+  [self not_sani_tizer];
+}
+@end
