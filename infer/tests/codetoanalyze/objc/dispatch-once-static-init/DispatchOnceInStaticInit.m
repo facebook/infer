5db9da5db24941136d08fd171d568d260ef18ecb
Dulma Churchill <dulmarod@meta.com>
2024-11-08 04:03:50 -0800

    [dispatch-once-static-init] Add tests for the kind of issues we want to flag with a new checker
    Summary: The problem here is that calling `dispatch_once` from a static constructor can lead to deadlocks.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D65428397
    
    fbshipit-source-id: 8dfeecd39fe36e0b8fb4a06433849295b056aa55

---
+++ infer/tests/codetoanalyze/objc/dispatch-once-static-init/DispatchOnceInStaticInit.m
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#import <Foundation/Foundation.h>
+
+@interface Manager : NSObject
+
+@end
+
+@implementation Manager
+
++ (instancetype)getInstance {
+  static Manager* manager;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    manager = [Manager new];
+  });
+  return manager;
+}
+
+@end
+
+__attribute__((constructor)) static void initializer_test_interproc_bad_FN(
+    void) {
+  [Manager getInstance];
+}
+
+__attribute__((constructor)) static void initializer_test_intraproc_bad_FN(
+    void) {
+  static Manager* manager;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    manager = [Manager new];
+  });
+}
