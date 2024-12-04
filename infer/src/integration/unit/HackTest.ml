fdae895c17311511a5d0eb29ea0a1a7b118133ed
Nikos Gorogiannis <nikosg@meta.com>
2024-12-04 08:21:29 -0800

    [hack] remove unused functions
    Summary: As per title.
    
    Reviewed By: jvillard
    
    Differential Revision:
    D66761754
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 265bd413f1148fb3af8c76851e405ed6a175bcb3

--- infer/src/integration/unit/HackTest.ml
+++ infer/src/integration/unit/HackTest.ml
@@ -25,32 +25,4 @@ let%test_module "naming_table" =
           MODULES TEXT
         );
     *)
-    (* TODO(vsiles): speed things up by computing the hash
-     * and querying FILE_INFO_ID instead of CLASSES
-     *)
-    let create_table =
-      {| CREATE TABLE NAMING_FILE_INFO(
-          FILE_INFO_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-          PATH_SUFFIX TEXT NOT NULL,
-          CLASSES TEXT
-        );
-        |}
-
-
-    let%expect_test _ =
-      let show (_, l) = List.to_string ~f:(fun s -> s) l in
-      let open Sqlite3 in
-      let& db = Sqlite3.db_open "in-memory.sql" ~memory:true in
-      let _ = Sqlite3.exec db create_table in
-      let _ =
-        Sqlite3.exec db
-          "INSERT INTO NAMING_FILE_INFO (FILE_INFO_ID, PATH_SUFFIX, CLASSES) VALUES (0, \"world\", \
-           \"hello\");"
-      in
-      let good = Integration.Hack.location_of_class_db db ~class_name:"hello" in
-      let bad = Integration.Hack.location_of_class_db db ~class_name:"bad" in
-      F.printf "%s" (show good) ;
-      [%expect {| (world) |}] ;
-      F.printf "%s" (show bad) ;
-      [%expect {| () |}]
   end )
