4e79bb43f987df1eded7b10bfc4e7bc2ecd42322
Dulma Churchill <dulmarod@meta.com>
2025-02-05 05:22:06 -0800

    [swift] Llair to Textual: translating formals and locals from functions
    Summary: We aim to translate llair functions, and we'll do it in parts. Here we do formals and locals (except for the types). We declare a temporary data structure `partial_proc_desc` to keep the translated parts and make sure that we have the required types in Textual correctly. So we can keep adding fields until we have complete proc_desc, similarly for proc_decl.
    
    Reviewed By: davidpichardie
    
    Differential Revision: D69127884
    
    fbshipit-source-id: 7c976039b61ed3045771af5d15933228467dba7b

--- infer/src/swift/Llair2Textual.ml
+++ infer/src/swift/Llair2Textual.ml
@@ -7,6 +7,8 @@
 
 open! IStd
 
+let iarray_to_list array = IArray.to_array array |> Array.to_list
+
 let to_textual_typ _llair_typ =
   (* TODO: translate types *)
   Textual.Typ.Void
@@ -20,11 +22,39 @@ let translate_llair_globals globals =
     let typ = to_textual_typ (Global.typ global) in
     Textual.Global.{name; typ; attributes= []}
   in
-  let globals = IArray.to_array globals |> Array.to_list in
+  let globals = iarray_to_list globals in
   List.map ~f:to_textual_global globals
+
+
+let reg_to_var_name reg = Textual.VarName.of_string (Reg.name reg)
+
+type partial_proc_desc = {params: Textual.VarName.t list; locals: Textual.VarName.t list}
+
+let to_formals func =
+  let to_textual_formal formal = reg_to_var_name formal in
+  List.map ~f:to_textual_formal (iarray_to_list func.Llair.formals)
+
+
+let to_locals func =
+  let to_textual_local local = reg_to_var_name local in
+  let locals = Reg.Set.to_list func.Llair.locals in
+  List.map ~f:to_textual_local locals
+
+
+let translate_llair_functions functions =
+  let function_to_formal proc_descs (_, func) =
+    let formals = to_formals func in
+    let locals = to_locals func in
+    {params= formals; locals} :: proc_descs
+  in
+  let values = FuncName.Map.to_list functions in
+  List.fold values ~f:function_to_formal ~init:[]
 
 
 let translate sourcefile (llair_program : Llair.Program.t) : Textual.Module.t =
   let globals = translate_llair_globals llair_program.Llair.globals in
+  (* We'll build the procdesc partially until we have all the pieces required in Textual
+     and can add them to the list of declarations *)
+  let _partial_proc_descs = translate_llair_functions llair_program.Llair.functions in
   let decls = List.map ~f:(fun global -> Textual.Module.Global global) globals in
   Textual.Module.{attrs= []; decls; sourcefile}
