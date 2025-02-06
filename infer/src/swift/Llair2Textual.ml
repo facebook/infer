f25c532f2171c62d97c4e97ebd5c12d1f1a5b4b5
Dulma Churchill <dulmarod@meta.com>
2025-02-06 05:23:51 -0800

    [swift] Llair to Textual: translating parts of ProcDecl
    Summary: Now we translate the function name, it's location and the function type (the proper translation of types is still to come).
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D69185749
    
    fbshipit-source-id: 8dcdc10ca3d9243f31fc051d5bd9e91a3af109ca

--- infer/src/swift/Llair2Textual.ml
+++ infer/src/swift/Llair2Textual.ml
@@ -9,10 +9,19 @@ open! IStd
 
 let iarray_to_list array = IArray.to_array array |> Array.to_list
 
+let reg_to_var_name reg = Textual.VarName.of_string (Reg.name reg)
+
 let to_textual_typ _llair_typ =
   (* TODO: translate types *)
   Textual.Typ.Void
 
+
+let to_annotated_textual_typ llair_typ =
+  let typ = to_textual_typ llair_typ in
+  {typ; Textual.Typ.attributes= []}
+
+
+let to_textual_loc {Loc.line; col} = Textual.Location.Known {line; col}
 
 let translate_llair_globals globals =
   let to_textual_global global =
@@ -26,9 +35,22 @@ let translate_llair_globals globals =
   List.map ~f:to_textual_global globals
 
 
-let reg_to_var_name reg = Textual.VarName.of_string (Reg.name reg)
-
 type partial_proc_desc = {params: Textual.VarName.t list; locals: Textual.VarName.t list}
+
+type partial_proc_decl =
+  {qualified_name: Textual.QualifiedProcName.t; result_type: Textual.Typ.annotated}
+
+let to_qualified_proc_name func_name loc =
+  let func_name = FuncName.name func_name in
+  let loc = to_textual_loc loc in
+  Textual.QualifiedProcName.
+    {enclosing_class= TopLevel; name= Textual.ProcName.of_string ~loc func_name}
+
+
+let to_result_type func_name =
+  let typ = FuncName.typ func_name in
+  to_textual_typ typ
+
 
 let to_formals func =
   let to_textual_formal formal = reg_to_var_name formal in
@@ -42,10 +64,12 @@ let to_locals func =
 
 
 let translate_llair_functions functions =
-  let function_to_formal proc_descs (_, func) =
+  let function_to_formal proc_descs (func_name, func) =
     let formals = to_formals func in
     let locals = to_locals func in
-    {params= formals; locals} :: proc_descs
+    let qualified_name = to_qualified_proc_name func_name func.Llair.loc in
+    let result_type = to_result_type func_name |> to_annotated_textual_typ in
+    ({params= formals; locals}, {qualified_name; result_type}) :: proc_descs
   in
   let values = FuncName.Map.to_list functions in
   List.fold values ~f:function_to_formal ~init:[]
@@ -55,6 +79,6 @@ let translate sourcefile (llair_program : Llair.Program.t) : Textual.Module.t =
   let globals = translate_llair_globals llair_program.Llair.globals in
   (* We'll build the procdesc partially until we have all the pieces required in Textual
      and can add them to the list of declarations *)
-  let _partial_proc_descs = translate_llair_functions llair_program.Llair.functions in
+  let _partial_procs = translate_llair_functions llair_program.Llair.functions in
   let decls = List.map ~f:(fun global -> Textual.Module.Global global) globals in
   Textual.Module.{attrs= []; decls; sourcefile}
