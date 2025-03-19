ace02028c9fc07e4b853b8f5e9d3479963ac21ea
David Pichardie <dpichardie@meta.com>
2025-03-19 03:02:16 -0700

    [inferpython][tests] new file dict-missing-key tests for OO
    Summary: we will put specific 00 reasonning here.
    
    Reviewed By: ngorogiannis
    
    Differential Revision:
    D71316624
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 08f541c6beeafdc9d7b77efd31143532796af441

---
+++ infer/tests/codetoanalyze/python/pulse/dict_object_oriented.py
@@ -0,0 +1,34 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+class Box:
+    def __init__(self, key):
+        self.key = key
+
+    def get_key(self):
+        return self.key
+
+def set_attribute_with_init_bad():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key2")
+    return my_dict[box.key]
+
+
+def set_attribute_with_init_ok():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key0")
+    return my_dict[box.key]
+
+
+#TODO: improve call_method builtin model
+def fn_set_attribute_with_init_read_with_getter_bad():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key2")
+    return my_dict[box.get_key()]
+
+def set_attribute_with_init_read_with_getter_ok():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key0")
+    return my_dict[box.get_key()]
