b6bfe5e20ec268a60235a8b25d42903f6a9b1ba1
David Pichardie <dpichardie@meta.com>
2025-03-20 00:12:13 -0700

    [inferpython][O0][tests] use setter and some object helpers
    Summary:
    we add more tests about OO. Everything works fine except one FN that
    is more related to our dictionary models. No easy answer to improve that.
    
    Reviewed By: Gabisampaio
    
    Differential Revision:
    D71475583
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 86ff10477fa0655492da0bc79a8026ac43894417

--- infer/tests/codetoanalyze/python/pulse/dict_object_oriented.py
+++ infer/tests/codetoanalyze/python/pulse/dict_object_oriented.py
@@ -10,6 +10,9 @@ class Box:
     def get_key(self):
         return self.key
 
+    def set_key(self, val):
+        self.key = val
+
 def set_attribute_with_init_bad():
     my_dict = {"key0": 0, "key1": 1}
     box = Box("key2")
@@ -27,7 +30,57 @@ def set_attribute_with_init_read_with_getter_bad():
     box = Box("key2")
     return my_dict[box.get_key()]
 
+
 def set_attribute_with_init_read_with_getter_ok():
     my_dict = {"key0": 0, "key1": 1}
     box = Box("key0")
     return my_dict[box.get_key()]
+
+
+def set_attribute_with_setter_read_with_getter_bad():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key0")
+    box.set_key("key2")
+    return my_dict[box.get_key()]
+
+
+def set_attribute_with_setter_read_with_getter_ok():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key2")
+    box.set_key("key0")
+    return my_dict[box.get_key()]
+
+
+# requires specialization because box could be anything
+def return_get_key(box):
+    return box.get_key()
+
+
+def set_attribute_with_init_read_with_return_get_key_bad():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key2")
+    return my_dict[return_get_key(box)]
+
+
+def set_attribute_with_init_read_with_return_get_key_ok():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key0")
+    return my_dict[return_get_key(box)]
+
+
+# requires specialization because box and dict could be anything
+# the generated summary returns an unknown value because of subscript builtin
+def read_dict_using_get_key(d, box):
+    return d[box.get_key()]
+
+
+def fn_set_attribute_with_init_read_with_external_call_bad():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key2")
+    return read_dict_using_get_key(my_dict, box)
+
+
+def set_attribute_with_init_read_with_external_call_ok():
+    my_dict = {"key0": 0, "key1": 1}
+    box = Box("key0")
+    return read_dict_using_get_key(my_dict, box)
