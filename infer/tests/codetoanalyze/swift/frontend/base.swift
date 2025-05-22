5704cb33818e79d3b8d1e4de98828b3e45b269c9
Dulma Churchill <dulmarod@meta.com>
2025-05-22 05:59:06 -0700

    [swift] Add tests about basic method calls
    Reviewed By: ngorogiannis
    
    Differential Revision: D75152453
    
    fbshipit-source-id: c8de16da938904d8ed31a981e4b7388b0ddd55cf

--- infer/tests/codetoanalyze/swift/frontend/base.swift
+++ infer/tests/codetoanalyze/swift/frontend/base.swift
@@ -3,6 +3,14 @@ func returnOne() -> Int {
     return 1
 }
 
-func test1(n: Int) -> Int {
+func test1(_ n: Int) -> Int {
     return n
+}
+
+func test2() -> Int {
+    return returnOne()
+}
+
+func test3(_ n : Int) -> Int {
+    return test1(n)
 }
