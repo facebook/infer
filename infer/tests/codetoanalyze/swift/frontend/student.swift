30d7b3985cf224fce9d34f7f5c4070dde0554ab0
Dulma Churchill <dulmarod@meta.com>
2025-09-04 04:13:34 -0700

    [swift] New test that shows a typechecking error
    Reviewed By: davidpichardie
    
    Differential Revision: D81613783
    
    fbshipit-source-id: be7849ec5a01c4c39ccc5135c309d3a8c9a83016

--- infer/tests/codetoanalyze/swift/frontend/student.swift
+++ infer/tests/codetoanalyze/swift/frontend/student.swift
@@ -1,12 +1,14 @@
 class Student {
     let age: Int
-    init(age: Int) {
+    var weight: Int
+    init(age: Int, weight: Int) {
         self.age = age
+        self.weight = weight
     }
 }
 
 func create_student() -> Int {
-    let john = Student(age: 30)
+    let john = Student(age: 30, weight: 60)
     return john.age
 }
 
@@ -16,4 +18,10 @@ func test_student_allocation_bad() {
 
 func test_student_allocation_good() {
     assert(create_student() == 30)
+}
+
+
+func setWeight( _ weight: Int) {
+    let john = Student(age: 30, weight: 60)
+    john.weight = weight
 }
