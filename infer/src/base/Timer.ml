8cd83b02c47e7e79456a81bd8734f755b5bce5a8
Nikos Gorogiannis <nikosg@meta.com>
2025-09-05 09:21:57 -0700

    [timer] reimplement using Mtime
    Summary: The timer module used Unix.gettimes to get the current time, which leads to contention around a futex in multicore mode when all threads are racing on it. We change the implementation to use the existing Mtime module, which provides access to the clock_gettime system call which doesn't seem to have the same drawbacks. We also re-enable timeouts on multicore as a result.
    
    Reviewed By: davidpichardie
    
    Differential Revision:
    D81702853
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 84bdf3d002bdd3c06bbe8a38baf5c01d78caaa36

--- infer/src/base/Timer.ml
+++ infer/src/base/Timer.ml
@@ -10,23 +10,21 @@ module L = Logging
 
 exception Timeout of float
 
-let now () = if Config.multicore then 0.0 else (Unix.times ()).tms_utime
+type timer =
+  { last_start_time: Mtime.t (* Last time the timer was started *)
+  ; previous_time_spent: Mtime.span (* Duration already recorded before last start *) }
+
+type state = Mtime.span option
 
 let timer = DLS.new_key (fun () -> None)
 
-let start () = DLS.set timer (Some (now ()))
+let now () = Mtime_clock.now ()
 
-let time_since start_time = now () -. start_time
+let start () = DLS.set timer (Some {last_start_time= now (); previous_time_spent= Mtime.Span.zero})
 
-let get () =
-  match DLS.get timer with
-  | None ->
-      L.die InternalError "trying to get the value of the timer but no timer is active"
-  | Some start_time ->
-      time_since start_time
+let time_since {last_start_time; previous_time_spent} =
+  Mtime.Span.add previous_time_spent (Mtime.span (now ()) last_start_time)
 
-
-type state = float option
 
 let suspend () : state =
   let current_timer = DLS.get timer in
@@ -35,19 +33,27 @@ let suspend () : state =
 
 
 let resume (t : state) =
-  (* forget about the time spent between [suspend ()] and [resume _] by pretending the timer started
-     at [now - previous_time_spent] *)
-  Option.map t ~f:(fun previous_time_spent -> now () -. previous_time_spent) |> DLS.set timer
+  Option.map t ~f:(fun previous_time_spent -> {last_start_time= now (); previous_time_spent})
+  |> DLS.set timer
 
 
-let check_timeout timeout =
-  let span = get () in
-  if Float.(span >. timeout) then (
-    Stats.incr_timeouts () ;
-    raise (Timeout span) )
+let get () =
+  match DLS.get timer with
+  | None ->
+      L.die InternalError "trying to get the value of the timer but no timer is active"
+  | Some start_time ->
+      Float.(Mtime.Span.to_float_ns (time_since start_time) / 1_000_000_000.0)
 
 
-let check_timeout () = if not Config.multicore then Option.iter ~f:check_timeout Config.timeout
+let check_timeout () =
+  let check_timeout timeout =
+    let span = get () in
+    if Float.(span >. timeout) then (
+      Stats.incr_timeouts () ;
+      raise (Timeout span) )
+  in
+  Option.iter ~f:check_timeout Config.timeout
+
 
 let time timeable ~on_timeout ~f =
   let timer = suspend () in
