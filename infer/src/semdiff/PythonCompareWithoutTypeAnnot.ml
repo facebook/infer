6ae655df6b075012e799708b4355f0c9e614cce5
Gabriela Cunha Sampaio <gabrielasampaio@meta.com>
2025-09-17 06:09:16 -0700

    [semdiff] change output diff format
    Summary: Showing the ast diff as before is not much useful because it's something hard for the human to read. We now change the output to be like a "git diff". We first compare the ASTs and get the lines which differ and then show the corresponding lines of the program.
    
    Reviewed By: davidpichardie
    
    Differential Revision:
    D82302868
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 5f8f6aef2772af9c6c3cc96b61e17ee5fc24b742

--- infer/src/semdiff/PythonCompareWithoutTypeAnnot.ml
+++ infer/src/semdiff/PythonCompareWithoutTypeAnnot.ml
@@ -6,6 +6,7 @@
  *)
 
 open! IStd
+module L = Logging
 
 let init () =
   let () = if not (Py.is_initialized ()) then Py.initialize ~interpreter:Version.python_exe () in
@@ -15,11 +16,11 @@ import ast
 def strip_type_annotations(tree):
     class TypeAnnotationRemover(ast.NodeTransformer):
         def _remove_arg_annotations(self, args):
-            return [ast.arg(arg=a.arg, annotation=None) for a in args]
+            return [ast.arg(arg=a.arg, annotation=None, lineno=a.lineno, end_lineno=a.end_lineno) for a in args]
         def _remove_fun_arg_annotations(self, node):
             node.returns = None
             node.args.args = self._remove_arg_annotations(node.args.args)
-            node.args.kwarg = ast.arg(arg=node.args.kwarg.arg, annotation=None) if node.args.kwarg else None
+            node.args.kwarg = ast.arg(arg=node.args.kwarg.arg, annotation=None, lineno=node.args.kwarg.lineno, end_lineno=node.args.kwarg.end_lineno) if node.args.kwarg else None
             node.args.kwonlyargs = self._remove_arg_annotations(node.args.kwonlyargs)
             return self.generic_visit(node)
         def visit_FunctionDef(self, node):
@@ -28,7 +29,7 @@ def strip_type_annotations(tree):
             return self._remove_fun_arg_annotations(node)
         def visit_AnnAssign(self, node):
             # Convert annotated assignment to normal assignment
-            return ast.Assign(targets=[node.target], value=node.value)
+            return ast.Assign(targets=[node.target], value=node.value, lineno=node.lineno, end_lineno=node.end_lineno)
         def visit_ImportFrom(self, node):
             return None
         def visit_Import(self, node):
@@ -51,34 +52,84 @@ def strip_type_annotations(tree):
                     return ast.copy_location(new_node, node)
             return self.generic_visit(node)
     return ClassEqualityToIsInstanceTransformer().visit(TypeAnnotationRemover().visit(tree))
+
+def diff_asts(t1, t2):
+    diffs = []
+
+    def collect_lines(node):
+        """Collect unique line numbers from a subtree rooted at node."""
+        lines = set()
+        if isinstance(node, ast.AST):
+            if hasattr(node, "lineno"):
+                lines.add(node.lineno)
+            for field in node._fields:
+                value = getattr(node, field, None)
+                if isinstance(value, list):
+                    for item in value:
+                        lines.update(collect_lines(item))
+                elif isinstance(value, ast.AST):
+                    lines.update(collect_lines(value))
+        return lines
+
+    def compare(n1, n2):
+        if type(n1) != type(n2):
+            for l1 in collect_lines(n1):
+                diffs.append((l1, -1))
+            for l2 in collect_lines(n2):
+                diffs.append((-1, l2))
+            return
+
+        if isinstance(n1, ast.AST):
+            for field in n1._fields:
+                v1 = getattr(n1, field, None)
+                v2 = getattr(n2, field, None)
+
+                if isinstance(v1, list) and isinstance(v2, list):
+                    for x1, x2 in zip(v1, v2):
+                        compare(x1, x2)
+
+                    for x1 in v1[len(v2):]:
+                        for l1 in collect_lines(x1):
+                            diffs.append((l1, -1))
+                    for x2 in v2[len(v1):]:
+                        for l2 in collect_lines(x2):
+                            diffs.append((-1, l2))
+
+                elif isinstance(v1, ast.AST) and isinstance(v2, ast.AST):
+                    compare(v1, v2)
+
+                elif v1 != v2:
+                    for l1 in collect_lines(n1):
+                        diffs.append((l1, -1))
+                    for l2 in collect_lines(n2):
+                        diffs.append((-1, l2))
+                    return
+
+    for stmt1, stmt2 in zip(t1.body, t2.body):
+        compare(stmt1, stmt2)
+
+    for stmt1 in t1.body[len(t2.body):]:
+        for l1 in collect_lines(stmt1):
+            diffs.append((l1, -1))
+    for stmt2 in t2.body[len(t1.body):]:
+        for l2 in collect_lines(stmt2):
+            diffs.append((-1, l2))
+
+    return diffs
   |}
   in
   let ast_module = Py.Import.import_module "ast" in
   let main_module = Py.Import.import_module "__main__" in
   let _ = Py.Run.simple_string strip_annotations_code in
   let strip_func = Py.Module.get main_module "strip_type_annotations" in
-  let dump_func = Py.Module.get ast_module "dump" |> Py.Callable.to_function_with_keywords in
-  (strip_func, ast_module, dump_func)
+  let diff_func = Py.Module.get main_module "diff_asts" in
+  (strip_func, ast_module, diff_func)
 
 
 let parse_and_strip strip_func ast_module source =
   let parse_func = Py.Module.get ast_module "parse" |> Py.Callable.to_function in
   let tree = parse_func [|Py.String.of_string source|] in
   Py.Callable.to_function strip_func [|tree|]
-
-
-let ast_to_string dump_func ast = dump_func [|ast|] [] |> Py.String.to_string
-
-let rec diff_lines l1 l2 =
-  match (l1, l2) with
-  | [], [] ->
-      []
-  | x :: xs, [] ->
-      ("- " ^ x) :: diff_lines xs []
-  | [], y :: ys ->
-      ("+ " ^ y) :: diff_lines [] ys
-  | x :: xs, y :: ys ->
-      if String.equal x y then diff_lines xs ys else ("- " ^ x) :: ("+ " ^ y) :: diff_lines xs ys
 
 
 let write_output previous_file current_file diffs =
@@ -94,19 +145,48 @@ let write_output previous_file current_file diffs =
   Out_channel.with_file out_path ~f:(fun out_channel -> Yojson.Safe.to_channel out_channel json)
 
 
+let tuple_to_int_pair (t : Py.Object.t) =
+  let lst = Py.Tuple.to_list t in
+  let get_some = function
+    | Some obj ->
+        obj
+    | None ->
+        L.die InternalError "Expected a value in tuple"
+  in
+  let a = get_some (List.nth lst 0) in
+  let b = get_some (List.nth lst 1) in
+  (Py.Int.to_int a, Py.Int.to_int b)
+
+
+let diff_code (pairs : (int * int) list) (code1 : string) (code2 : string) : string list =
+  let lines1 = String.split_on_chars ~on:['\n'] code1 in
+  let lines2 = String.split_on_chars ~on:['\n'] code2 in
+  let get_line lines n : string =
+    if Int.equal n (-1) then ""
+    else if n - 1 < List.length lines then List.nth_exn lines (n - 1)
+    else ""
+  in
+  let diffs =
+    List.fold_left
+      ~f:(fun acc (l1, l2) ->
+        let line1 = get_line lines1 l1 in
+        let line2 = get_line lines2 l2 in
+        let acc = if String.length line1 > 0 then acc @ ["- " ^ line1] else acc in
+        let acc = if String.length line2 > 0 then acc @ ["+ " ^ line2] else acc in
+        acc )
+      ~init:[] pairs
+  in
+  diffs
+
+
 let ast_diff ?(debug = false) src1 src2 =
-  let strip_func, ast_module, dump_func = init () in
+  let strip_func, ast_module, diff_func = init () in
   let ast1 = parse_and_strip strip_func ast_module src1 in
   let ast2 = parse_and_strip strip_func ast_module src2 in
-  let s1 = ast_to_string dump_func ast1 in
-  let s2 = ast_to_string dump_func ast2 in
-  let lines1 = String.split_on_chars ~on:['\n'] s1 in
-  let lines2 = String.split_on_chars ~on:['\n'] s2 in
-  let equal = String.equal s1 s2 in
-  let diffs = if equal then [] else diff_lines lines1 lines2 in
-  if debug then (
-    Printf.printf "SemDiff:\n" ;
-    List.iter ~f:print_endline diffs ) ;
+  let diffs_pairs = Py.Callable.to_function diff_func [|ast1; ast2|] in
+  let diffs_pairs = Py.List.to_list diffs_pairs |> List.map ~f:tuple_to_int_pair in
+  let diffs = diff_code diffs_pairs src1 src2 in
+  if debug then Printf.printf "SemDiff:\n%s" (String.concat ~sep:"\n" diffs) ;
   diffs
 
 
