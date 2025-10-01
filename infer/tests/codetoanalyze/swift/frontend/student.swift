c2431c2fda9700587c6d1c521897710f14b4bdcf
Dulma Churchill <dulmarod@meta.com>
2025-10-01 09:07:54 -0700

    [swift] Adding a struct example
    Summary:
    Adding a similar example to the one about classes to see the differences between translation of classes and structs.
    
    In structs, the line `john.weight = weight` is not resolved by calling the setter like in classes.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D83657593
    
    fbshipit-source-id: 67b85996b662d9bc25fb435129040cacfaf791d5

--- infer/tests/codetoanalyze/swift/frontend/student.swift
+++ infer/tests/codetoanalyze/swift/frontend/student.swift
@@ -8,8 +8,7 @@ class Student {
 }
 
 func create_student() -> Int {
-    let john = Student(age: 30, weight: 60)
-    return john.age
+    return Student(age: 30, weight: 60).age
 }
 
 func test_student_allocation_bad() {
@@ -23,5 +22,32 @@ func test_student_allocation_good() {
 
 func setWeight( _ weight: Int) {
     let john = Student(age: 30, weight: 60)
+    john.weight = weight
+}
+
+struct Employee {
+    let age: Int
+    var weight: Int
+    init(age: Int, weight: Int) {
+        self.age = age
+        self.weight = weight
+    }
+}
+
+func create_employee() -> Int {
+    return Employee(age: 30, weight: 60).age
+}
+
+func test_employee_allocation_bad() {
+    assert(create_employee() == 35)
+}
+
+func test_employee_allocation_good() {
+    assert(create_employee() == 30)
+}
+
+
+func setEmployeeWeight( _ weight: Int) {
+    var john = Employee(age: 30, weight: 60)
     john.weight = weight
 }
