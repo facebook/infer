4edd942e1721dc7aa7d739f1432793299e628632
Dulma Churchill <dulmarod@meta.com>
2025-10-21 03:55:51 -0700

    [swift] Add optional example
    Summary: Simple optional example. There are some problems with the translation. In particular, the typing error below.
    
    Reviewed By: martintrojer
    
    Differential Revision: D85046861
    
    fbshipit-source-id: dc4cb4d82ec88bfe1db9b732c94a1364ca18c9d6

--- infer/tests/codetoanalyze/swift/frontend/student.swift
+++ infer/tests/codetoanalyze/swift/frontend/student.swift
@@ -103,3 +103,19 @@ func test_retain_cycle1_bad() {
     let jane = Individual(age: 35)
     set_spouses(john, jane)
 }
+
+func test_optional(_ age : Int?) -> Int {
+    if let actualAge = age {
+        return actualAge
+    } else {
+        return 0
+    }
+}
+
+func test_optional_good() {
+    assert(test_optional(30) == 30)
+}
+
+func test_optional_bad() {
+    assert(test_optional(30) == 35)
+}
