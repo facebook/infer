e933fdc2b8662fe3dda241df52fa396400b864a9
David Pichardie <dpichardie@meta.com>
2025-11-19 01:24:54 -0800

    [infinite-loop] new FP
    Summary: These examples make more explicit the current problem we have with our formula manipulation
    
    Reviewed By: ngorogiannis
    
    Differential Revision:
    D87236662
    
    Privacy Context Container: L1208441
    
    fbshipit-source-id: 26bb09f6c0e2028dfa652ce6280aab4941476271

--- infer/tests/codetoanalyze/c/pulse/infinite.c
+++ infer/tests/codetoanalyze/c/pulse/infinite.c
@@ -996,3 +996,27 @@ int nested_goto_bad(int b1, int b2, int b3, int b4) {
     }
   }
 }
+
+void FP_no_incr_first_iterations_ok() {
+  int i = 0;
+  int j = 0;
+  while (i < 1) {
+    i += j / 2;
+    j++;
+  }
+}
+
+void incr_if_eq(int k1, int k2, int* x) {
+  if (k1 == k2) {
+    (*x)++;
+  }
+}
+
+void FP_no_incr_first_iterations_interproc_ok() {
+  int i = 0;
+  int j = 0;
+  while (i < 1) {
+    incr_if_eq(j, 2, &i);
+    j++;
+  }
+}
