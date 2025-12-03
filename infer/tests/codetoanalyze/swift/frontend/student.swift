abc2fd86b39bd4a3e0ca24f99f94d753fe73d00b
Dulma Churchill <dulmarod@meta.com>
2025-12-03 08:07:12 -0800

    [swift] Adding retain cycle test
    Summary: Repro example from a real diff shadow job. We can work to improve the message and trace's readability.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D88268573
    
    fbshipit-source-id: d3e52d83ed58c493cb9c07174033b7ea162715aa

--- infer/tests/codetoanalyze/swift/frontend/student.swift
+++ infer/tests/codetoanalyze/swift/frontend/student.swift
@@ -197,3 +197,33 @@ class RetainCycleExample {
         }
     }
 }
+
+final class State {
+    var delegate: DeviceAppManagerClientDelegate?
+}
+
+protocol DeviceAppManagerClientDelegate: AnyObject {}
+
+final class DeviceAppManagerDelegateImpl: DeviceAppManagerClientDelegate {
+    let onStartServiceResponse: () -> Void
+    init(onStartServiceResponse: @escaping () -> Void) {
+        self.onStartServiceResponse = onStartServiceResponse
+    }
+}
+
+func foo(_ state : State?) {}
+
+func test_retain_cycle_bad2() {
+    // Set up state and a reference to it
+    let state = State()
+    var stateRef: State? = state
+
+    // Create the delegate, capturing stateRef in the closure
+    let delegate = DeviceAppManagerDelegateImpl {
+        // This closure captures stateRef, creating a retain cycle
+        stateRef = nil
+    }
+    foo(stateRef)
+
+    state.delegate = delegate
+}
