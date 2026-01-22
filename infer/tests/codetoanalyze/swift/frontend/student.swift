83b7020d695e82337af3e78b3a32e2f073909d34
Dulma Churchill <dulmarod@meta.com>
2026-01-22 07:44:28 -0800

    [swift] Example retain cycle false positive because of variable shadowing
    Summary: The example shows a retain cycle fp because we identify the two variables quality, one is a formal and the other a local. To be fixed in the next diff.
    
    Reviewed By: ngorogiannis
    
    Differential Revision: D91132126
    
    fbshipit-source-id: 7abc83fe374148d051f25f14de8c052771aca064

--- infer/tests/codetoanalyze/swift/frontend/student.swift
+++ infer/tests/codetoanalyze/swift/frontend/student.swift
@@ -285,3 +285,13 @@ class Profile {
         print("Updating collection view for items: \(items)")
     }
 }
+
+struct Quality: Equatable {
+    let value: Int
+    let sndValue: Int
+}
+
+func retain_cycle_variable_shadow_fp(quality: Int) -> Quality {
+    let quality = Quality(value: quality, sndValue: 0)
+    return quality
+}
